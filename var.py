39fb092f0949a5d546290ad208fcea51ab518814
Rob Hagemans <robhagemans@yahoo.co.uk>
2014-01-23 21:00:29 +0000

    var.py cleanup

--- var.py
+++ var.py
@@ -9,7 +9,6 @@
 # please see text file COPYING for licence terms.
 #
 
-
 import error
 import vartypes
 from string_ptr import StringPtr
@@ -40,27 +39,24 @@ def clear_variables():
     common_names = []
     common_array_names = []
 
-
 def set_var(name, value):
     global variables
     name = vartypes.complete_name(name)
     if value[0]=='$':
         if len(str(vartypes.unpack_string(value)))>255:
             # this is a copy if we use StringPtr!
-            value = ('$', str(vartypes.unpack_string(value)[:255]))
+            value = vartypes.pack_string(str(vartypes.unpack_string(value)[:255]))
     variables[name] = vartypes.pass_type_keep(name[-1], value)[1]
-    
     
 def get_var(name):
     name = vartypes.complete_name(name)
     try:
         if name[-1] == '$':
-            return ('$', str(variables[name]) ) # cast StringPtrs, if any
+            return vartypes.pack_string(str(variables[name]) ) # cast StringPtrs, if any
         else:
             return (name[-1], variables[name])
     except KeyError:
         return vartypes.null[name[-1]]
-
 
 def swap_var(name1, name2):
     global variables
@@ -76,7 +72,6 @@ def swap_var(name1, name2):
         val1 = variables[name1] # we need a pointer swap #get_var(name1)
         variables[name1] = variables[name2]
         variables[name2] = val1
-        
 
 def erase_array(name):
     global arrays
@@ -101,13 +96,11 @@ def index_array(index, dimensions):
 def array_len(dimensions):
     return index_array(dimensions, dimensions)+1    
 
-
 def var_size_bytes(name):
     try:
         return byte_size[name[-1]]  
     except KeyError:
         raise error.RunError(5)
-
 
 def array_size_bytes(name):
     try:
@@ -116,7 +109,6 @@ def array_size_bytes(name):
         return 0
     size = array_len(dimensions)
     return size*var_size_bytes(name)     
-
 
 def dim_array(name, dimensions):
     global arrays
@@ -137,15 +129,15 @@ def dim_array(name, dimensions):
     else:
         arrays[name] = [ dimensions, bytearray(size*var_size_bytes(name)) ]  
 
-
-
 def check_dim_array(name, index):
-    if name not in arrays:
+    try:
+        [dimensions, lst] = arrays[name]
+    except KeyError:
         # auto-dimension - 0..10 or 1..10 
         # this even fixes the dimensions if the index turns out to be out of range!
         dimensions = [ 10 ] * len(index)
         dim_array(name, dimensions)
-    [dimensions, lst] = arrays[name]
+        [dimensions, lst] = arrays[name]
     if len(index) != len(dimensions):
         raise error.RunError(9)
     for i in range(len(index)):
@@ -155,7 +147,6 @@ def check_dim_array(name, index):
             # WARNING: dimensions is the *maximum index number*, regardless of array_base!
             raise error.RunError(9)
     return [dimensions, lst]
-
 
 def get_bytearray(name):
     if name[-1]=='$':
@@ -167,7 +158,6 @@ def get_bytearray(name):
     except KeyError:
         return bytearray()
 
-
 def base_array(base):
     global array_base
     if base not in (1, 0):
@@ -177,7 +167,6 @@ def base_array(base):
         # duplicate definition
         raise error.RunError(10)
     array_base = base
-
 
 def get_array(name, index):
     [dimensions, lst] = check_dim_array(name, index)
@@ -189,7 +178,6 @@ def get_array(name, index):
         return ('%', vartypes.sint_to_value(value))
     else:
         return (name[-1], value)
-    
     
 def set_array(name, index, value):
     [dimensions, lst] = check_dim_array(name, index)
@@ -203,8 +191,6 @@ def set_array(name, index, value):
         lst[bigindex*bytesize:(bigindex+1)*bytesize] = vartypes.value_to_sint(value)
     else:
         lst[bigindex*bytesize:(bigindex+1)*bytesize] = value
-
-
     
 def get_var_or_array(name, indices):
     if indices == []:
@@ -212,14 +198,12 @@ def get_var_or_array(name, indices):
     else:
         return get_array(name, indices)
 
-
 def set_var_or_array(name, indices, value):
-    if indices != []:    
-        set_array(name, indices, value)
-    else:
+    if indices == []:    
         set_var(name, value)
-
-
+    else:
+        set_array(name, indices, value)
+        
 def set_field_var(field, varname, offset, length):
     if varname[-1] != '$':
         # type mismatch
@@ -227,8 +211,7 @@ def set_field_var(field, varname, offset, length):
     str_ptr = StringPtr(field, offset, length)
     # assign the string ptr to the variable name
     # desired side effect: if we re-assign this string variable through LET, it's no longer connected to the FIELD.
-    set_var(varname, ('$', str_ptr))
-
+    set_var(varname, vartypes.pack_string(str_ptr))
     
 def lset(varname, value, justify_right=False):
     if varname[-1] != '$' or value[0] != '$':
@@ -250,7 +233,6 @@ def lset(varname, value, justify_right=False):
         variables[varname].set_str(s)    
     else:
         variables[varname] = s    
-
 
 # for reporting by FRE()        
 def variables_memory_size():
