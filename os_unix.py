bdabd0050955a3b327bdeae2a5258d1a0819b22f
Rob Hagemans <robhagemans@yahoo.co.uk>
2014-02-01 11:32:04 +0000

    os_unix.py code cleanup

--- os_unix.py
+++ os_unix.py
@@ -23,7 +23,6 @@ shell_cmd = shell + ' -c'
 def disk_free(path):
     st = os.statvfs(path)
     return st.f_bavail * st.f_frsize
-        
 
 def lock(fd, access, lock, length=0, start=0, whence=0):
     # w locks are rw locks - unix distinguishes 'shared' and 'exclusive' locks 
@@ -32,42 +31,35 @@ def lock(fd, access, lock, length=0, start=0, whence=0):
     else:
         fcntl.lockf(fd, fcntl.LOCK_SH, length, start, whence)
 
-
 def unlock(fd):
     fcntl.flock(fd, fcntl.LOCK_UN)
-    
   
 def spawn_interactive_shell(cmd):
     try:
         p = pexpect.spawn(cmd)
     except Exception:
         return 
-    
     while True:
         c = console.get_char()
-        if c=='\x08': # BACKSPACE
+        if c == '\x08': # BACKSPACE
             p.send('\x7f')
         elif c != '':
             p.send(c)
-            
         c = ''
         try:
             c = p.read_nonblocking(1, timeout=0)
         except: 
             pass
-            
         if c != '':
-            if c=='\r':
+            if c == '\r':
                 console.idle()
                 console.check_events()
-            elif c=='\x08':
+            elif c == '\x08':
                 if console.col !=1:
                     console.col -= 1
             else:
                 console.write(c)
-                
         elif not p.isalive(): 
             break
-        
         
 
