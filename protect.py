09dd944681bb00df1e0f6eead7278a4dc0adf70b
Rob Hagemans <robhagemans@yahoo.co.uk>
2014-02-01 19:52:45 +0000

    protect.py code cleanup

--- protect.py
+++ protect.py
@@ -18,59 +18,44 @@
 key1 = [0xA9, 0x84, 0x8D, 0xCD, 0x75, 0x83, 0x43, 0x63, 0x24, 0x83, 0x19, 0xF7, 0x9A]
 key2 = [0x1E, 0x1D, 0xC4, 0x77, 0x26, 0x97, 0xE0, 0x74, 0x59, 0x88, 0x7C]
       
-        
+""" Decrypt a byte stream read from the GWBASIC ,P (read protected) format. This will allow it to be subsequently parsed """
 def unprotect(ins, outs):    
-    """ Decrypt a byte stream read from the GWBASIC ,P (read protected) format. This will allow it to be subsequently parsed """
-        
-
     index = 0
     s = ins.read(1)
     while s != '': 
-        
         nxt = ins.read(1)
         # drop last char (EOF 0x1a)
         if nxt == '':
             break
-
         c = ord(s)
-
         # Kocher's algorithm:    
         c -= 11 - (index % 11)
         c ^= key1 [index % 13]
         c ^= key2 [index % 11]
         c += 13 - (index % 13)
-        
         outs.write(chr(c%256))
         index = (index+1) % (13*11);
         s = nxt
-        
     return True
     
-
+""" Encrypt a byte stream read from the GWBASIC tokenised format """
 def protect(ins, outs):    
-    """ Encrypt a byte stream read from the GWBASIC tokenised format """
-    
     index = 0
     s = ins.read(1)
     while s != '': 
-        
         nxt = ins.read(1)
         # drop last char (EOF 0x1a)
         if nxt == '':
             break
-
         c = ord(s)
-
         # inverse Kocher's algorithm:
         c -= 13 - (index % 13)
         c ^= key1 [index % 13]
         c ^= key2 [index % 11]
         c += 11 - (index % 11)
-
         outs.write(chr(c%256))
         index = (index+1) % (13*11);
         s = nxt
-        
     return True
     
 
