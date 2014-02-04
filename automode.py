c7f8ebbb717ec0c36e7bb32f892c13eae842f621
Rob Hagemans <robhagemans@yahoo.co.uk>
2014-02-04 20:54:55 +0000

    automode.py code cleanup

--- automode.py
+++ automode.py
@@ -20,45 +20,37 @@ auto_linenum = 10
 
 
 def auto_input_loop():
-    global auto_mode, auto_linenum, auto_increment
-    
+    global auto_mode, auto_linenum
     auto_mode_show_line()
     try:
         # input loop, checks events
         line = console.read_screenline(from_start=True) 
     except error.Break:
-        program.prompt=True
-        auto_mode=False
+        program.prompt = True
+        auto_mode = False
         return ''
-
-    if line=='':
+    if line == '':
         # linenum remains the same, remove increment
         auto_linenum -= auto_increment
-        program.prompt=False
-                
+        program.prompt = False
     return auto_mode_remove_star(line)
             
-            
 def auto_mode_show_line():
-    global auto_linenum, auto_increment
-    
+    global auto_linenum
     auto_linenum += auto_increment
     console.write(str(auto_linenum))
     if auto_linenum in program.line_numbers:
         console.write('*')
     else:
         console.write(' ')
-
                 
 def auto_mode_remove_star(line):                
-    global auto_linenum
-    
     if auto_linenum in program.line_numbers:
-        num_len=len(str(auto_linenum))
+        num_len = len(str(auto_linenum))
         if line[:num_len] == str(auto_linenum):
-            line=list(line)
-            line[num_len]=' '
-            line=''.join(line)
+            line = list(line)
+            line[num_len] = ' '
+            line = ''.join(line)
     return line
 
 
