4f1ba0b010eabddb06eb12567aee749bf9735af5
Rob Hagemans <rob@wallaby.(none)>
2014-03-15 22:06:54 +0000

    main loops code cleanup

--- pcbasic.py
+++ pcbasic.py
@@ -12,7 +12,6 @@
 #
 # (GW-BASIC is a trademark of Microsoft Corporation)
 
-
 import sys
 import argparse
                 
@@ -34,6 +33,79 @@ import program
 
 
 def main():
+    args = build_parser().parse_args()
+    # converter and RUN invocations
+    if args.conv_asc:
+        convert(args.infile, args.save, 'A')
+    elif args.conv_byte:
+        convert(args.infile, args.save, 'B')
+    elif args.conv_prot:
+        convert(args.infile, args.save, 'P')
+    elif args.run or (not args.load and args.infile != None):
+        args.run = True    
+    # DEBUG mode
+    tokenise.init_DEBUG(args.debug)
+    debugstr = ' [DEBUG mode]' if args.debug else ''
+    # PEEK presets
+    if args.peek != None:
+        for a in args.peek:
+            [addr,val] = a.split(':')
+            expressions.peek_values[int(addr)]=int(val)
+    # choose the screen backends and other devices 
+    prepare_devices(args)
+    # announce ourselves; go!
+    try:
+        if not args.run and args.cmd == None:
+            console.write(greeting(debugstr))
+        run.main_loop(args.run, args.load, args.quit, args.cmd, args.infile)
+    finally:
+        # fix the terminal on exit or crashes (inportant for ANSI terminals)
+        console.close()
+
+def prepare_devices(args):
+    sound.backend = nosound
+    if args.dumb or not sys.stdout.isatty() or not sys.stdin.isatty():
+        console.backend = backend_dumb
+        console.backend.set_dumberterm()
+    elif args.uni:                
+        console.backend = backend_dumb
+        console.backend.set_dumbterm()
+    elif args.text:
+        import backend_ansi
+        console.backend = backend_ansi
+    else:   
+        import backend_pygame
+        console.backend = backend_pygame   
+        graphics.backend = backend_pygame
+        if not args.nosound:
+            sound.backend = backend_pygame
+    # initialise backends
+    console.keys_visible = (not args.run and args.cmd == None)
+    console.init()    
+    if not sound.init_sound():
+        # fallback warning here?
+        sound.backend = nosound
+    # choose peripherals    
+    deviceio.init_devices(args)
+    
+def convert(infile, outfile, mode):
+    console.backend = backend_dumb
+    console.init()
+    fin = fileio.open_file_or_device(0, infile, mode='L', access='rb', defext='') if infile else sys.stdin
+    fout = fileio.open_file_or_device(0, outfile, mode='S', access='wb', defext='') if outfile else sys.stdout
+    program.load(fin)
+    # allow conversion of protected files
+    program.protected = False
+    program.save(fout,mode)
+    console.close()
+    run.exit()
+
+def greeting(debugstr):
+    return ('PC-BASIC 3.23' + debugstr + util.endl +
+             '(C) Copyright 2013 PC-BASIC authors. Type RUN "INFO" for more.'+ util.endl +
+             ("%d Bytes free" % var.total_mem) + util.endl )
+
+def build_parser():
     parser = argparse.ArgumentParser(
         description='PC-BASIC 3.23 interpreter. If no options are present, the interpreter will run in interactive mode.')
     parser.add_argument('infile', metavar='in_file', nargs='?', 
@@ -61,85 +133,7 @@ def main():
     parser.add_argument('-p3', '--lpt3', nargs='*', metavar=('TYPE:VAL'), help='Set LPT3: to FILE:file_name or CUPS:printer_name.')
     parser.add_argument('-s1', '--com1', nargs='*', metavar=('TYPE:VAL'), help='Set COM1: to FILE:file_name or CUPS:printer_name or PORT:device_name.')
     parser.add_argument('-s2', '--com2', nargs='*', metavar=('TYPE:VAL'), help='Set COM2: to FILE:file_name or CUPS:printer_name PORT:device_name.')
-    args = parser.parse_args()
-    ########################################
-    # converter invocations
-    if args.conv_asc:
-        convert(args.infile, args.save, 'A')
-    elif args.conv_byte:
-        convert(args.infile, args.save, 'B')
-    elif args.conv_prot:
-        convert(args.infile, args.save, 'P')
-    elif args.run or (not args.load and args.infile != None):
-        args.run = True    
-    ########################################
-    # DEBUG mode
-    tokenise.init_DEBUG(args.debug)
-    if args.debug:
-        debugstr = ' [DEBUG mode]'
-    else:
-        debugstr = ''
-    ########################################
-    # PEEK presets
-    if args.peek != None:
-        for a in args.peek:
-            [addr,val] = a.split(':')
-            expressions.peek_values[int(addr)]=int(val)
-    ########################################
-    # choose the screen 
-    sound.backend = nosound
-    if args.dumb or not sys.stdout.isatty() or not sys.stdin.isatty():
-        console.backend = backend_dumb
-        console.backend.set_dumberterm()
-    elif args.uni:                
-        console.backend = backend_dumb
-        console.backend.set_dumbterm()
-    elif args.text:
-        import backend_ansi
-        console.backend = backend_ansi
-    else:   
-        import backend_pygame
-        console.backend = backend_pygame   
-        graphics.backend = backend_pygame
-        if not args.nosound:
-            sound.backend = backend_pygame
-    # initialise backends
-    console.keys_visible = (not args.run and args.cmd == None)
-    console.init()    
-    if not sound.init_sound():
-        # fallback warning here?
-        sound.backend = nosound
-    # choose peripherals    
-    deviceio.init_devices(args)
-    # prepare screen
-    try:
-        if not args.run and args.cmd == None:
-            console.write("PC-BASIC 3.23"+debugstr+util.endl)
-            console.write('(C) Copyright 2013 PC-BASIC authors. Type RUN "INFO" for more.'+util.endl)
-            console.write(("%d Bytes free" % var.total_mem) + util.endl)
-        run.init_run(args.run, args.load, args.quit, args.cmd, args.infile)
-        run.main_loop()    
-    finally:
-        # fix the terminal
-        console.close()
-
-
-def convert(infile, outfile, mode):
-    #error.warnings_on = True
-    console.backend = backend_dumb
-    console.init()
-    fin = sys.stdin
-    if infile:
-        fin = fileio.open_file_or_device(0, infile, mode='L', access='rb', defext='')
-    fout = sys.stdout
-    if outfile:
-        fout = fileio.open_file_or_device(0, outfile, mode='S', access='wb', defext='')
-    program.load(fin)
-    # allow conversion of protected files
-    program.protected = False
-    program.save(fout,mode)
-    console.close()
-    run.exit()
+    return parser
 
   
 main()
