5d209a25d4af7ef7ac37c310140e27cc34658021
Rob Hagemans <rob@wallaby.(none)>
2014-03-18 15:27:52 +0000

    statements.py cleaup

--- statements.py
+++ statements.py
@@ -37,14 +37,10 @@ from stat_graph import *
 from stat_sound import *
 # machine access
 from stat_machine import *
-
-tron = False
-
-
 # debugging
 from stat_debug import *
-    
-    
+
+tron = False
 
 # parses one statement at the current stream pointer in current_codestream
 # return value False: stream ends
@@ -53,11 +49,11 @@ def parse_statement():
     ins = program.current_codestream
     program.current_statement = ins.tell()
     c = util.skip_white_read(ins).upper()
-    if c=='':
+    if c == '':
         # stream has ended.
         return False
     # code stream should either be at '\x00' or at ':' to start a statement
-    elif c=='\x00':
+    elif c == '\x00':
         # line number marker, new statement
         program.linenum = util.parse_line_number(ins)
         if program.linenum == -1:
@@ -69,7 +65,7 @@ def parse_statement():
             console.write('['+('%i' % program.linenum) +']')
         debug_step(program.linenum)
         c = util.skip_white_read(ins).upper()
-    elif c==':':
+    elif c == ':':
         # new statement
         c = util.skip_white_read(ins).upper()
     if c in util.end_statement:
@@ -81,236 +77,128 @@ def parse_statement():
         # implicit LET
         ins.seek(-1,1)
         exec_let(ins)
-    elif c=='\x81':     # END
-        exec_end(ins)
-    elif c=='\x82':     # FOR
-        exec_for(ins)
-    elif c=='\x83':     # NEXT
-        exec_next(ins)
-    elif c=='\x84':     # DATA
-        exec_data(ins)
-    elif c=='\x85':     # INPUT
-        exec_input(ins)
-    elif c=='\x86':     # DIM
-        exec_dim(ins)
-    elif c=='\x87':     # READ
-        exec_read(ins)
-    elif c=='\x88':    # LET
-        exec_let(ins)
-    elif c=='\x89':     # GOTO
-        exec_goto(ins)
-    elif c=='\x8A':     # RUN
-        exec_run(ins)
-    elif c=='\x8B':     # IF
-        exec_if(ins)
-    elif c=='\x8C':     # RESTORE
-        exec_restore(ins)
-    elif c=='\x8D':     # GOSUB
-        exec_gosub(ins)
-    elif c=='\x8E':     # RETURN
-        exec_return(ins)
-    elif c=='\x8F':     # REM
-        exec_rem(ins)
-    elif c=='\x90':     # STOP
-        exec_stop(ins)
-    elif c=='\x91':     # PRINT
-        exec_print(ins)
-    elif c=='\x92':     # CLEAR
-        exec_clear(ins)  
-    elif c=='\x93':     # LIST
-        exec_list(ins)      
-    elif c=='\x94':     # NEW
-        exec_new(ins)
-    elif c=='\x95':     # ON
-        exec_on(ins)
-    elif c=='\x96':     # WAIT
-        exec_wait(ins)
-    elif c=='\x97':     # DEF
-        exec_def(ins)
-    elif c=='\x98':     # POKE
-        exec_poke(ins)
-    elif c=='\x99':     # CONT
-        exec_cont(ins)
-    elif c=='\x9C':     # OUT
-        exec_out(ins)
-    elif c=='\x9D':     # LPRINT
-        exec_lprint(ins)
-    elif c=='\x9E':     # LLIST
-        exec_llist(ins)    
-    elif c=='\xA0':     # WIDTH
-        exec_width(ins)    
-    elif c=='\xA1':     # ELSE
-        exec_else(ins)    
-    elif c=='\xA2':    # TRON
-        exec_tron(ins)
-    elif c=='\xA3':    # TROFF
-        exec_troff(ins)
-    elif c=='\xA4':    # SWAP
-        exec_swap(ins)
-    elif c=='\xA5':    # ERASE
-        exec_erase(ins)
-    elif c=='\xA6':    # EDIT
-        exec_edit(ins)
-    elif c=='\xA7':    # ERROR
-        exec_error(ins)
-    elif c=='\xA8':    # RESUME
-        exec_resume(ins)
-    elif c=='\xA9':    # DELETE
-        exec_delete(ins)
-    elif c=='\xAA':    # AUTO
-        exec_auto(ins)
-    elif c=='\xAB':    # RENUM
-        exec_renum(ins)
-    elif c=='\xAC':   # DEFSTR    
-        exec_defstr(ins)
-    elif c=='\xAD':   # DEFINT   
-        exec_defint(ins)
-    elif c=='\xAE':   # DEFSNG    
-        exec_defsng(ins)
-    elif c=='\xAF':   # DEFDBL    
-        exec_defdbl(ins)    
-    elif c=='\xB0':     # LINE
-        exec_line(ins)
-    elif c=='\xB1':     # WHILE
-        exec_while(ins)
-    elif c=='\xB2':     # WEND
-        exec_wend(ins)
-    elif c=='\xB3':     # CALL
-        exec_call(ins)
-    elif c=='\xB7':     # WRITE
-        exec_write(ins)
-    elif c=='\xB8':     # OPTION
-        exec_option(ins)
-    elif c=='\xB9':     # RANDOMIZE
-        exec_randomize(ins)
-    elif c=='\xBA':     # OPEN
-        exec_open(ins)
-    elif c=='\xBB':     # CLOSE
-        exec_close(ins)
-    elif c=='\xBC':     # LOAD
-        exec_load(ins)
-    elif c=='\xBD':     # MERGE
-        exec_merge(ins)
-    elif c=='\xBE':     # SAVE
-        exec_save(ins)
-    elif c=='\xBF':     # COLOR
-        exec_color(ins)
-    elif c=='\xC0':     # CLS
-        exec_cls(ins)
-    elif c=='\xC1':     # MOTOR
-        exec_motor(ins)        
-    elif c=='\xC2':     # BSAVE
-        exec_bsave(ins)        
-    elif c=='\xC3':     # BLOAD
-        exec_bload(ins)        
-    elif c=='\xC4':     # SOUND
-        exec_sound(ins)        
-    elif c=='\xC5':     # BEEP
-        exec_beep(ins)        
-    elif c=='\xC6':     # PSET
-        exec_pset(ins)        
-    elif c=='\xC7':     # PRESET
-        exec_preset(ins)        
-    elif c=='\xC8':     # SCREEN
-        exec_screen(ins)
-    elif c=='\xC9':     # KEY
-        exec_key(ins)
-    elif c=='\xCA':     # LOCATE
-        exec_locate(ins)
+    elif c == '\x81':     exec_end(ins)
+    elif c == '\x82':     exec_for(ins)
+    elif c == '\x83':     exec_next(ins)
+    elif c == '\x84':     exec_data(ins)
+    elif c == '\x85':     exec_input(ins)
+    elif c == '\x86':     exec_dim(ins)
+    elif c == '\x87':     exec_read(ins)
+    elif c == '\x88':     exec_let(ins)
+    elif c == '\x89':     exec_goto(ins)
+    elif c == '\x8A':     exec_run(ins)
+    elif c == '\x8B':     exec_if(ins)
+    elif c == '\x8C':     exec_restore(ins)
+    elif c == '\x8D':     exec_gosub(ins)
+    elif c == '\x8E':     exec_return(ins)
+    elif c == '\x8F':     exec_rem(ins)
+    elif c == '\x90':     exec_stop(ins)
+    elif c == '\x91':     exec_print(ins)
+    elif c == '\x92':     exec_clear(ins)  
+    elif c == '\x93':     exec_list(ins)      
+    elif c == '\x94':     exec_new(ins)
+    elif c == '\x95':     exec_on(ins)
+    elif c == '\x96':     exec_wait(ins)
+    elif c == '\x97':     exec_def(ins)
+    elif c == '\x98':     exec_poke(ins)
+    elif c == '\x99':     exec_cont(ins)
+    elif c == '\x9C':     exec_out(ins)
+    elif c == '\x9D':     exec_lprint(ins)
+    elif c == '\x9E':     exec_llist(ins)    
+    elif c == '\xA0':     exec_width(ins)    
+    elif c == '\xA1':     exec_else(ins)    
+    elif c == '\xA2':     exec_tron(ins)
+    elif c == '\xA3':     exec_troff(ins)
+    elif c == '\xA4':     exec_swap(ins)
+    elif c == '\xA5':     exec_erase(ins)
+    elif c == '\xA6':     exec_edit(ins)
+    elif c == '\xA7':     exec_error(ins)
+    elif c == '\xA8':     exec_resume(ins)
+    elif c == '\xA9':     exec_delete(ins)
+    elif c == '\xAA':     exec_auto(ins)
+    elif c == '\xAB':     exec_renum(ins)
+    elif c == '\xAC':     exec_defstr(ins)
+    elif c == '\xAD':     exec_defint(ins)
+    elif c == '\xAE':     exec_defsng(ins)
+    elif c == '\xAF':     exec_defdbl(ins)    
+    elif c == '\xB0':     exec_line(ins)
+    elif c == '\xB1':     exec_while(ins)
+    elif c == '\xB2':     exec_wend(ins)
+    elif c == '\xB3':     exec_call(ins)
+    elif c == '\xB7':     exec_write(ins)
+    elif c == '\xB8':     exec_option(ins)
+    elif c == '\xB9':     exec_randomize(ins)
+    elif c == '\xBA':     exec_open(ins)
+    elif c == '\xBB':     exec_close(ins)
+    elif c == '\xBC':     exec_load(ins)
+    elif c == '\xBD':     exec_merge(ins)
+    elif c == '\xBE':     exec_save(ins)
+    elif c == '\xBF':     exec_color(ins)
+    elif c == '\xC0':     exec_cls(ins)
+    elif c == '\xC1':     exec_motor(ins)        
+    elif c == '\xC2':     exec_bsave(ins)        
+    elif c == '\xC3':     exec_bload(ins)        
+    elif c == '\xC4':     exec_sound(ins)        
+    elif c == '\xC5':     exec_beep(ins)        
+    elif c == '\xC6':     exec_pset(ins)        
+    elif c == '\xC7':     exec_preset(ins)        
+    elif c == '\xC8':     exec_screen(ins)
+    elif c == '\xC9':     exec_key(ins)
+    elif c == '\xCA':     exec_locate(ins)
     # two-byte tokens
-    elif c=='\xFD':
+    elif c == '\xFD':
         ins.read(1)
-        # these are all expression tokens, not statement tokens.
-        # syntax error
+        # syntax error; these are all expression tokens, not statement tokens.
         raise error.RunError(2)
     # two-byte tokens
-    elif c=='\xFE':
+    elif c == '\xFE':
         c = ins.read(1)
-        if c=='\x81':      # FILES
-            exec_files(ins)
-        elif c=='\x82':    # FIELD
-            exec_field(ins)
-        elif c=='\x83':    # SYSTEM
-            exec_system(ins)
-        elif c=='\x84':    # NAME
-            exec_name(ins)
-        elif c=='\x85':    # LSET
-            exec_lset(ins)
-        elif c=='\x86':    # RSET
-            exec_rset(ins)
-        elif c=='\x87':    # KILL
-            exec_kill(ins)
-        elif c=='\x88':    # PUT
-            exec_put(ins)
-        elif c=='\x89':    # GET
-            exec_get(ins)
-        elif c=='\x8A':    # RESET
-            exec_reset(ins)
-        elif c=='\x8B':    # COMMON
-            exec_common(ins)
-        elif c=='\x8C':    # CHAIN
-            exec_chain(ins)
-        elif c=='\x8D':    # DATE$
-            exec_date(ins)
-        elif c=='\x8E':    # TIME$
-            exec_time(ins)
-        elif c=='\x8F':    # PAINT
-            exec_paint(ins)
-        elif c=='\x90':    # COM
-            exec_com(ins)
-        elif c=='\x91':    # CIRCLE
-            exec_circle(ins)
-        elif c=='\x92':    # DRAW
-            exec_draw(ins)
-        elif c=='\x93':    # PLAY
-            exec_play(ins)
-        elif c=='\x94':    # TIMER
-            exec_timer(ins)
-        elif c=='\x96':    # IOCTL
-            exec_ioctl(ins)
-        elif c=='\x97':    # CHDIR
-            exec_chdir(ins)
-        elif c=='\x98':    # MKDIR
-            exec_mkdir(ins)
-        elif c=='\x99':    # RMDIR
-            exec_rmdir(ins)
-        elif c=='\x9A':    # SHELL
-            exec_shell(ins)
-        elif c=='\x9B':    # ENVIRON
-            exec_environ(ins)
-        elif c=='\x9C':    # VIEW
-            exec_view(ins)
-        elif c=='\x9D':    # WINDOW
-            exec_window(ins)
-        elif c=='\x9F':    # PALETTE
-            exec_palette(ins)
-        elif c=='\xA0':    # LCOPY
-            exec_lcopy(ins)
-        elif c=='\xA4':     # DEBUG
-            exec_DEBUG(ins)
-        elif c=='\xA5':     # PCOPY
-            exec_pcopy(ins)
-        elif c== '\xA7':    # LOCK
-            exec_lock(ins)
-        elif c== '\xA8':    # UNLOCK
-            exec_unlock(ins)
+        if   c == '\x81':    exec_files(ins)
+        elif c == '\x82':    exec_field(ins)
+        elif c == '\x83':    exec_system(ins)
+        elif c == '\x84':    exec_name(ins)
+        elif c == '\x85':    exec_lset(ins)
+        elif c == '\x86':    exec_rset(ins)
+        elif c == '\x87':    exec_kill(ins)
+        elif c == '\x88':    exec_put(ins)
+        elif c == '\x89':    exec_get(ins)
+        elif c == '\x8A':    exec_reset(ins)
+        elif c == '\x8B':    exec_common(ins)
+        elif c == '\x8C':    exec_chain(ins)
+        elif c == '\x8D':    exec_date(ins)
+        elif c == '\x8E':    exec_time(ins)
+        elif c == '\x8F':    exec_paint(ins)
+        elif c == '\x90':    exec_com(ins)
+        elif c == '\x91':    exec_circle(ins)
+        elif c == '\x92':    exec_draw(ins)
+        elif c == '\x93':    exec_play(ins)
+        elif c == '\x94':    exec_timer(ins)
+        elif c == '\x96':    exec_ioctl(ins)
+        elif c == '\x97':    exec_chdir(ins)
+        elif c == '\x98':    exec_mkdir(ins)
+        elif c == '\x99':    exec_rmdir(ins)
+        elif c == '\x9A':    exec_shell(ins)
+        elif c == '\x9B':    exec_environ(ins)
+        elif c == '\x9C':    exec_view(ins)
+        elif c == '\x9D':    exec_window(ins)
+        elif c == '\x9F':    exec_palette(ins)
+        elif c == '\xA0':    exec_lcopy(ins)
+        elif c == '\xA4':    exec_DEBUG(ins)
+        elif c == '\xA5':    exec_pcopy(ins)
+        elif c == '\xA7':    exec_lock(ins)
+        elif c == '\xA8':    exec_unlock(ins)
         else:
             # syntax error
             raise error.RunError(2)
     # two-byte tokens    
-    elif c=='\xFF':
+    elif c == '\xFF':
         c = ins.read(1)
-        if c == '\x83':       # MID$ statement
-            exec_mid(ins)
-        elif c == '\xA0':     # PEN statement
-            exec_pen(ins)
-        elif c == '\xA2':     # STRIG statement
-            exec_strig(ins)
+        if   c == '\x83':   exec_mid(ins)
+        elif c == '\xA0':   exec_pen(ins)
+        elif c == '\xA2':   exec_strig(ins)
         else:
-            # syntax error
             raise error.RunError(2)
     else:
-        # syntax error
         raise error.RunError(2)
     return True
 
