b3dad3296686dd480fc1509625c4b190f61509d2
Rob Hagemans <robhagemans@yahoo.co.uk>
2014-03-19 11:10:40 +0000

    consolidated backend_ansi

--- backend_ansi.py
+++ backend_ansi.py
@@ -22,19 +22,87 @@ import sys, tty, termios, select
 import os
 import sys
 
-import ansi, unicodepage
+import unicodepage
 import error
 import console
-
-palette = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
 
 term_echo_on = True
 term_attr = None
 term = sys.stdout
 
-esc_scroll_screen = '\x1b[r'
+palette = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+
+# black, blue, green, cyan, red, magenta, yellow, white
+colours = [0, 4, 2, 6, 1, 5, 3, 7]
+colournames = ['Black','Dark Blue','Dark Green','Dark Cyan','Dark Red','Dark Magenta','Brown','Light Gray',
+'Dark Gray','Blue','Green','Cyan','Red','Magenta','Yellow','White']
+
+# ANSI escape sequences
+# for reference, see:
+# http://en.wikipedia.org/wiki/ANSI_escape_code
+# http://misc.flogisoft.com/bash/tip_colors_and_formatting
+
+esc_reset = '\x1b[0m\x1bc'
+esc_set_scroll_screen = '\x1b[r'
+esc_set_scroll_region = '\x1b[%i;%ir'
 esc_clear_screen = '\x1b[2J'
 esc_clear_line = '\x1b[2K'
+esc_scroll_up = '\x1b[%iS'
+esc_scroll_down = '\x1b[%iT'
+esc_show_cursor = '\x1b[?25h'
+esc_hide_cursor = '\x1b[?25l'
+esc_resize_term = '\x1b[8;%i;%i;t'
+esc_move_cursor = '\x1b[%i;%if' 
+esc_save_cursor_pos = '\x1b[s'
+esc_restore_cursor_pos = '\x1b[u'
+
+def esc_set_cursor_colour(fore):
+    return '\x1b]12;' +colournames[fore%16] +'\x07'
+        
+def esc_set_colour(fore=None, back=None):
+    seq = ''
+    if fore != None:
+        if (fore%16)<8:
+            seq += '\x1b[%im' % (30+colours[fore%8])
+        else:
+            seq += '\x1b[%im' % (90+colours[fore%8])
+    if back != None:
+        seq += '\x1b[%im' % (40+colours[back%8])
+    return seq    
+
+def esc_set_cursor_shape(is_line=False, blinks=False):
+    return '\x1b[%i q' % (2*(is_line+1) - blinks)    # 1,2,3,4
+    
+    
+# escape sequence to scancode dictionary
+# for scan codes, see e.g. http://www.antonis.de/qbebooks/gwbasman/appendix%20h.html
+esc_to_scan = {
+    '\x1b\x4f\x50': '\x00\x3b', # F1
+    '\x1b\x4f\x51': '\x00\x3c', # F2
+    '\x1b\x4f\x52': '\x00\x3d', # F3
+    '\x1b\x4f\x53': '\x00\x3e', # F4
+    '\x1b\x5b\x31\x35\x7e':  '\x00\x3f', # F5
+    '\x1b\x5b\x31\x37\x7e':  '\x00\x40', # F6
+    '\x1b\x5b\x31\x38\x7e':  '\x00\x41', # F7
+    '\x1b\x5b\x31\x39\x7e':  '\x00\x42', # F8
+    '\x1b\x5b\x32\x30\x7e':  '\x00\x43', # F9
+    '\x1b\x5b\x32\x31\x7e':  '\x00\x44', # F10
+    '\x1b\x4f\x46': '\x00\x4F', # END
+    '\x1b\x4f\x48': '\x00\x47', # HOME
+    '\x1b\x5b\x41': '\x00\x48', # arrow up
+    '\x1b\x5b\x42': '\x00\x50', # arrow down
+    '\x1b\x5b\x43': '\x00\x4d', # arrow right
+    '\x1b\x5b\x44': '\x00\x4b', # arrow left
+    '\x1b\x5b\x32\x7e': '\x00\x52', # INS
+    '\x1b\x5b\x33\x7e': '\x00\x53', # DEL
+    '\x1b\x5b\x35\x7e': '\x00\x49', # PG UP
+    '\x1b\x5b\x36\x7e': '\x00\x51', # PG DN
+    # this is not an esc sequence, but UTF-8 for GBP symbol
+    '\xc2\xa3': '\x9c'  # pound sterling symbol
+}
+ 
+
+######
 
 def init():
     # we need raw terminal the whole time to keep control of stdin and keep it from waiting for 'enter'
@@ -46,29 +114,34 @@ def init_screen_mode(mode, new_font_height):
     
 def setup_screen(height, width):
     term.write(esc_clear_screen)
-    ansi.resize_term(height, width)   
+    term.write(esc_resize_term % (height, width))
     set_palette()
         
 def close():
     term_echo()
     build_line_cursor(True)
-    show_cursor(True, False)
+    term.write(esc_show_cursor)
     term.write(esc_clear_screen)
-    ansi.reset()
+    term.write(esc_reset)
+    term.flush()
+
+def idle():
+    time.sleep(0.024)
     
-def init_graphics():
-    pass
+######
 
 def clear_rows(bg, start, stop):
     for r in range(start, stop+1):
-        ansi.move_cursor(r,1)    
+        term.write(esc_move_cursor % (r, 1))    
         term.write(esc_clear_line)
-    ansi.move_cursor(console.row, console.col)
+    term.write(esc_move_cursor % (console.row, console.col))
     term.flush()
 
 def redraw():
    for y in range(console.height):
         console.redraw_row(0, y+1)
+
+#####
 
 def set_palette(new_palette=None):
     global palette
@@ -83,25 +156,25 @@ def set_palette_entry(index, colour):
 def get_palette_entry(index):
     return palette[index]
 
-def set_scroll_area(view_start, height, width):
+####
+
+def set_scroll_area(start, stop, width):
     pass
-    #ansi.set_scroll_region(view_start, height)    
     
 def set_cursor_colour(color):
-    ansi.set_cursor_colour(apply_palette(color))
-
-def build_line_cursor( is_line):
-    #ansi.set_cursor_shape(is_line, True)
-    pass
+    term.write(esc_set_cursor_colour(apply_palette(color)))
+    term.flush()
     
 def show_cursor(do_show, prev):
-    ansi.show_cursor(do_show)
+    term.write(esc_show_cursor if do_show else esc_hide_cursor)
+    term.flush()
 
 def check_events():
     check_keyboard()
     if console.cursor:
-        ansi.move_cursor(console.row,console.col)
-
+        term.write(esc_move_cursor % (console.row,console.col))
+        term.flush()
+        
 def apply_palette(colour):
     return colour&0x8 | palette[colour&0x7]
 
@@ -111,30 +184,33 @@ def set_attr(attr):
     if attr == last_attr:
         return
     fore, back = attr & 0xf, (attr>>4) & 0x7
-    ansi.set_colour(apply_palette(fore), apply_palette(back))
-    ansi.set_cursor_colour(apply_palette(fore))  
+    term.write(esc_set_colour(apply_palette(fore), apply_palette(back)))
+    term.write(esc_set_cursor_colour(apply_palette(fore)))
+    term.flush()  
     last_attr = attr
 
 def putc_at(row, col, c):
-    ansi.move_cursor(row, col)
+    term.write(esc_move_cursor % (row, col))
     term.write(unicodepage.to_utf8(c))
     term.flush()
    
 def scroll(from_line):
-    ansi.set_scroll_region(from_line, console.scroll_height)    
-    ansi.scroll_up(1)
-    term.write(esc_scroll_screen)
+    term.write(esc_set_scroll_region % (from_line, console.scroll_height))
+    term.write(esc_scroll_up % 1)
+    term.write(esc_set_scroll_screen)
     if console.row > 0:
-        ansi.move_cursor(console.row-1, console.col)
+        term.write(esc_move_cursor % (console.row-1, console.col))
     term.flush()
     
 def scroll_down(from_line):
-    ansi.set_scroll_region(from_line, console.scroll_height)    
-    ansi.scroll_down(1)
-    sys.stdout.write(esc_scroll_screen)
+    term.write(esc_set_scroll_region % (from_line, console.scroll_height))
+    term.write(esc_scroll_down % 1)
+    term.write(esc_set_scroll_screen)
     if console.row < console.height:
-        ansi.move_cursor(console.row+1, console.col)
-    sys.stdout.flush()
+        term.write(esc_move_cursor % (console.row+1, console.col))
+    term.flush()
+
+#######
 
 def term_echo(on=True):
     global term_attr, term_echo_on
@@ -158,18 +234,33 @@ def check_keyboard():
     if d[0] != []:
         c = os.read(fd,5)
     # handle key
-    if c == '\x03': # ctrl-C
+    if c == '':
+        pass
+    elif c == '\x03': # ctrl-C
         raise error.Break() 
-    # move scancodes (starting \x00) or ascii into keybuf
-    # also apply KEY replacements as they work at a low level
-    console.insert_key(ansi.translate_char(c))
+    elif c == '\x00':      # to avoid confusion with scancodes
+        console.insert_key('\x00\x00')      
+    elif c == '\x7f':      # backspace
+        console.insert_key('\x08')
+    else:
+        try:
+            console.insert_key(esc_to_scan[c])       
+        except KeyError:
+            # all other codes are chopped off, 
+            # so other escape sequences will register as an escape keypress.
+            console.insert_key(c[0])    
+
+########
 
 def copy_page(src, dst):
     pass
-    
-def idle():
-    time.sleep(0.024)
         
+def build_line_cursor( is_line):
+    # works on xterm, not on xfce
+    # on xfce, gibberish is printed
+    #term.write(esc_set_cursor_shape(is_line, blink=True))
+    pass
+    
 def build_shape_cursor(from_line, to_line):
     pass
 
