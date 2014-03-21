29b4ece50f09ee389ae7b85e3ddb1b0f2e29699c
Rob Hagemans <rob@wallaby.(none)>
2014-03-21 18:06:54 +0000

    stat_os, oslayer cleanup

--- oslayer.py
+++ oslayer.py
@@ -32,8 +32,6 @@ os_error = {
     errno.ENOENT: 53, errno.EISDIR: 53, errno.ENOTDIR: 53,
     # permission denied
     errno.EAGAIN: 70, errno.EACCES: 70, errno.EBUSY: 70, errno.EROFS: 70, errno.EPERM: 70,
-    # file already exists
-    errno.EEXIST: 58,
     # disk full
     errno.ENOSPC: 61, 
     # disk not ready
@@ -41,7 +39,7 @@ os_error = {
     # disk media error
     errno.EIO: 72,
     # path/file access error
-    errno.ENOTEMPTY: 75,
+    errno.EEXIST: 75, errno.ENOTEMPTY: 75,
     }
        
 
@@ -54,6 +52,70 @@ def timer_milliseconds():
     micro = diff.microseconds
     return long(seconds)*1000 + long(micro)/1000 
 
+def set_time(timestr):    
+    global time_offset
+    now = datetime.datetime.today() + time_offset
+    timelist = [0, 0, 0]
+    pos. listpos, word = 0, 0, ''
+    while pos < len(timestr):
+        if listpos > 2:
+            break
+        c = chr(timestr[pos])
+        if c in (':', '.'):
+            timelist[listpos] = int(word)
+            listpos += 1
+            word = ''
+        elif (c < '0' or c > '9'): 
+            raise error.RunError(5)
+        else:
+            word += c
+        pos += 1
+    if word:
+        timelist[listpos] = int(word)     
+    if timelist[0] > 23 or timelist[1] > 59 or timelist[2] > 59:
+        raise error.RunError(5)
+    newtime = datetime.datetime(now.year, now.month, now.day, timelist[0], timelist[1], timelist[2], now.microsecond)
+    time_offset += newtime - now    
+        
+def set_date(datestr):    
+    global time_offset
+    now = datetime.datetime.today() + time_offset
+    datelist = [1, 1, 1]
+    pos, listpos, word = 0, 0, ''
+    if len(datestr) < 8:
+        raise error.RunError(5)
+    while pos < len(datestr):
+        if listpos > 2:
+            break
+        c = chr(datestr[pos])
+        if c in ('-', '/'):
+            datelist[listpos] = int(word)
+            listpos += 1
+            word = ''
+        elif (c < '0' or c > '9'): 
+            if listpos == 2:
+                break
+            else:
+                raise error.RunError(5)
+        else:
+            word += c
+        pos += 1
+    if word:
+        datelist[listpos] = int(word)     
+    if (datelist[0] > 12 or datelist[1] > 31 or
+            (datelist[2] > 77 and datelist[2] < 80) or 
+            (datelist[2] > 99 and datelist[2] < 1980 or datelist[2] > 2099)):
+        raise error.RunError(5)
+    if datelist[2] <= 77:
+        datelist[2] = 2000 + datelist[2]
+    elif datelist[2] < 100 and datelist[2] > 79:
+        datelist[2] = 1900 + datelist[2]
+    try:
+        newtime = datetime.datetime(datelist[2], datelist[0], datelist[1], now.hour, now.minute, now.second, now.microsecond)
+    except ValueError:
+        raise error.RunError(5)
+    time_offset += newtime - now    
+    
 def safe_open(name, mode, access):
     name = str(name)
     posix_access = access_access[access] if (access and mode == 'R') else access_modes[mode]  
@@ -89,6 +151,8 @@ def istype(name, isdir):
     #        # 53: file not found
     #        raise error.RunError(errdots)
 def dosname_write(s, defext='BAS', path='', dummy=0, isdir_dummy=False):
+    if not s:
+        raise error.RunError(64)
     pre = str(path)
     if path and pre[-1] != os.sep:
         pre += os.sep
@@ -108,6 +172,8 @@ def dosname_write(s, defext='BAS', path='', dummy=0, isdir_dummy=False):
 
 # if name does not exist, put name in 8x3, all upper-case format with standard extension            
 def dosname_read(s, defext='BAS', path='', err=53, isdir=False):
+    if not s:
+        raise error.RunError(64)
     pre = str(path)
     if path and pre[-1] != os.sep:
         pre += os.sep
--- stat_os.py
+++ stat_os.py
@@ -10,7 +10,6 @@
 #
 
 import os
-import datetime
 
 import error
 import vartypes
@@ -22,25 +21,13 @@ import fileio
 
 def exec_chdir(ins):
     name = vartypes.pass_string_unpack(expressions.parse_expression(ins))
-    if name == '':
-        raise error.RunError(64)
     name = oslayer.dospath_read_dir(name, '', 76)
     oslayer.safe(os.chdir, str(name))
     util.require(ins, util.end_statement)
 
 def exec_mkdir(ins):
     name = vartypes.pass_string_unpack(expressions.parse_expression(ins))
-    if name == '':
-        raise error.RunError(64)
-    try:
-        oslayer.safe(os.mkdir, str(oslayer.dospath_write_dir(name,'', 76)))
-    except error.RunError as e:
-        # file already exists
-        if e.err == 58:
-            # path/file access error
-            raise error.RunError(75)
-        else:
-            raise e    
+    oslayer.safe(os.mkdir, str(oslayer.dospath_write_dir(name,'', 76)))
     util.require(ins, util.end_statement)
 
 def exec_rmdir(ins):
@@ -50,16 +37,14 @@ def exec_rmdir(ins):
     util.require(ins, util.end_statement)
 
 def exec_name(ins):
-    oldname = vartypes.pass_string_unpack(expressions.parse_expression(ins))
-    oldname = oslayer.dospath_read(oldname, '', 53)
+    oldname = oslayer.dospath_read(vartypes.pass_string_unpack(expressions.parse_expression(ins)), '', 53)
     # don't rename open files
     fileio.check_file_not_open(oldname)
     # AS is not a tokenised word
     word = util.skip_white_read(ins) + ins.read(1)
     if word.upper() != 'AS':
         raise error.RunError(2)
-    newname = vartypes.pass_string_unpack(expressions.parse_expression(ins))
-    newname = oslayer.dospath_write(newname, '', 76)
+    newname = oslayer.dospath_write(vartypes.pass_string_unpack(expressions.parse_expression(ins)), '', 76)
     if os.path.exists(str(newname)):
         # file already exists
         raise error.RunError(58)
@@ -109,29 +94,8 @@ def exec_time(ins):
     # allowed formats:  hh   hh:mm   hh:mm:ss  where hh 0-23, mm 0-59, ss 0-59
     timestr = vartypes.pass_string_unpack(expressions.parse_expression(ins))
     util.require(ins, util.end_statement)
-    now = datetime.datetime.today() + oslayer.time_offset
-    timelist = [0, 0, 0]
-    pos. listpos, word = 0, 0, ''
-    while pos < len(timestr):
-        if listpos > 2:
-            break
-        c = chr(timestr[pos])
-        if c in (':', '.'):
-            timelist[listpos] = int(word)
-            listpos += 1
-            word = ''
-        elif (c < '0' or c > '9'): 
-            raise error.RunError(5)
-        else:
-            word += c
-        pos += 1
-    if word:
-        timelist[listpos] = int(word)     
-    if timelist[0] > 23 or timelist[1] > 59 or timelist[2] > 59:
-        raise error.RunError(5)
-    newtime = datetime.datetime(now.year, now.month, now.day, timelist[0], timelist[1], timelist[2], now.microsecond)
-    oslayer.time_offset += newtime - now    
-        
+    oslayer.set_time(timestr)
+
 def exec_date(ins):
     util.require_read(ins, ('\xE7',)) # date$=
     # allowed formats:
@@ -139,40 +103,5 @@ def exec_date(ins):
     # mm/dd/yyyy  or mm-dd-yyyy  yyyy 1980--2099
     datestr = vartypes.pass_string_unpack(expressions.parse_expression(ins))
     util.require(ins, util.end_statement)
-    now = datetime.datetime.today() + oslayer.time_offset
-    datelist = [1, 1, 1]
-    pos, listpos, word = 0, 0, ''
-    if len(datestr) < 8:
-        raise error.RunError(5)
-    while pos < len(datestr):
-        if listpos > 2:
-            break
-        c = chr(datestr[pos])
-        if c in ('-', '/'):
-            datelist[listpos] = int(word)
-            listpos += 1
-            word = ''
-        elif (c < '0' or c > '9'): 
-            if listpos == 2:
-                break
-            else:
-                raise error.RunError(5)
-        else:
-            word += c
-        pos += 1
-    if word:
-        datelist[listpos] = int(word)     
-    if (datelist[0] > 12 or datelist[1] > 31 or
-            (datelist[2] > 77 and datelist[2] < 80) or 
-            (datelist[2] > 99 and datelist[2] < 1980 or datelist[2] > 2099)):
-        raise error.RunError(5)
-    if datelist[2] <= 77:
-        datelist[2] = 2000 + datelist[2]
-    elif datelist[2] < 100 and datelist[2] > 79:
-        datelist[2] = 1900 + datelist[2]
-    try:
-        newtime = datetime.datetime(datelist[2], datelist[0], datelist[1], now.hour, now.minute, now.second, now.microsecond)
-    except ValueError:
-        raise error.RunError(5)
-    oslayer.time_offset += newtime - now    
+    oslayer.set_date(datestr)
     
