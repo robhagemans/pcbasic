888e9fdf2d1d6969ab648a3a8f20d676f788a9eb
Rob Hagemans <robhagemans@yahoo.co.uk>
2014-03-22 22:31:34 +0000

    adjusted music queue length counting to new implementation. Added wait loops to nosound - play queue and events could now work with nosound

--- nosound.py
+++ nosound.py
@@ -9,26 +9,41 @@
 # please see text file COPYING for licence terms.
 #
 
+import oslayer
+import console
+
 music_foreground = True
+music_queue = []
 
 def music_queue_length():
-    return 0       
+    # top of sound_queue is currently playing
+    return max(0, len(music_queue)-1)
     
 def beep():
-    pass
+    play_sound(800, 0.25)
     
 def init_sound():
     return True
     
 def stop_all_sound():
-    pass
+    music_queue = []
     
 def play_sound(frequency, duration, fill=1, loop=False):
-    pass
+    if music_queue:
+        latest = max(music_queue)
+    else:    
+        latest = oslayer.timer_milliseconds()
+    wait_music(15)    
+    music_queue.append(latest + duration*1000)
         
 def check_sound():
-    pass
+    now = oslayer.timer_milliseconds()
+    while music_queue and now >= music_queue[0]:
+        music_queue.pop(0)
     
 def wait_music(wait_length=0, wait_last=True):
-    pass
+    while len(music_queue) + wait_last - 1 > wait_length:
+        console.idle()
+        console.check_events()
+        check_sound()
       
