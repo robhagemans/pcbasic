ea939ca47ff264c58b83e0225d171b21596c467e
Rob Hagemans <robhagemans@yahoo.co.uk>
2014-03-22 21:51:00 +0000

    implemented indefinite sound loop on SOUND nn, 0.02

--- nosound.py
+++ nosound.py
@@ -23,7 +23,7 @@ def init_sound():
 def stop_all_sound():
     pass
     
-def play_sound(frequency, duration, fill=1):
+def play_sound(frequency, duration, fill=1, loop=False):
     pass
         
 def check_sound():
--- stat_sound.py
+++ stat_sound.py
@@ -9,6 +9,7 @@
 # please see text file COPYING for licence terms.
 #
 
+import fp
 import vartypes
 import util
 import expressions
@@ -26,15 +27,23 @@ def exec_beep(ins):
 def exec_sound(ins):
     freq = vartypes.pass_int_unpack(expressions.parse_expression(ins))
     util.require_read(ins, (',',))
-    dur = vartypes.pass_int_unpack(expressions.parse_expression(ins), maxint=65535)
+    dur = fp.unpack(vartypes.pass_single_keep(expressions.parse_expression(ins)))
+    if fp.Single.from_int(-65535).gt(dur) or dur.gt(fp.Single.from_int(65535)):
+        raise error.RunError(5)
     util.require(ins, util.end_statement)
-    if freq == 0:
+    if dur.is_zero():
         console.sound.stop_all_sound()
         return
     util.range_check(37, 32767, freq) # 32767 is pause
-    console.sound.play_sound(freq, float(dur)/18.2)
-    if console.sound.music_foreground:
-        console.sound.wait_music(wait_last=False)
+    one_over_44 = fp.Single.from_bytes(bytearray('\x8c\x2e\x3a\x7b')) # 1/44 = 0.02272727248
+    dur_sec = dur.to_value()/18.2
+    if one_over_44.gt(dur):
+        # play indefinitely in background
+        console.sound.play_sound(freq, dur_sec, loop=True)
+    else:
+        console.sound.play_sound(freq, dur_sec)
+        if console.sound.music_foreground:
+            console.sound.wait_music(wait_last=False)
     
 def exec_play(ins):
     if events.play_handler.command(util.skip_white(ins)):
