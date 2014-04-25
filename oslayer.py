acc401b8eaa7ddb5b2d12ec53d2b7d869aeb5b51
Rob Hagemans <rob@wallaby.(none)>
2014-04-25 18:35:46 +0100

    moved platform detection into plat.py, reorganise oslayer

--- oslayer.py
+++ oslayer.py
@@ -18,7 +18,27 @@ from functools import partial
 import StringIO
 
 import error
+import console
 import unicodepage
+import plat
+
+if plat.system == 'Windows':
+    import msvcrt
+    import win32ui
+    import win32gui
+    import win32api
+    import win32con
+    import win32ui
+    import win32print
+    import subprocess
+    import threading
+else:
+    try:
+        import pexpect
+    except Exception:
+        import logging
+        logging.warning('Pexpect module not found. SHELL command will not work.')    
+    
 
 # datetime offset for duration of the run (so that we don't need permission to touch the system clock)
 # given in seconds        
@@ -175,6 +195,72 @@ def handle_oserror(e):
         
 #########################################
 # drives & paths
+
+pcbasic_dir = os.path.dirname(os.path.realpath(__file__))
+
+
+if plat.system == 'Windows':
+    drives = { '@': os.path.join(pcbasic_dir, 'info') }
+    current_drive = os.path.abspath(os.getcwd()).split(':')[0]
+    drive_cwd = { '@': '' }
+
+    # get all drives in use by windows
+    # if started from CMD.EXE, get the 'current wworking dir' for each drive
+    # if not in CMD.EXE, there's only one cwd
+    save_current = os.getcwd()
+    for letter in win32api.GetLogicalDriveStrings().split(':\\\x00')[:-1]:
+        try:
+            os.chdir(letter + ':')
+            cwd = win32api.GetShortPathName(os.getcwd())
+            # must not start with \\
+            drive_cwd[letter] = cwd[3:]  
+            drives[letter] = cwd[:3]
+        except WindowsError:
+            pass    
+    os.chdir(save_current)    
+
+    # get windows short name
+    def dossify(path, name):
+        if not path:
+            path = current_drive
+        try:
+            shortname = win32api.GetShortPathName(os.path.join(path, name)).upper()
+        except Exception as e:
+            # something went wrong, show as dots in FILES
+            return "........", "..."
+        split = shortname.split('\\')[-1].split('.')
+        trunk, ext = split[0], ''
+        if len(split)>1:
+            ext = split[1]
+        if len(trunk)>8 or len(ext)>3:
+            # on some file systems, ShortPathName returns the long name
+            trunk = trunk[:8]
+            ext = '...'    
+        return trunk, ext    
+
+else:
+    drives = { 'C': '/', '@': os.path.join(pcbasic_dir, 'info') }
+    current_drive = 'C'
+    # must not start with a /
+    drive_cwd = { 'C': os.getcwd()[1:], '@': '' }
+    
+    if plat.system == 'Android':
+        drives['C'] = os.path.join(pcbasic_dir, 'files')
+        drive_cwd['C'] =''
+
+    # change names in FILES to some 8.3 variant             
+    def dossify(path, name):
+        if name.find('.') > -1:
+            trunk, ext = name[:name.find('.')][:8], name[name.find('.')+1:][:3]
+        else:
+            trunk, ext = name[:8], ''
+        # non-DOSnames passed as UnixName....    
+        if (ext and name != trunk+'.'+ext) or (ext == '' and name != trunk and name != '.'):
+            ext = '...'
+        if name in ('.', '..'):
+            trunk, ext = '', ''
+        return trunk, ext
+
 
 def istype(path, name, isdir):
     name = os.path.join(str(path), str(name))
@@ -355,7 +441,126 @@ def rename(oldname, newname):
         raise error.RunError(58)
     safe(os.rename, oldname, newname)
         
-    
+###################################################
+# FILES: disk_free
+
+if plat.system == 'Windows':
+    import ctypes
+    def disk_free(path):
+        free_bytes = ctypes.c_ulonglong(0)
+        ctypes.windll.kernel32.GetDiskFreeSpaceExW(ctypes.c_wchar_p(path), None, None, ctypes.pointer(free_bytes))
+        return free_bytes.value
+
+elif plat.system == 'Android':
+    def disk_free(path):
+        # TODO: implement with jnius
+        return 0        
+
+else:
+    def disk_free(path):
+        st = os.statvfs(path)
+        return st.f_bavail * st.f_frsize
+        
+###################################################
+# SHELL
+
+if plat.system == 'Windows':
+    shell_output = ''   
+
+    def process_stdout(p, stream):
+        global shell_output
+        while True:
+            c = stream.read(1)
+            if c != '': 
+                # don't access screen in this thread, the other thread already does
+                shell_output += c
+            elif p.poll() != None:
+                break        
+            else:
+                # don't hog cpu
+                console.idle()
+
+    def shell(command):
+        global shell_output
+        if not command:
+            command = 'CMD'
+        p = subprocess.Popen( str(command).split(), stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True )
+        outp = threading.Thread(target=process_stdout, args=(p, p.stdout))
+        outp.daemon = True
+        outp.start()
+        errp = threading.Thread(target=process_stdout, args=(p, p.stderr))
+        errp.daemon = True
+        errp.start()
+        word = ''
+        while p.poll() == None or shell_output:
+            if shell_output:
+                lines = shell_output.split('\r\n')
+                shell_output = '' 
+                last = lines.pop()
+                for line in lines:
+                    console.check_events()
+                    console.write_line(line)
+                console.write(last)    
+            if p.poll() != None:
+                # drain output then break
+                continue    
+            c = console.get_char()
+            if c in ('\r', '\n'): 
+                # Windows CMD.EXE echo to overwrite the command that's already there
+                # NOTE: WINE cmd.exe doesn't echo the command, so it's overwritten by the output...
+                console.write('\x1D' * len(word))
+                p.stdin.write(word + '\r\n')
+                word = ''
+            elif c == '\b':
+                # handle backspace
+                if word:
+                    word = word[:-1]
+                    console.write('\x1D \x1D')
+            elif c != '':    
+                # only send to pipe when enter pressed rather than p.stdin.write(c)
+                # workaround for WINE - it seems to attach a CR to each letter sent to the pipe. not needed in proper Windows.
+                # also needed to handle backsapce properly
+                word += c
+                console.write(c)
+        outp.join()
+        errp.join()
+
+else:
+    def shell(command):
+        cmd = '/bin/sh'
+        if command:
+            cmd += ' -c "' + command + '"'            
+        try:
+            p = pexpect.spawn(str(cmd))
+        except Exception:
+            return 
+        while True:
+            c = console.get_char()
+            if c == '\b': # BACKSPACE
+                p.send('\x7f')
+            elif c != '':
+                p.send(c)
+            while True:
+                try:
+                    c = p.read_nonblocking(1, timeout=0)
+                except: 
+                    c = ''
+                if c == '' or c == '\n':
+                    break
+                elif c == '\r':
+                    console.write_line()    
+                elif c == '\b':
+                    if console.col != 1:
+                        console.col -= 1
+                else:
+                    console.write(c)
+            if c == '' and not p.isalive(): 
+                return
+
+
+###################################################
+# printing
+
 # print to CUPS or windows printer    
 class CUPSStream(StringIO.StringIO):
     def __init__(self, printer_name=''):
@@ -376,11 +581,46 @@ class CUPSStream(StringIO.StringIO):
             utf8buf += unicodepage.cp_to_utf8[printbuf]
         line_print(utf8buf, self.printer_name)
 
-        
-# platform-specific:
-import platform
-if platform.system() == 'Windows':
-    from os_windows import *
-else:    
-    from os_unix import *
+if plat.system == 'Windows':
+    # print to Windows printer
+    def line_print(printbuf, printer_name):        
+        if printer_name == '' or printer_name=='default':
+            printer_name = win32print.GetDefaultPrinter()
+        handle = win32ui.CreateDC()
+        handle.CreatePrinterDC(printer_name)
+        handle.StartDoc("PC-BASIC 3_23 Document")
+        handle.StartPage()
+        # a4 = 210x297mm = 4950x7001px; Letter = 216x280mm=5091x6600px; 
+        # 65 tall, 100 wide with 50x50 margins works for US letter
+        # 96 wide works for A4 with 75 x-margin
+        y, yinc = 50, 100
+        lines = printbuf.split('\r\n')
+        slines = []
+        for l in lines:
+            slines += [l[i:i+96] for i in range(0, len(l), 96)]
+        for line in slines:
+            handle.TextOut(75, y, line) 
+            y += yinc
+            if y > 6500:  
+                y = 50
+                handle.EndPage()
+                handle.StartPage()
+        handle.EndPage()
+        handle.EndDoc()       
+
+elif plat.system == 'Android':
+    def line_print(printbuf, printer_name):
+        # printing not supported on Android
+        pass          
+
+else:
+    # print to LPR printer (ok for CUPS)
+    def line_print(printbuf, printer_name): 
+        options = ''
+        if printer_name != '' and printer_name != 'default':
+            options += ' -P ' + printer_name
+        if printbuf != '':
+            pr = os.popen("lpr " + options, "w")
+            pr.write(printbuf)
+            pr.close()
 
