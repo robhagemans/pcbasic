83862869aed7f29a882f1a51d438dd9ab4c1fd78
Rob Hagemans <rob@wallaby.(none)>
2014-04-27 19:44:37 +0100

    merged automode.py into run.py

--- run.py
+++ run.py
@@ -26,6 +26,8 @@ import state
 
 # suppress one prompt by setting to False (used by EDIT)
 state.basic_state.prompt = True
+# AUTO mode    
+state.basic_state.auto_mode = False
 
 def loop():
     # main loop
@@ -81,7 +83,50 @@ def execution_loop():
                 console.show_cursor()
                 raise e
     console.show_cursor()
-                   
-def exit():
-    raise error.Exit
-    
+
+def auto_loop(new_linenum, new_increment):
+    # don't nest, but reset linenum and increment
+    state.basic_state.auto_linenum = new_linenum if new_linenum != None else 10
+    state.basic_state.auto_increment = new_increment if new_increment != None else 10    
+    if not state.basic_state.auto_mode:
+        state.basic_state.auto_mode = True   
+        while True:
+            numstr = str(state.basic_state.auto_linenum)
+            console.write(numstr)
+            try:
+                if state.basic_state.auto_linenum in state.basic_state.line_numbers:
+                    console.write('*')
+                    line = console.wait_screenline(from_start=True)
+                    if line[:len(numstr)+1] == numstr+'*':
+                        line[len(numstr)] = ' '
+                else:
+                    console.write(' ')
+                    line = console.wait_screenline(from_start=True)
+            except error.Break:
+                # exit auto mode
+                break
+            while len(line) > 0 and line[-1] in util.whitespace:
+                line = line[:-1]
+            # run or store it; don't clear lines or raise undefined line number
+            try:
+                state.basic_state.direct_line = tokenise.tokenise_line(line)    
+                c = util.peek(state.basic_state.direct_line)
+                if c == '\x00':
+                    # check for lines starting with numbers (6553 6) and empty lines
+                    empty, scanline = program.check_number_start(state.basic_state.direct_line)
+                    if not empty:
+                        program.store_line(state.basic_state.direct_line)
+                    state.basic_state.auto_linenum = scanline + state.basic_state.auto_increment
+                elif c != '':    
+                    # it is a command, go and execute    
+                    run.execution_loop()
+            except error.Break as e:
+                e.handle_break() 
+                run.show_prompt()
+            except error.RunError as e:
+                e.handle_break()             
+                run.show_prompt()
+        state.basic_state.auto_mode = False
+        program.set_runmode(False)
+
+        
