47e7cbf5212a24df0c7bc46a2620ca3d2f6b3d79
Rob Hagemans <robhagemans@yahoo.co.uk>
2014-05-04 11:20:31 +0100

    allow storing state of files and devices

--- state.py
+++ state.py
@@ -53,7 +53,7 @@ try:
 except ImportError:
     from StringIO import StringIO
 
-class cStringIO_Pickler(object):
+class cStringIOPicklable(object):
     def __init__(self, csio):
         self.value = csio.getvalue()
         self.pos = csio.tell()
@@ -65,6 +65,64 @@ class cStringIO_Pickler(object):
         csio.seek(self.pos)
         return csio             
 
+class FilePicklable(object):
+    def __init__(self, f):
+        self.pos = f.tell()
+        self.name = f.name
+        self.mode = f.mode
+
+    def unpickle(self):
+        if 'w' in self.mode:
+            # preserve existing contents of writable file
+            f = open(self.name, 'rb')
+            buf = f.read(self.pos)
+            f.close()
+            f = open(self.name, self.mode)
+            f.write(buf)            
+        else:    
+            f = open(self.name, self.mode)
+            f.seek(self.pos)
+        return f
+    
+class FileDictPicklable(object):
+    def __init__(self, dict_obj):
+        self.dict = {}
+        for key, value in dict_obj.items():
+            picklable_value = copy.copy(value)
+            try:
+                picklable_value.fhandle = get_picklable(picklable_value.fhandle)
+            except AttributeError:
+                pass
+            self.dict[key] = picklable_value
+
+    def unpickle(self):
+        unpickled_dict = {}
+        for key, value in self.dict.items():
+            try:
+                fhandle = value.fhandle
+                try:
+                    value.fhandle = fhandle.unpickle()
+                except AttributeError:
+                    pass
+                except IOError:
+                    logging.warning('Could not reopen file %s', fhandle.name)
+                    continue
+            except AttributeError:
+                pass
+            unpickled_dict[key] = value
+        return unpickled_dict
+    
+    
+picklables = { "<type 'cStringIO.StringO'>": cStringIOPicklable, "<type 'file'>": FilePicklable, "<type 'dict'>": FileDictPicklable }
+
+def get_picklable(obj):
+    try:
+        return picklables[str(type(obj))](obj)
+    except KeyError:
+        return obj
+
+    
+
 ###############################################
 
 def save():
@@ -72,13 +130,12 @@ def save():
     to_pickle = State()
     # BASIC state
     to_pickle.basic = copy.copy(basic_state)
-    to_pickle.basic.bytecode = cStringIO_Pickler(basic_state.bytecode)
-    to_pickle.basic.direct_line = cStringIO_Pickler(basic_state.direct_line)
+    to_pickle.basic.bytecode = get_picklable(basic_state.bytecode)
+    to_pickle.basic.direct_line = get_picklable(basic_state.direct_line)
     # I/O
     to_pickle.io = copy.copy(io_state)
-    to_pickle.io.files = {}
-    to_pickle.io.devices = {}
-    
+    to_pickle.io.files = get_picklable(to_pickle.io.files)
+    to_pickle.io.devices = get_picklable(to_pickle.io.devices)
     # Console
     to_pickle.console = copy.copy(console_state)
     # Display 
@@ -108,6 +165,8 @@ def load():
     io_state, basic_state, console_state = from_pickle.io, from_pickle.basic, from_pickle.console
     basic_state.bytecode = basic_state.bytecode.unpickle()
     basic_state.direct_line = basic_state.direct_line.unpickle()
+    io_state.devices = io_state.devices.unpickle()
+    io_state.files = io_state.files.unpickle()
     from_pickle.display.unpickle()
     loaded = True
     return True
