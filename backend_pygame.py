ba1725aca6edbdc1cc79201008be966541fc42d7
Rob Hagemans <rob@wallaby.(none)>
2014-05-09 19:33:16 +0100

    implemented alt+keypad ascii entry

--- backend_pygame.py
+++ backend_pygame.py
@@ -206,6 +206,11 @@ if pygame:
         pygame.K_DELETE:    '\x53',        pygame.K_BACKSLASH: '\x56',
     }
 
+    keypad_numbers = {
+        pygame.K_KP0:   '0',    pygame.K_KP1:   '1',    pygame.K_KP2:   '2',    pygame.K_KP3:   '3',    pygame.K_KP4:   '4',
+        pygame.K_KP5:   '5',    pygame.K_KP6:   '6',    pygame.K_KP7:   '7',    pygame.K_KP8:   '8',    pygame.K_KP9:   '9',
+    }
+    
 ####################################            
 # set constants based on commandline arguments
 
@@ -648,7 +653,10 @@ def do_flip():
         pygame.transform.scale(screen, display.get_size(), display)  
     pygame.display.flip()
 
+keypad_ascii = ''
+
 def handle_key(e):
+    global keypad_ascii
     c = ''
     mods = pygame.key.get_mods()
     if android:
@@ -685,7 +693,11 @@ def handle_key(e):
             if (mods & pygame.KMOD_CTRL):
                 c = ctrl_keycode_to_scancode[e.key]
             elif (mods & pygame.KMOD_ALT):
-                c = alt_keycode_to_scancode[e.key]
+                try:
+                    keypad_ascii += keypad_numbers[e.key]
+                    return
+                except KeyError:    
+                    c = alt_keycode_to_scancode[e.key]
             else:
                 c = keycode_to_scancode[e.key]
         except KeyError:
@@ -702,11 +714,19 @@ def handle_key(e):
         pass    
                     
 def handle_key_up(e):
+    global keypad_ascii
     # last key released gets remembered
     try:
         state.console_state.inp_key = 0x80 + ord(keycode_to_inpcode[e.key])
     except KeyError:
-        pass    
+        pass 
+    # ALT+keycode    
+    if e.key in (pygame.K_RALT, pygame.K_LALT) and keypad_ascii:
+        char = chr(int(keypad_ascii)%256)
+        if char == '\0':
+            char = '\0\0'
+        console.insert_key(char)
+        keypad_ascii = ''
            
 def handle_mouse(e):
     if e.button == 1: # LEFT BUTTON
