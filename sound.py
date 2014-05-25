d6143e699a41fc80687b679567bef2a74b13d108
Rob Hagemans <robhagemans@yahoo.co.uk>
2014-05-25 09:11:25 +0100

    SOUND ON implementation; start of 3-voice implementation in sound.py

--- sound.py
+++ sound.py
@@ -13,26 +13,28 @@ import state
 import backend
 
 state.console_state.music_foreground = True
-state.console_state.music_queue = []
+state.console_state.music_queue = [[], [], [], []]
+state.console_state.sound_on = False
 
 def init_sound():
     if not backend.sound.init_sound():
         return False
-    # rebuild sound queue    
-    for note in state.console_state.music_queue:
-        backend.sound.play_sound(*note)
+    # rebuild sound queue
+    for voice in range(4):    
+        for note in state.console_state.music_queue[voice]:
+            backend.sound.play_sound(*note)
     return True
     
-def music_queue_length():
+def music_queue_length(voice=0):
     # top of sound_queue is currently playing
-    return max(0, len(state.console_state.music_queue)-1)
+    return max(0, len(state.console_state.music_queue[voice])-1)
     
 def beep():
     play_sound(800, 0.25)
 
-def play_sound(frequency, duration, fill=1, loop=False):
-    state.console_state.music_queue.append((frequency, duration, fill, loop))
-    backend.sound.play_sound(frequency, duration, fill, loop) 
+def play_sound(frequency, duration, fill=1, loop=False, voice=0, volume=15):
+    state.console_state.music_queue[voice].append((frequency, duration, fill, loop, volume))
+    backend.sound.play_sound(frequency, duration, fill, loop, voice, volume) 
     # at most 16 notes in the sound queue (not 32 as the guide says!)
     wait_music(15, wait_last=False)    
 
