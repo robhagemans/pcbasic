886b9564d58356067402fd1a232e782aab2ad1bb
Rob Hagemans <rob@wallaby.(none)>
2014-05-25 10:12:57 +0100

    implemented PCjr and Tandy SOUND syntax with voice and volume

--- statements.py
+++ statements.py
@@ -465,18 +465,31 @@ def exec_sound(ins):
     dur = fp.unpack(vartypes.pass_single_keep(expressions.parse_expression(ins)))
     if fp.Single.from_int(-65535).gt(dur) or dur.gt(fp.Single.from_int(65535)):
         raise error.RunError(5)
+    if state.basic_state.machine in ('pcjr', 'tandy') and util.skip_white_read_if(ins, (',',)):
+        volume = vartypes.pass_int_unpack(expressions.parse_expression(ins))
+        util.range_check(0, 15, volume)        
+        if util.skip_white_read_if(ins, (',',)):
+            voice = vartypes.pass_int_unpack(expressions.parse_expression(ins))
+            util.range_check(0, 2, voice) # can't address noise channel here
+        else:
+            voice = 0    
+    else:
+        volume, voice = 15, 0                
     util.require(ins, util.end_statement)
     if dur.is_zero():
         sound.stop_all_sound()
         return
-    util.range_check(37, 32767, freq) # 32767 is pause
+    if state.basic_state.machine in ('pcjr', 'tandy'):
+        util.range_check(1, 32767, freq) 
+    else:    
+        util.range_check(37, 32767, freq) # 32767 is pause
     one_over_44 = fp.Single.from_bytes(bytearray('\x8c\x2e\x3a\x7b')) # 1/44 = 0.02272727248
     dur_sec = dur.to_value()/18.2
     if one_over_44.gt(dur):
         # play indefinitely in background
-        sound.play_sound(freq, dur_sec, loop=True)
+        sound.play_sound(freq, dur_sec, loop=True, voice=voice, volume=volume)
     else:
-        sound.play_sound(freq, dur_sec)
+        sound.play_sound(freq, dur_sec, voice=voice, volume=volume)
         if state.console_state.music_foreground:
             sound.wait_music(wait_last=False)
     
