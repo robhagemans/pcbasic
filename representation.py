8c55dc6bebd408b71b8e3a370af87d62af7a80f6
Rob Hagemans <rob@wallaby.(none)>
2014-08-17 09:36:03 +0100

    fix VAL() returning zero for leading whitespaces and LFs

--- representation.py
+++ representation.py
@@ -676,11 +676,13 @@ def tokenise_number(ins, outs):
 ##########################################
 
 def str_to_value_keep(strval, allow_nonnum=True):
-    if strval==('$',''):
+    if strval == ('$', ''):
         return vartypes.null['%']
     strval = vartypes.pass_string_unpack(strval)
     ins = StringIO(strval)
     outs = StringIO()
+    # skip spaces and line feeds (but not NUL).
+    util.skip(ins, (' ', '\n'))
     tokenise_number(ins, outs)    
     outs.seek(0)
     value = util.parse_value(outs)
