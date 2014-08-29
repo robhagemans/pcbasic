c60a54a4558a70bc346eba7a59b938a4f59091a1
Rob Hagemans <robhagemans@yahoo.co.uk>
2014-08-29 12:12:53 +0100

    unicodepage.py: clear up trailing whitespace

--- unicodepage.py
+++ unicodepage.py
@@ -51,13 +51,13 @@ def load_codepage(codepage_name):
         f = open(name, 'rb')
         for line in f:
             # ignore empty lines and comment lines (first char is #)
-            if (not line) or (line[0] == '#'): 
+            if (not line) or (line[0] == '#'):
                 continue
             # split unicodepoint and hex string
-            splitline = line.split(':')    
+            splitline = line.split(':')
             # ignore malformed lines
             if len(splitline) < 2:
-                continue    
+                continue
             try:
                 # extract codepage point
                 cp_point = splitline[0].strip().decode('hex')
@@ -70,7 +70,7 @@ def load_codepage(codepage_name):
                     trail.add(cp_point[1])
                     dbcs_num_chars += 1
             except ValueError:
-                logging.warning('Could not parse line in unicode mapping table: %s', repr(line))    
+                logging.warning('Could not parse line in unicode mapping table: %s', repr(line))
     except IOError:
         if codepage_name == '437':
             logging.error('Could not load unicode mapping table for codepage 437.')
@@ -84,20 +84,20 @@ def load_codepage(codepage_name):
             cp_to_utf8[chr(c)] = '\0'
     utf8_to_cp = dict((reversed(item) for item in cp_to_utf8.items()))
     if dbcs_num_chars > 0:
-        dbcs = True    
-    return codepage_name  
+        dbcs = True
+    return codepage_name
 
-# convert utf8 wchar to codepage char        
+# convert utf8 wchar to codepage char
 def from_utf8(c):
     return utf8_to_cp[c]
 
-# line buffer for conversion to UTF8 - supports DBCS                                    
+# line buffer for conversion to UTF8 - supports DBCS
 class UTF8Converter (object):
     def __init__(self):
         self.buf = ''
 
     # add chars to buffer
-    def to_utf8(self, s, preserve_control=False):        
+    def to_utf8(self, s, preserve_control=False):
         if not dbcs:
             # stateless if not dbcs
             return ''.join([ (c if (preserve_control and c in control) else cp_to_utf8[c]) for c in s ])
@@ -105,7 +105,7 @@ class UTF8Converter (object):
             out = ''
             if self.buf:
                 # remove the naked lead-byte first
-                out += '\b'                
+                out += '\b'
             for c in s:
                 if preserve_control and c in control:
                     # control char; flush buffer as SBCS and add control char unchanged
@@ -117,19 +117,19 @@ class UTF8Converter (object):
                 elif self.buf:
                     if c in trail:
                         # add a DBCS character
-                        out += cp_to_utf8[self.buf + c] 
+                        out += cp_to_utf8[self.buf + c]
                         self.buf = ''
                         continue
                     else:
                         # flush buffer
-                        out += cp_to_utf8[self.buf] 
+                        out += cp_to_utf8[self.buf]
                         self.buf = ''
                 if c in lead:
                     self.buf = c
                 else:
                     out += cp_to_utf8[c]
-            # any naked lead-byte left will be printed        
+            # any naked lead-byte left will be printed
             if self.buf:
                 out += cp_to_utf8[self.buf]
             return out
-        
+
