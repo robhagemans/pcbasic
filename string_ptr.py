8e8066fab4cee7e0c533b3b26d9dbf1f19247823
Rob Hagemans <rob@wallaby.(none)>
2014-08-31 07:37:23 +0100

    Docstrings for StringPtr

--- string_ptr.py
+++ string_ptr.py
@@ -10,6 +10,7 @@
 #
 
 def apply_slice(slice0, slice1):
+    """ Get a slice of a slice. """
     if isinstance(slice1, slice):
         start, stop = slice0.start + slice1.start, slice0.start + slice1.stop
     else:
@@ -19,21 +20,11 @@ def apply_slice(slice0, slice1):
     return slice(start, stop) 
 
 
-# string pointer implementation, allows for unions of strings (for FIELD)
 class StringPtr:
-    def assign(self, in_str):
-        self.stream[self.slice] = in_str[len(self)] + ' '*(len(self)-len(in_str))
-    
-    def get(self):
-        return self.stream[self.slice]
-        
-    def __str__(self):
-        return str(self.stream[self.slice])
-        
-    def __len__(self):
-        return self.slice.stop - self.slice.start
+    """ String pointer implementation. Allows for unions of strings (for FIELD). """
 
     def __init__(self, stream, offset, length):
+        """ Set the buffer and where to put the string in it. Don't allocate, use the buffer provided. """
         if length < 0:
             length = 0
         if isinstance(stream, StringPtr):
@@ -51,9 +42,26 @@ class StringPtr:
         if self.slice.stop - self.slice.start > max_length:
             self.slice.stop = self.slice.start + max_length
     
+    def __len__(self):
+        """ Return the length of the string. """
+        return self.slice.stop - self.slice.start
+
     def __getitem__(self, key):
+        """ Return a copy of a range of characters from the string. """
         return self.stream[self.slice][key]
 
     def __setitem__(self, key, value):
+        """ Change a range of characters in the string. """
         self.stream[apply_slice(self.slice, key)] = value    
+            def __str__(self):
+        return str(self.stream[self.slice])
         
+    def assign(self, in_str):
+        """ Replace the string with a new value in-place. """
+        self.stream[self.slice] = in_str[len(self)] + ' '*(len(self)-len(in_str))
+    
+    def get(self):
+        """ Return a copy of the the full string value. """
+        return self.stream[self.slice]
+        
+
