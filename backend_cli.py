114afb49b645b8fc9e0659588831d19bd2ed7681
Rob Hagemans <robhagemans@yahoo.co.uk>
2014-09-03 22:23:06 +0100

    cli backend for Windows, sort of works

--- backend_cli.py
+++ backend_cli.py
@@ -20,11 +20,6 @@ import sys
 import time
 import os
 import plat
-
-if plat.system != 'Windows':
-    # this fails on Windows
-    import tty, termios, select
-
 import unicodepage
 import error
 import console
@@ -34,8 +29,6 @@ supports_graphics = False
 # palette is ignored
 max_palette = 64
 
-term_echo_on = True
-term_attr = None
 term = sys.stdout
 
 # unused, but needs to be defined
@@ -44,48 +37,89 @@ colorburst = False
 # cursor is visible
 cursor_visible = True
 
-# ANSI escape sequences
-# for reference, see:
-# http://en.wikipedia.org/wiki/ANSI_escape_code
-# http://misc.flogisoft.com/bash/tip_colors_and_formatting
-
+# ANSI escape codes for output
+# WINE handles these, does Windows?
 esc_clear_line = '\x1b[2K'
 esc_move_right = '\x1b\x5b\x43'
 esc_move_left = '\x1b\x5b\x44'
 
-# escape sequence to scancode dictionary
-# for scan codes, see e.g. http://www.antonis.de/qbebooks/gwbasman/appendix%20h.html
-esc_to_scan = {
-    '\x1b\x4f\x50': '\x00\x3b', # F1
-    '\x1b\x4f\x51': '\x00\x3c', # F2
-    '\x1b\x4f\x52': '\x00\x3d', # F3
-    '\x1b\x4f\x53': '\x00\x3e', # F4
-    '\x1b\x5b\x31\x35\x7e':  '\x00\x3f', # F5
-    '\x1b\x5b\x31\x37\x7e':  '\x00\x40', # F6
-    '\x1b\x5b\x31\x38\x7e':  '\x00\x41', # F7
-    '\x1b\x5b\x31\x39\x7e':  '\x00\x42', # F8
-    '\x1b\x5b\x32\x30\x7e':  '\x00\x43', # F9
-    '\x1b\x5b\x32\x31\x7e':  '\x00\x44', # F10
-    '\x1b\x4f\x46': '\x00\x4F', # END
-    '\x1b\x4f\x48': '', #'\x00\x47', # HOME, ignore
-    '\x1b\x5b\x41': '', #'\x00\x48', # arrow up, ignore
-    '\x1b\x5b\x42': '', #'\x00\x50', # arrow down, ignore
-    esc_move_right: '\x00\x4d', # arrow right
-    esc_move_left: '\x00\x4b', # arrow left
-    '\x1b\x5b\x32\x7e': '\x00\x52', # INS
-    '\x1b\x5b\x33\x7e': '\x00\x53', # DEL
-    '\x1b\x5b\x35\x7e': '\x00\x49', # PG UP
-    '\x1b\x5b\x36\x7e': '\x00\x51', # PG DN
-}
- 
+if plat.system == 'Windows':
+    import msvcrt
+
+    def term_echo(on=True):
+        pass
+            
+    def getc():
+        # won't work under WINE
+        if not msvcrt.kbhit():
+            return ''
+        return msvcrt.getch()    
+    
+    def replace_scancodes(s):
+        # windows scancodes should be the same as gw-basic ones
+        return s.replace('\xe0', '\0')
+        
+else:
+    import tty, termios, select
+
+    term_echo_on = True
+    term_attr = None
+
+    def term_echo(on=True):
+        global term_attr, term_echo_on
+        # sets raw terminal - no echo, by the character rather than by the line
+        fd = sys.stdin.fileno()
+        if (not on) and term_echo_on:
+            term_attr = termios.tcgetattr(fd)
+            tty.setraw(fd)
+        elif not term_echo_on and term_attr != None:
+            termios.tcsetattr(fd, termios.TCSADRAIN, term_attr)
+        previous = term_echo_on
+        term_echo_on = on    
+        return previous
+
+    # escape sequence to scancode dictionary
+    esc_to_scan = {
+        '\x1b\x4f\x50': '\x00\x3b', # F1
+        '\x1b\x4f\x51': '\x00\x3c', # F2
+        '\x1b\x4f\x52': '\x00\x3d', # F3
+        '\x1b\x4f\x53': '\x00\x3e', # F4
+        '\x1b\x5b\x31\x35\x7e':  '\x00\x3f', # F5
+        '\x1b\x5b\x31\x37\x7e':  '\x00\x40', # F6
+        '\x1b\x5b\x31\x38\x7e':  '\x00\x41', # F7
+        '\x1b\x5b\x31\x39\x7e':  '\x00\x42', # F8
+        '\x1b\x5b\x32\x30\x7e':  '\x00\x43', # F9
+        '\x1b\x5b\x32\x31\x7e':  '\x00\x44', # F10
+        '\x1b\x4f\x46': '\x00\x4F', # END
+        '\x1b\x4f\x48': '', #'\x00\x47', # HOME, ignore
+        '\x1b\x5b\x41': '', #'\x00\x48', # arrow up, ignore
+        '\x1b\x5b\x42': '', #'\x00\x50', # arrow down, ignore
+        esc_move_right: '\x00\x4d', # arrow right
+        esc_move_left: '\x00\x4b', # arrow left
+        '\x1b\x5b\x32\x7e': '\x00\x52', # INS
+        '\x1b\x5b\x33\x7e': '\x00\x53', # DEL
+        '\x1b\x5b\x35\x7e': '\x00\x49', # PG UP
+        '\x1b\x5b\x36\x7e': '\x00\x51', # PG DN
+    }
+     
+    def getc():
+        if select.select([sys.stdin], [], [], 0)[0] == []:
+            return ''
+        return os.read(sys.stdin.fileno(), 1)        
+        
+    def replace_scancodes(s):    
+        # avoid confusion of NUL with scancodes    
+        s = s.replace('\0', '\0\0')
+        # first replace escape sequences in s with scancodes
+        # this plays nice with utf8 as long as the scan codes are all in 7 bit ascii, ie no \00\f0 or above    
+        for esc in esc_to_scan:
+            s = s.replace(esc, esc_to_scan[esc])
+        return s
+
 def prepare(args):
     pass
 
 def init():
-    if plat.system == 'Windows':
-        import logging
-        logging.warning('Command-line interface not supported on Windows.\n')
-        return False
     term_echo(False)
     term.flush()
     return True
@@ -176,35 +210,20 @@ def scroll_down(from_line):
         
 #######
 
-def term_echo(on=True):
-    global term_attr, term_echo_on
-    # sets raw terminal - no echo, by the character rather than by the line
-    fd = sys.stdin.fileno()
-    if (not on) and term_echo_on:
-        term_attr = termios.tcgetattr(fd)
-        tty.setraw(fd)
-    elif not term_echo_on and term_attr != None:
-        termios.tcsetattr(fd, termios.TCSADRAIN, term_attr)
-    previous = term_echo_on
-    term_echo_on = on    
-    return previous
 
 def check_keyboard():
     global pre_buffer
-    fd = sys.stdin.fileno()
     s = ''
     # drain input buffer of all charaters available
     while True:
+        c = getc()
         # break if stdin has no more characters to read
-        if select.select([sys.stdin], [], [], 0)[0] == []:
+        if c == '':
             break
-        s += os.read(fd, 1)
-    # avoid confusion of NUL with scancodes    
-    s.replace('\0', '\0\0')
-    # first replace escape sequences in s with scancodes
-    # this plays nice with utf8 as long as the scan codes are all in 7 bit ascii, ie no \00\f0 or above    
-    for esc in esc_to_scan:
-        s = s.replace(esc, esc_to_scan[esc])
+        s += c    
+    if s == '':    
+        return
+    s = replace_scancodes(s)
     # replace utf-8 with codepage
     # convert into unicode codepoints
     u = s.decode('utf-8')
