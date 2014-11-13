ce8bf49f00b3c03ff8a4dd921149f0d94bec5a8e
Rob Hagemans <robhagemans@yahoo.co.uk>
2014-11-13 22:14:43 +0000

    config: adjust head docstring

--- config.py
+++ config.py
@@ -1,13 +1,10 @@
-#
-# PC-BASIC 3.23 
-#
-# Configuration file and command-line options parser
-# 
-# (c) 2013, 2014 Rob Hagemans 
-#
-# This file is released under the GNU GPL version 3. 
-# please see text file COPYING for licence terms.
-#
+"""
+PC-BASIC 3.23 
+Configuration file and command-line options parser
+ 
+(c) 2013, 2014 Rob Hagemans 
+This file is released under the GNU GPL version 3. 
+"""
 
 import os
 import sys
