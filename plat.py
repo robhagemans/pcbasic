58a268d0ae70d488a3ca1bb16aa80940527cf5ef
Rob Hagemans <robhagemans@yahoo.co.uk>
2014-11-29 20:06:06 +0000

    plat.py: head docstring

--- plat.py
+++ plat.py
@@ -1,13 +1,10 @@
-#
-# PC-BASIC 3.23  - plat.py
-#
-# Platform identification
-# 
-# (c) 2014 Rob Hagemans 
-#
-# This file is released under the GNU GPL version 3. 
-# please see text file COPYING for licence terms.
-#
+"""
+PC-BASIC 3.23  - plat.py
+Platform identification
+
+(c) 2014 Rob Hagemans 
+This file is released under the GNU GPL version 3. 
+"""
 
 try:
     import android
