a54f974ce97c98a14b9a0e26f04656f1ec606f1a
Rob Hagemans <robhagemans@yahoo.co.uk>
2014-11-29 20:30:38 +0000

    audio_none.py: dosctrings

--- audio_none.py
+++ audio_none.py
@@ -1,27 +1,28 @@
-#
-# PC-BASIC 3.23 - nosound.py
-#
-# Null sound implementation
-# 
-# (c) 2013, 2014 Rob Hagemans 
-#
-# This file is released under the GNU GPL version 3. 
-# please see text file COPYING for licence terms.
-#
+"""
+PC-BASIC 3.23 - nosound.py
+Null sound implementation
+
+(c) 2013, 2014 Rob Hagemans 
+This file is released under the GNU GPL version 3. 
+"""
 
 import datetime
 import state
 
 music_queue = [ [], [], [], [] ]
 
+
 def init_sound():
+    """ Initialise sound system. """
     return True
     
 def stop_all_sound():
+    """ Clear all sound queues. """
     global music_queue
     music_queue = [ [], [], [], [] ]
     
 def play_sound(frequency, duration, fill=1, loop=False, voice=0, volume=15):
+    """ Queue a sound for playing. """
     if music_queue[voice]:
         latest = max(music_queue[voice])
     else:    
@@ -29,6 +30,7 @@ def play_sound(frequency, duration, fill=1, loop=False, voice=0, volume=15):
     music_queue[voice].append(latest + datetime.timedelta(seconds=duration))
         
 def check_sound():
+    """ Update the sound queue. """
     now = datetime.datetime.now()
     for voice in range(4):
         while music_queue[voice] and now >= music_queue[voice][0]:
@@ -38,8 +40,10 @@ def check_sound():
             state.console_state.music_queue[voice].pop(0)
     
 def busy():
+    """ Is the mixer busy? """
     return False        
 
 def set_noise(is_white):
+    """ Set the character of the noise channel. """
     pass      
     
