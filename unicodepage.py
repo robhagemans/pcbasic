611baf71d9cd6c58699a796431a513d5a354aef5
Rob Hagemans <robhagemans@yahoo.co.uk>
2014-11-29 21:26:49 +0000

    unicodepage.py: head docstring

--- unicodepage.py
+++ unicodepage.py
@@ -1,13 +1,10 @@
-#
-# PC-BASIC 3.23 - unicodepage.py
-#
-# Codepage conversions
-#
-# (c) 2013, 2014 Rob Hagemans 
-#
-# This file is released under the GNU GPL version 3. 
-# please see text file COPYING for licence terms.
-#
+"""
+PC-BASIC 3.23 - unicodepage.py
+Codepage conversions
+
+(c) 2013, 2014 Rob Hagemans 
+This file is released under the GNU GPL version 3. 
+"""
 
 import config
 import state
