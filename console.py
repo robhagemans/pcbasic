92fea6052a78afb40c29452c77a81db9921dd020
Rob Hagemans <robhagemans@yahoo.co.uk>
2014-11-29 21:12:13 +0000

    console.py: head docstring

--- console.py
+++ console.py
@@ -1,13 +1,10 @@
-#
-# PC-BASIC 3.23 - console.py
-#
-# Console front-end
-# 
-# (c) 2013, 2014 Rob Hagemans 
-#
-# This file is released under the GNU GPL version 3. 
-# please see text file COPYING for licence terms.
-#
+"""
+PC-BASIC 3.23 - console.py
+Console front-end
+
+(c) 2013, 2014 Rob Hagemans 
+This file is released under the GNU GPL version 3. 
+"""
 
 import logging
 
