b7b9b1250600aa6aa0a3bdfe6f956abf46dc077e
Rob Hagemans <robhagemans@yahoo.co.uk>
2014-11-30 13:05:56 +0000

    util.py: head docstring

--- util.py
+++ util.py
@@ -1,9 +1,9 @@
 """
-# PC-BASIC 3.23 - util.py
-# Token stream utilities
-# 
-# (c) 2013, 2014 Rob Hagemans 
-# This file is released under the GNU GPL version 3. 
+PC-BASIC 3.23 - util.py
+Token stream utilities
+
+(c) 2013, 2014 Rob Hagemans 
+This file is released under the GNU GPL version 3. 
 """
 
 from functools import partial
