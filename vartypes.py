b0eb5e3bfa76f5bc09bc4c9ee6fa42257476ed86
Rob Hagemans <robhagemans@yahoo.co.uk>
2014-11-30 12:09:09 +0000

    vartypes.py: docstrings

--- vartypes.py
+++ vartypes.py
@@ -1,13 +1,10 @@
-#
-# PC-BASIC 3.23 - vartypes.py
-#
-# Type conversions and generic functions
-# 
-# (c) 2013, 2014 Rob Hagemans 
-#
-# This file is released under the GNU GPL version 3. 
-# please see text file COPYING for licence terms.
-#
+"""
+PC-BASIC 3.23 - vartypes.py
+Type conversions and generic functions
+
+(c) 2013, 2014 Rob Hagemans 
+This file is released under the GNU GPL version 3. 
+"""
 
 import fp
 import error
@@ -17,11 +14,16 @@ import state
 null = { '$': ('$', ''), '%': ('%', bytearray('\x00')*2), '!': ('!', bytearray('\x00')*4), '#': ('#', bytearray('\x00')*8) }
 
 def complete_name(name):
+    """ Add type specifier to a name, if missing. """
     if name and name[-1] not in ('$', '%', '!', '#'):
         name += state.basic_state.deftype[ord(name[0].upper()) - 65] # ord('A') 
     return name
 
+###############################################################################
+# Int (%) - stored as two's complement, little-endian 
+
 def pass_int_keep(inp, maxint=0x7fff, err=13):
+    """ Check if variable is numeric, convert to Int. """
     if not inp:
         raise error.RunError(2)    
     typechar = inp[0]
@@ -38,17 +40,23 @@ def pass_int_keep(inp, maxint=0x7fff, err=13):
         raise error.RunError(err)
     
 def pass_int_unpack(inp, maxint=0x7fff, err=13):
+    """ Convert numeric variable to Python integer. """
     return unpack_int(pass_int_keep(inp, maxint, err))
 
 def unpack_int(inp):
+    """ Convert Int to Python integer. """
     return sint_to_value(inp[1])
 
 def pack_int(inp):
+    """ Convert Python integer to Int. """
     return ('%', value_to_sint(inp))
        
-######################################
+       
+###############################################################################
+# Single (!) - stored as 4-byte Microsoft Binary Format
 
 def pass_single_keep(num):
+    """ Check if variable is numeric, convert to Single. """
     if not num:
         raise error.RunError(2)
     typechar = num[0]
@@ -61,8 +69,12 @@ def pass_single_keep(num):
         return fp.pack(fp.unpack(num).round_to_single())
     elif typechar == '$':
         raise error.RunError(13)
+
+###############################################################################
+# Double (!) - stored as 8-byte Microsoft Binary Format
     
 def pass_double_keep(num):
+    """ Check if variable is numeric, convert to Double. """
     if not num:
         raise error.RunError(2)
     typechar = num[0]
@@ -75,13 +87,11 @@ def pass_double_keep(num):
     elif typechar == '$':
         raise error.RunError(13)
 
-def pass_float_keep(num, allow_double=True):
-    if num and num[0] == '#' and allow_double:
-        return num
-    else:
-        return pass_single_keep(num)
+###############################################################################
+# String ($) - stored as 1-byte length plus 2-byte pointer to string space
 
 def pass_string_keep(inp, allow_empty=False, err=13):
+    """ Check if variable is String-valued. """
     if not inp:
         if not allow_empty:
             raise error.RunError(2)    
@@ -92,15 +102,36 @@ def pass_string_keep(inp, allow_empty=False, err=13):
     else:     
         raise error.RunError(err)
 
+def pass_string_unpack(inp, allow_empty=False, err=13):
+    """ Convert string-valued variable to Python bytearray. """
+    return pass_string_keep(inp, allow_empty, err)[1]
+
+def unpack_string(inp):
+    """ Convert string-valued variable to Python bytearray, no checks """
+    return inp[1]
+            
+def pack_string(inp):
+    """ Convert Python bytearray to string-valued variable, no checks. """
+    return ('$', inp)
+
+###############################################################################
+# multi-type functions
+
+def pass_float_keep(num, allow_double=True):
+    """ Check if variable is numeric, convert to Double or Single. """
+    if num and num[0] == '#' and allow_double:
+        return num
+    else:
+        return pass_single_keep(num)
+
 def pass_number_keep(inp, err=13):
+    """ Check if variable is numeric. """
     if inp[0] not in ('%', '!', '#'):
         raise error.RunError(err)
     return inp    
 
-def pass_string_unpack(inp, allow_empty=False, err=13):
-    return pass_string_keep(inp, allow_empty, err)[1]
-
-def pass_type_keep(typechar, value):     
+def pass_type_keep(typechar, value): 
+    """ Check if variable can be converted to the given type and convert. """    
     if typechar == '$':
         return pass_string_keep(value)
     elif typechar == '%':
@@ -113,6 +144,7 @@ def pass_type_keep(typechar, value):
         raise error.RunError(2)
  
 def pass_most_precise_keep(left, right, err=13):
+    """ Check if variables are numeric and convert to highest-precision. """
     left_type, right_type = left[0][-1], right[0][-1]
     if left_type=='#' or right_type=='#':
         return (pass_double_keep(left), pass_double_keep(right))
@@ -124,14 +156,15 @@ def pass_most_precise_keep(left, right, err=13):
         raise error.RunError(err)
 
     
-##################################################
-# unpack tokenised numeric constants
+###############################################################################
+# int tokens to Python ints
 
 def uint_to_value(s):
-    # unsigned int. 
+    """ Convert unsigned little-endian int token to Python integer """
     return 0x100 * s[1] + s[0]
 
 def sint_to_value(s):
+    """ Convert two's complement little-endian int token to Python integer """
     # 2's complement signed int, least significant byte first, sign bit is most significant bit
     value = 0x100 * (s[1] & 0x7f) + s[0]
     if (s[1] & 0x80) == 0x80:
@@ -139,15 +172,18 @@ def sint_to_value(s):
     else: 
         return value
     
-# python ints to tokenised ints
+###############################################################################
+# Python ints to int tokens
 
 def value_to_uint(n):
+    """ Convert Python integer to unsigned little-endian token. """
     if n > 0xffff:
         # overflow
         raise error.RunError(6)        
     return bytearray((n&0xff, n >> 8)) 
 
 def value_to_sint(n):
+    """ Convert Python integer to two's complement little-endian token. """
     if n > 0xffff:  # 0x7fff ?
         # overflow
         raise error.RunError(6)     
@@ -155,37 +191,38 @@ def value_to_sint(n):
         n = 0x10000 + n        
     return bytearray((n&0xff, n >> 8)) 
 
-##################################################
-
-def unpack_string(inp):
-    return inp[1]
-            
-def pack_string(inp):
-    return ('$', inp)
-
-# boolean functions - two's complement int
+###############################################################################
+# boolean functions operate as bitwise functions on unsigned Python ints
 
 def bool_to_int_keep(boo):
+    """ Convert Python boolean to Int. """
     return pack_int(-boo)
     
 def int_to_bool(iboo):
+    """ Convert Int to Python boolean. """
     return not (unpack_int(iboo) == 0)
 
+# _twoscomp is a misnomer here, _unsigned would be better
 def pass_twoscomp(num):
+    """ Convert Int to Python int (unsigned). """
     val = pass_int_unpack(num)
     if val < 0:
         return 0x10000 + val
     else:
         return val
 
+# _twoscomp is a misnomer here, _unsigned would be better
 def twoscomp_to_int(num):
+    """ Convert Python int (unsigned) to Int. """
     if num > 0x7fff:
-        num -= 0x10000 
-    return pack_int(num)    
+        num -= 0x10000
+    return pack_int(num)
 
-##################################################
+###############################################################################
+# string operations
 
-def str_gt(left,right):
+def str_gt(left, right):
+    """ String ordering: return whether left > right. """
     shortest = min(len(left), len(right))
     for i in range(shortest):
         if left[i] > right[i]:
@@ -201,6 +238,7 @@ def str_gt(left,right):
     return False                    
 
 def str_instr(big, small, n):
+    """ Find substring in string and return starting index. """
     if big == '' or n > len(big):
         return null['%']
     # BASIC counts string positions from 1
@@ -208,8 +246,12 @@ def str_instr(big, small, n):
     if find == -1:
         return null['%']
     return pack_int(n + find)  
+
+###############################################################################
+# numeric operations
     
 def number_add(left, right):
+    """ Add two numbers. """
     left, right = pass_most_precise_keep(left, right)
     if left[0] in ('#', '!'):
         return fp.pack(fp.unpack(left).iadd(fp.unpack(right)))
@@ -217,6 +259,7 @@ def number_add(left, right):
         return pack_int(unpack_int(left) + unpack_int(right))           
 
 def number_sgn(inp):
+    """ Return the sign of a number. """
     if inp[0] == '%':
         i = unpack_int(inp)
         if i > 0:
@@ -230,6 +273,7 @@ def number_sgn(inp):
     return inp
 
 def number_abs(inp):
+    """ Return the absolute value of a number. """
     if inp[0] == '%':
         val = abs(unpack_int(inp))
         if val == 32768:
@@ -243,6 +287,7 @@ def number_abs(inp):
     return inp
 
 def number_neg(inp):
+    """ Return the negation of a number. """
     if inp[0] == '%':
         val = -unpack_int(inp)
         if val == 32768:
@@ -257,6 +302,7 @@ def number_neg(inp):
     return inp
             
 def equals(left,right): 
+    """ Return whether two numbers are equal. """
     if left[0] == '$':
         return pass_string_unpack(left) == pass_string_unpack(right)
     else:
@@ -267,6 +313,7 @@ def equals(left,right):
             return unpack_int(left)==unpack_int(right)
 
 def gt(left, right):
+    """ Number ordering: return whether left > right. """
     if left[0] == '$':
         return str_gt(pass_string_unpack(left), pass_string_unpack(right))
     else:
