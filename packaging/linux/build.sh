75ad89160e2eeafd4f8adf7db36c875328f824a8
Rob Hagemans <robhagemans@yahoo.co.uk>
2015-01-18 22:35:09 +0000

    Automate build process on linux (2)

---
+++ packaging/linux/build.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+NAME="pc-basic-$1-linux-i386"
+
+pyinstaller installer.spec
+# add files to top level of archive
+cp install.sh dist/install.sh
+cp pcbasic.png dist/pcbasic.png
+cp ../../README.md dist/README.md
+chmod ugo+x dist/install.sh
+
+mv dist "$NAME"
+tar cvfz $NAME.tgz $NAME
+
+rm -rf build
+rm -rf "$NAME"
+
+
