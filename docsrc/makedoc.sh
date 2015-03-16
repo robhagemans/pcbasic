f887df9a1c4a95fcb55607bb721637b2faf57540
Rob Hagemans <robhagemans@yahoo.co.uk>
2015-03-16 20:59:13 +0000

    add documentation builder script

---
+++ docsrc/makedoc.sh
@@ -0,0 +1,2 @@
+cat header.html description.html options.html footer.html | pandoc -f html -t plain > ../data/USAGE
+cat header.html description.html documentation.html options.html examples.html reference.html acknowledgements.html footer.html > PC-BASIC_documentation.html
