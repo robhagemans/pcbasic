fcd6c5a9308cf066b9a97847020ad1ea5ccb9da6
Rob Hagemans <robhagemans@yahoo.co.uk>
2015-03-28 17:38:51 +0000

    integrate ansipipe compiling in windows build

--- packaging/windows/build.bat
+++ packaging/windows/build.bat
@@ -1,5 +1,8 @@
 pyinstaller installer.spec
-copy ansipipe.exe dist\pcbasic\pcbasic.com
+cd ..\..\ansipipe
+make launcher
+cd ..\packaging\windows
+move ..\..\ansipipe\launcher.exe dist\pcbasic\pcbasic.com
 makensis pcbasic.nsi
 ren pcbasic-win32.exe pcbasic-%1-win32.exe
 rmdir /s /q build
