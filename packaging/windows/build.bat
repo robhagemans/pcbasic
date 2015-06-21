3e39ed7616bda5eb6c256b15c8efcb7f89908266
Rob Hagemans <robhagemans@yahoo.co.uk>
2015-06-21 22:12:17 +0100

    determine package version string from VERSION file

--- packaging/windows/build.bat
+++ packaging/windows/build.bat
@@ -1,10 +1,11 @@
+set /p VERSION=<..\..\data\VERSION
 pyinstaller installer.spec
 cd ..\..\ansipipe
 gcc -s launcher.c -o launcher -DSUPPRESS_STDERR
 cd ..\packaging\windows
 move ..\..\ansipipe\launcher.exe dist\pcbasic\pcbasic.com
 makensis pcbasic.nsi
-ren pcbasic-win32.exe pcbasic-%1-win32.exe
+ren pcbasic-win32.exe pcbasic-%VERSION%-win32.exe
 rmdir /s /q build
 rmdir /s /q dist
 
