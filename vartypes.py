f35cdfb99ad4b04d5e881e7c583dbd5811d6fcdc
Rob Hagemans <robhagemans@yahoo.co.uk>
2015-07-25 16:49:21 +0100

    vartypes: update author notice

--- vartypes.py
+++ vartypes.py
@@ -1,8 +1,8 @@
 """
-PC-BASIC 3.23 - vartypes.py
+PC-BASIC - vartypes.py
 Type conversions and generic functions
 
-(c) 2013, 2014 Rob Hagemans
+(c) 2013, 2014, 2015 Rob Hagemans
 This file is released under the GNU GPL version 3.
 """
 
