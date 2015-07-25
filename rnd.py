34e861cd94d37623df663466e202f985d713ecdb
Rob Hagemans <robhagemans@yahoo.co.uk>
2015-07-25 16:49:42 +0100

    rnd: remove trailing whitespace

--- rnd.py
+++ rnd.py
@@ -2,8 +2,8 @@
 PC-BASIC 3.23  - rnd.py
 Random number generator
 
-(c) 2013, 2014 Rob Hagemans 
-This file is released under the GNU GPL version 3. 
+(c) 2013, 2014 Rob Hagemans
+This file is released under the GNU GPL version 3.
 """
 
 import fp
@@ -23,7 +23,7 @@ def clear():
     """ Reset the random number generator. """
     state.basic_state.rnd_seed = 5228370 # 0x4fc752
 
-def randomize(val):        
+def randomize(val):
     """ Reseed the random number generator. """
     # RANDOMIZE converts to int in a non-standard way - looking at the first two bytes in the internal representation
     # on a program line, if a number outside the signed int range (or -32768) is entered,
@@ -31,10 +31,10 @@ def randomize(val):
     #   - ignores the first 4 bytes (if it's a double)
     #   - reads the next two
     #   - xors them with the final two (most signifant including sign bit, and exponent)
-    # and interprets them as a signed int 
+    # and interprets them as a signed int
     # e.g. 1#    = /x00/x00/x00/x00 /x00/x00/x00/x81 gets read as /x00/x00 ^ /x00/x81 = /x00/x81 -> 0x10000-0x8100 = -32512 (sign bit set)
     #      0.25# = /x00/x00/x00/x00 /x00/x00/x00/x7f gets read as /x00/x00 ^ /x00/x7f = /x00/x7F -> 0x7F00 = 32512 (sign bit not set)
-    #              /xDE/xAD/xBE/xEF /xFF/x80/x00/x80 gets read as /xFF/x80 ^ /x00/x80 = /xFF/x00 -> 0x00FF = 255   
+    #              /xDE/xAD/xBE/xEF /xFF/x80/x00/x80 gets read as /xFF/x80 ^ /x00/x80 = /xFF/x00 -> 0x00FF = 255
     s = val[1]
     final_two = s[-2:]
     mask = bytearray('\x00\x00')
@@ -46,7 +46,7 @@ def randomize(val):
     get_random_int(1) # RND(1)
     state.basic_state.rnd_seed += n * rnd_step
     state.basic_state.rnd_seed %= rnd_period
-    
+
 def get_random_int(n):
     """ Get a value from the random number generator (int argument). """
     if n < 0:
@@ -55,14 +55,12 @@ def get_random_int(n):
             n *= 2
         state.basic_state.rnd_seed = n
     if n != 0:
-        state.basic_state.rnd_seed = (state.basic_state.rnd_seed*rnd_a + rnd_c) % rnd_period       
+        state.basic_state.rnd_seed = (state.basic_state.rnd_seed*rnd_a + rnd_c) % rnd_period
     # rnd_seed/rnd_period
     return fp.pack(fp.div(fp.Single.from_int(state.basic_state.rnd_seed), fp.Single.from_int(rnd_period)))
 
 def get_random(mbf):
     """ Get a value from the random number generator (MBF single argument). """
-    return get_random_int(-(mbf.man>>8) if mbf.neg else mbf.man>>8)    
-    
+    return get_random_int(-(mbf.man>>8) if mbf.neg else mbf.man>>8)
+
 prepare()
-
-
