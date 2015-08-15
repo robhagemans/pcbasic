666f17102b90e9685cb907117d083d13d70ec325
Rob Hagemans <robhagemans@yahoo.co.uk>
2015-08-15 19:02:59 +0100

    change documentation headers

--- docsrc/makedoc.sh
+++ docsrc/makedoc.sh
@@ -1,5 +1,5 @@
 cat header.html description.html options.html footer.html | pandoc -f html -t plain > ../data/USAGE
 cat documentation.html options.html examples.html reference.html acknowledgements.html footer.html > predoc.html
 ./maketoc.py predoc.html > toc.html
-(cat header.html description.html; echo -n "<small>PC-BASIC "; cat ../data/VERSION; echo -n "&mdash; documentation compiled on "; date --utc; echo "</small>"; cat toc.html predoc.html) > ../doc/PC-BASIC_documentation.html
-rm predoc.html
+(cat header.html; echo -e "<h1>PC-BASIC $(cat ../data/VERSION)</h1>\n<p><small>Documentation compiled on $(date --utc).</small></p>"; cat toc.html predoc.html) > ../doc/PC-BASIC_documentation.html
+rm predoc.html toc.html
