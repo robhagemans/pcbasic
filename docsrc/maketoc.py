8179f789d842f205b3b6ab5841a8f9396900e9fd
Rob Hagemans <robhagemans@yahoo.co.uk>
2015-08-18 20:24:06 +0100

    fix TOC html validity

--- docsrc/maketoc.py
+++ docsrc/maketoc.py
@@ -15,10 +15,17 @@ for node in doc.xpath('//h1|//h2|//h3'):
     node.attrib.clear()
     if node_id:
         node.set('href', '#' + node_id)
-    if level-last:
-        print '    '*min(level, last) + '<ul>\n'*(level-last)+'</ul>\n'*(last-level),
-    print '    '*level + '<li>' + etree.tostring(node).strip() + '</li>'
+    if level-last < 0:
+        print '</li>'
+        print '    '*level + '</ul></li>\n'*(last-level),
+    elif level-last > 0:
+        print
+        print '    '*last + '<ul>\n'*(level-last)
+    else:
+        print '</li>'
+    print '    '*level + '<li>' + etree.tostring(node).strip(),
     last = level
+print '</li>'
 while level > first:
     level -= 1
     print '    '*level + '</ul>'
