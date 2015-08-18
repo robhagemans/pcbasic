13799fb3cc194846827dab1c6da8274d41ae417d
Rob Hagemans <robhagemans@yahoo.co.uk>
2015-08-18 10:48:26 +0100

    restructure documentation

--- docsrc/makedoc.sh
+++ docsrc/makedoc.sh
@@ -1,5 +1,5 @@
 cat header.html description.html options.html footer.html | pandoc -f html -t plain > ../data/USAGE
-cat documentation.html options.html examples.html reference.html acknowledgements.html footer.html > predoc.html
+(cat documentation.html; echo -e '<h1 id="invocation">Invocation</h1>'; cat options.html examples.html reference.html acknowledgements.html footer.html) > predoc.html
 ./maketoc.py predoc.html > toc.html
 (cat header.html; echo -e "<h1>PC-BASIC $(cat ../data/VERSION)</h1>\n<p><small>Documentation compiled on $(date --utc).</small></p>"; cat toc.html predoc.html) > ../doc/PC-BASIC_documentation.html
 rm predoc.html toc.html
