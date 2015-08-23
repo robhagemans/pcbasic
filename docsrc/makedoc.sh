a4a22435a7b2fc1793d34bd212548a70bf327bcd
Rob Hagemans <robhagemans@yahoo.co.uk>
2015-08-23 17:45:54 +0100

    use modified doc.css from www project for offline docs; move to docsrc/

--- docsrc/makedoc.sh
+++ docsrc/makedoc.sh
@@ -1,4 +1,6 @@
 cat header.html description.html options.html footer.html | pandoc -f html -t plain > ../pcbasic/data/USAGE
+mkdir ../doc
+cp doc.css ../doc
 (cat documentation.html; echo -e '<article>\n<h1 id="invocation">Invocation</h1>'; cat options.html examples.html; echo -e "</article>"; cat reference.html acknowledgements.html footer.html) > predoc.html
 ./maketoc.py predoc.html > toc.html
 (cat header.html; echo -e "<header>\n<h1>PC-BASIC $(cat ../pcbasic/data/VERSION)</h1>\n<p><small>Documentation compiled on $(date --utc).</small></p></header>"; cat toc.html predoc.html) > ../doc/PC-BASIC_documentation.html
