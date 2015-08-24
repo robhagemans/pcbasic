c1e5d4ee88aa8a8534243b07e7e551a35974e497
Rob Hagemans <robhagemans@yahoo.co.uk>
2015-08-24 06:44:40 +0100

    correct path to main() for setuptools entry point

--- setup.py
+++ setup.py
@@ -39,7 +39,6 @@ setup(
     ],
 
     keywords='emulator interpreter basic retro legacy gwbasic basica pcjr tandy basicode',
-
     packages=find_packages(exclude=['doc', 'test', 'docsrc', 'packaging', 'patches']),
 
     # List run-time dependencies here.  These will be installed by pip when
@@ -57,26 +56,17 @@ setup(
     #    'test': ['coverage'],
     #},
 
-    # If there are data files included in your packages that need to be
-    # installed, specify them here.  If using Python 2.6 or less, then these
-    # have to be included in MANIFEST.in as well.
     package_data={
         'pcbasic': ['data/*', 'encoding/*',
                     'font/*'],
     },
-
-    # Although 'package_data' is the preferred approach, in some case you may
-    # need to place data files outside of your packages. See:
-    # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files # noqa
-    # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
-    #data_files=[('my_data', ['data/data_file'])],
-
-    # To provide executable scripts, use entry points in preference to the
-    # "scripts" keyword. Entry points provide cross-platform support and allow
-    # pip to create the appropriate form of executable for the target platform.
     entry_points={
         'console_scripts': [
-            'pcbasic=pcbasic:main',
+            'pcbasic=pcbasic.__main__:main',
         ],
+        'gui_scripts': [
+            'pcbasic=pcbasic.__main__:main',
+        ],
+
     },
 )
