979d5432babfae6b32828f6762d29a33c1b6b6ce
Rob Hagemans <robhagemans@yahoo.co.uk>
2015-08-24 13:40:18 +0100

    remove stray -e from doc header

--- docsrc/makedoc.sh
+++ docsrc/makedoc.sh
@@ -4,7 +4,7 @@ cat header.html description.html options.html footer.html | pandoc -f html -t pl
 mkdir ../doc
 cp doc.css ../doc
 cp LICENSE.md ../doc
-(cat documentation.html; echo -e '<article>\n<h1 id="invocation">Invocation</h1>'; cat options.html examples.html; echo -e "</article>"; cat reference.html acknowledgements.html footer.html) > predoc.html
+(cat documentation.html; echo "<article>\n<h1 id=\"invocation\">Invocation</h1>"; cat options.html examples.html; echo "</article>"; cat reference.html acknowledgements.html footer.html) > predoc.html
 ./maketoc.py predoc.html > toc.html
-(cat header.html; echo -e "<header>\n<h1>PC-BASIC $(cat ../pcbasic/data/version.txt)</h1>\n<p><small>Documentation compiled on $(date --utc).</small></p></header>"; cat toc.html predoc.html) > ../doc/PC-BASIC_documentation.html
+(cat header.html; echo '<header>\n<h1>PC-BASIC $(cat ../pcbasic/data/version.txt)</h1>\n<p><small>Documentation compiled on $(date --utc).</small></p></header>'; cat toc.html predoc.html) > ../doc/PC-BASIC_documentation.html
 rm predoc.html toc.html description.html
