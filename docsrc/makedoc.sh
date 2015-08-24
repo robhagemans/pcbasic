f687e640ea1194f11ed83b39352055a32aa26723
Rob Hagemans <robhagemans@yahoo.co.uk>
2015-08-24 13:51:20 +0100

    fix incorrect quoting

--- docsrc/makedoc.sh
+++ docsrc/makedoc.sh
@@ -6,5 +6,5 @@ cp doc.css ../doc
 cp LICENSE.md ../doc
 (cat documentation.html; echo "<article>\n<h1 id=\"invocation\">Invocation</h1>"; cat options.html examples.html; echo "</article>"; cat reference.html acknowledgements.html footer.html) > predoc.html
 ./maketoc.py predoc.html > toc.html
-(cat header.html; echo '<header>\n<h1>PC-BASIC $(cat ../pcbasic/data/version.txt)</h1>\n<p><small>Documentation compiled on $(date --utc).</small></p></header>'; cat toc.html predoc.html) > ../doc/PC-BASIC_documentation.html
+(cat header.html; echo "<header>\n<h1>PC-BASIC $(cat ../pcbasic/data/version.txt)</h1>\n<p><small>Documentation compiled on $(date --utc).</small></p></header>"; cat toc.html predoc.html) > ../doc/PC-BASIC_documentation.html
 rm predoc.html toc.html description.html
