55332714dfe3eecca719a164512c6ebc57f3b68f
Rob Hagemans <robhagemans@yahoo.co.uk>
2015-08-25 10:27:50 +0100

    do not copy ansipipe binary into source package

--- packaging/source/build.sh
+++ packaging/source/build.sh
@@ -10,7 +10,6 @@ rsync -rvp ../.. pcbasic/ --delete --exclude-from=excludes --delete-excluded
 cp install.sh pcbasic/
 cp pcbasic.png pcbasic/
 cp pcbasic.bat pcbasic/
-cp ansipipe.exe pcbasic/
 mv pcbasic $NAME
 tar cvfz "$NAME.tgz" "$NAME/"
 rm -rf "$NAME/"
