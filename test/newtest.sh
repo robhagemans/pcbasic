e421968c26ee073044be550366b95fbd89094b52
Rob Hagemans <robhagemans@yahoo.co.uk>
2015-08-26 20:07:26 +0100

    create new testing framework, convert BSAVE and BSAVE-tandy tests

---
+++ test/newtest.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+if [ -z $1 ]
+then
+     echo "Usage: test.sh TESTNAME"
+     exit
+fi
+name=$1
+echo -n "Running test $name... "
+if [ ! -e $name ]
+then
+     echo "no such test."
+     exit
+fi
+mkdir "$name/output"
+cp "$name"/* "$name/output" 2>/dev/null
+pushd "$name/output" > /dev/null
+../../../pcbasic.py
+popd > /dev/null
+pass=1
+for file in "$name/output"/*
+do
+filename=$(basename "$file")
+  diff "$name/output/$filename" "$name/model/$filename"
+  if [ $? -ne 0 ]
+  then
+     echo "$filename: FAIL";
+     pass=0
+  fi
+done
+if [ $pass -ne 1 ]
+then
+     echo "FAILED.";
+else
+     echo "passed.";
+     rm -r $name/output
+fi
