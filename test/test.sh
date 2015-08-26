445ed5d0a4d3b07d56914a3d6697a16f643314c4
Rob Hagemans <robhagemans@yahoo.co.uk>
2015-08-26 20:34:27 +0100

    test framework: test for missing or additional files

--- test/test.sh
+++ test/test.sh
@@ -17,14 +17,23 @@ pushd "$name/output" > /dev/null
 ../../../pcbasic.py
 popd > /dev/null
 pass=1
-for file in "$name/output"/*
+for file in "$name/model"/*
 do
-filename=$(basename "$file")
+  filename=$(basename "$file")
   diff "$name/output/$filename" "$name/model/$filename"
   if [ $? -ne 0 ]
   then
      echo "$filename: FAIL";
      pass=0
+  fi
+done
+for file in "$name/output"/*
+do
+  filename=$(basename "$file")
+  if [ ! -e "$name/model/$filename" ]
+  then
+    echo "$filename: FAIL (not in model)"
+    pass=0
   fi
 done
 if [ $pass -ne 1 ]
