58db3db2713864181e35c3cf590846bd1a1e8db3
Rob Hagemans <robhagemans@yahoo.co.uk>
2015-08-28 20:37:11 +0100

    makedoc: ensure pandoc generates html5

--- docsrc/makedoc.sh
+++ docsrc/makedoc.sh
@@ -16,13 +16,13 @@ else
    OUTPUT=$2
 fi
 echo $HEADER $OUTPUT
-pandoc ../README.md -o readme.html
+pandoc ../README.md -t html5 -o readme.html
 (echo "<article>"; cat readme.html; echo "</article>") | sed -e "s/h3/h1/g" -e "s/h4/h2/g" -e "s_PC-BASIC</h1>_Overview</h1>_"> readme2.html
 mkdir ../doc
 cp doc.css ../doc
 cp LICENSE.md ../doc
-pandoc ../LICENSE.md -o pcbasiclicense.html
-pandoc LICENSE.md -o doclicense.html
+pandoc ../LICENSE.md -t html5 -o pcbasiclicense.html
+pandoc LICENSE.md -t html5 -o doclicense.html
 
 (echo "<footer><h1 id=\"licence\">Licences</h1>"; cat pcbasiclicense.html doclicense.html; echo "</footer>") > licences.html
 (cat readme2.html documentation.html; echo -e "<article>\n<h1 id=\"invocation\">Invocation</h1>"; cat options.html examples.html; echo "</article>"; cat reference.html acknowledgements.html licences.html footer.html) > predoc.html
