82d22274acb13d7a5440588f7a996892374edaf7
Rob Hagemans <robhagemans@yahoo.co.uk>
2015-08-28 23:17:30 +0100

    create header bar for offline docs

--- docsrc/makedoc.sh
+++ docsrc/makedoc.sh
@@ -27,6 +27,6 @@ pandoc LICENSE.md -t html5 -o doclicense.html
 (echo "<footer><h1 id=\"licence\">Licences</h1>"; cat pcbasiclicense.html doclicense.html; echo "</footer>") > licences.html
 (cat readme2.html documentation.html; echo -e "<article>\n<h1 id=\"invocation\">Invocation</h1>"; cat options.html examples.html; echo "</article>"; cat reference.html acknowledgements.html licences.html footer.html) > predoc.html
 ./maketoc.py predoc.html > toc.html
-echo -e "<header>\n<h1>PC-BASIC Documentation</h1>\n<p>This is the documentation for PC-BASIC <em>$(cat ../pcbasic/data/version.txt)</em>.</p>\n<small>This file was compiled on $(date --utc).</small>\n</header>" > subheader.html
+echo -e "<header>\n<h1>PC-BASIC $(cat ../pcbasic/data/version.txt) documentation</h1>\n<small>Documentation compiled on $(date --utc).</small>\n</header>" > subheader.html
 (cat ../../$HEADER subheader.html toc.html predoc.html) > ../../$OUTPUT
 rm predoc.html toc.html description.html readme.html readme2.html subheader.html pcbasiclicense.html doclicense.html licences.html
