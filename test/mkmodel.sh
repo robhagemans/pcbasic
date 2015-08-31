3fab6056c1c167774b40bffbc732ffcf19f7f369
Rob Hagemans <robhagemans@yahoo.co.uk>
2015-08-31 09:02:54 +0100

    use textfile rather than symlink for model gwbasic location

--- test/mkmodel.sh
+++ test/mkmodel.sh
@@ -4,7 +4,7 @@ cd "$name"
 mkdir model
 cp -R * model/
 cd model/
-GWBASIC=$(readlink ../../gwbasicdir)
+GWBASIC=$(cat ../../gwbasicdir)
 echo "gwbasic dir: $GWBASIC"
 dosbox -c "MOUNT C ." -c "MOUNT E $GWBASIC" -c "C:" -c "E:\GWBASIC\GWBASIC TEST.BAS" -c "EXIT"
 cd ../..
