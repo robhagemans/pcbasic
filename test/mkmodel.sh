1392acb79a39bb345d00e46c60ec0d0270ebd212
Rob Hagemans <robhagemans@yahoo.co.uk>
2015-08-31 09:04:45 +0100

    mkmodel.sh: remove model/model directory

--- test/mkmodel.sh
+++ test/mkmodel.sh
@@ -4,6 +4,8 @@ cd "$name"
 mkdir model
 cp -R * model/
 cd model/
+# remove model/model created by cp -R
+rm -r model/
 GWBASIC=$(cat ../../gwbasicdir)
 echo "gwbasic dir: $GWBASIC"
 dosbox -c "MOUNT C ." -c "MOUNT E $GWBASIC" -c "C:" -c "E:\GWBASIC\GWBASIC TEST.BAS" -c "EXIT"
