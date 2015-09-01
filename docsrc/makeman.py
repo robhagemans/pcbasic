f9af8cd793864a295b453357e1cb7b3da346f92d
Rob Hagemans <robhagemans@yahoo.co.uk>
2015-09-01 21:28:25 +0100

    add description, name, examples to manpage

--- docsrc/makeman.py
+++ docsrc/makeman.py
@@ -1,25 +1,27 @@
 #!/usr/bin/env python2
 from lxml import etree, html
+import markdown
+import codecs
 import sys
 import re
-#from cStringIO import StringIO
+from cStringIO import StringIO
 
 def massage(text):
-    return re.sub(' +', ' ', text.encode('utf-8').replace('-', '\\-').replace('|', '\\||').replace('.', '\\|.').replace('\n', ' '))
+    return re.sub(' +', ' ', text.encode('utf-8').replace('\\', '\\\\').replace('-', '\\-').replace('|', '\\||').replace('.', '\\|.').replace('\n', ' '))
 
 def html_to_text(e, indent='', inside=False):
     inner = massage(e.text) if e.text else ''
     tail = massage(e.tail) if e.tail else ''
-    sys.stderr.write(indent + e.tag.upper())
-    if inner:
-        sys.stderr.write( ' inner "%s"' % inner )
-    if tail:
-        sys.stderr.write( ' tail "%s"' % tail )
-    sys.stderr.write( '\n')
-
-    children = ''.join(html_to_text(child, '  '+indent, inside or e.tag.upper()=='DD') for child in e.iterchildren())
-
-    if e.tag.upper() == 'H2':
+    #sys.stderr.write(indent + e.tag.upper())
+    #if inner:
+    #    sys.stderr.write( ' inner "%s"' % inner )
+    #if tail:
+    #    sys.stderr.write( ' tail "%s"' % tail )
+    #sys.stderr.write( '\n')
+    children = ''.join(html_to_text(child, '  '+indent, inside or e.tag.upper()=='DD') for child in e.iterchildren(tag=etree.Element))
+    if e.tag.upper() == 'H1':
+        return '\n.SH NAME\n' + inner.lower().replace('\-', '') + ' \- '
+    elif e.tag.upper() == 'H2':
         return '\n.SH ' + inner.upper() + children.upper() + '\n' + tail
     elif e.tag.upper() == 'VAR':
         return '\\fI' + inner + children + '\\fR' + tail
@@ -29,7 +31,7 @@ def html_to_text(e, indent='', inside=False):
         if inside:
             return '\n\n'+ inner + children + '\t' + tail
         else:
-            return '\n.IP ' + inner + children + '\n' + tail
+            return '\n.IP "' + inner + children + '"\n' + tail
     elif e.tag.upper() == 'P':
         return '\n'+ inner + children + '\n' +tail
     elif e.tag.upper() == 'DD':
@@ -39,8 +41,16 @@ def html_to_text(e, indent='', inside=False):
     else:
         return inner + children + tail
 
+title_html = '<h1>pcbasic</h1><p>%s</p>\n' % open('tagline.txt', mode='r').read()
+desc_html = '<h2>Description</h2><p>%s</p>\n' % open('description.txt', mode='r').read()
+options_html = open('options.html', mode='r').read()
+examples_html = open('examples.html', mode='r').read()
+html = title_html + desc_html + options_html + examples_html
+
 parser = etree.HTMLParser(encoding='utf-8')
-doc = etree.parse(sys.argv[1], parser)
+doc = etree.parse(StringIO(html), parser)
 docroot = doc.getroot()
+
+manpage = '\'\\" t\n.pc\n.TH PCBASIC 1\n' + html_to_text(docroot)
 # replace two starting spaces (not sure where from)
-print re.sub('\t +', '\t', re.sub('\n +', '\n', '\'\\" t\n.pc\n.TH PCBASIC 1\n'+html_to_text(docroot)))
+print re.sub('\t +', '\t', re.sub('\n +', '\n', manpage))
