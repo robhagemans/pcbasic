72135e3d3b59b88798cef502ceedf436ede750c3
Rob Hagemans <robhagemans@yahoo.co.uk>
2015-09-07 10:42:18 +0200

    reorganise html_to_man

--- docsrc/makeman.py
+++ docsrc/makeman.py
@@ -9,40 +9,42 @@ import time
 import os
 
 
-def massage(text):
-    return re.sub(' +', ' ', text.encode('utf-8').replace('\\', '\\\\').replace('-', '\\-').replace('|', '\\||').replace('.', '\\|.').replace('\n', ' '))
 
-def html_to_text(e, indent='', inside=False):
-    inner = massage(e.text) if e.text else ''
-    tail = massage(e.tail) if e.tail else ''
-    children = ''.join(html_to_text(child, '  '+indent, inside or e.tag.upper()=='DD') for child in e.iterchildren(tag=etree.Element))
-    if e.tag.upper() == 'H1':
-        return '\n.SH NAME\n' + inner.lower().replace('\-', '') + ' \- '
-    elif e.tag.upper() == 'H2':
-        return '\n.SH ' + inner.upper() + children.upper() + '\n' + tail
-    elif e.tag.upper() == 'VAR':
-        return '\\fI' + inner + children + '\\fR' + tail
-    elif e.tag.upper() == 'B':
-        return '\\fB' + inner + children + '\\fR' + tail
-    elif e.tag.upper() == 'DT':
-        if inside:
-            return '\n\n'+ inner + children + '\t' + tail
-        else:
-            return '\n.IP "' + inner + children + '"\n' + tail
-    elif e.tag.upper() == 'P':
-        return '\n'+ inner + children + '\n' +tail
-    elif e.tag.upper() == 'DD':
-        return inner + children + '\n' +tail
-    elif e.tag.upper() == 'DL':
-        return inner + children + '\n' +tail
-    else:
-        return inner + children + tail
 
 def html_to_man(html):
+    def massage(text):
+        return re.sub(' +', ' ', text.encode('utf-8').replace('\\', '\\\\').replace('-', '\\-').replace('|', '\\||').replace('.', '\\|.').replace('\n', ' '))
+
+    def convert_html(e, indent='', inside=False):
+        inner = massage(e.text) if e.text else ''
+        tail = massage(e.tail) if e.tail else ''
+        children = ''.join(convert_html(child, '  '+indent, inside or e.tag.upper()=='DD') for child in e.iterchildren(tag=etree.Element))
+        if e.tag.upper() == 'H1':
+            return '\n.SH NAME\n' + inner.lower().replace('\-', '') + ' \- '
+        elif e.tag.upper() == 'H2':
+            return '\n.SH ' + inner.upper() + children.upper() + '\n' + tail
+        elif e.tag.upper() == 'VAR':
+            return '\\fI' + inner + children + '\\fR' + tail
+        elif e.tag.upper() == 'B':
+            return '\\fB' + inner + children + '\\fR' + tail
+        elif e.tag.upper() == 'DT':
+            if inside:
+                return '\n\n'+ inner + children + '\t' + tail
+            else:
+                return '\n.IP "' + inner + children + '"\n' + tail
+        elif e.tag.upper() == 'P':
+            return '\n'+ inner + children + '\n' +tail
+        elif e.tag.upper() == 'DD':
+            return inner + children + '\n' +tail
+        elif e.tag.upper() == 'DL':
+            return inner + children + '\n' +tail
+        else:
+            return inner + children + tail
+
     parser = etree.HTMLParser(encoding='utf-8')
     doc = etree.parse(StringIO(html), parser)
     docroot = doc.getroot()
-    manpage = '\'\\" t\n.pc\n.TH PCBASIC 1\n' + html_to_text(docroot)
+    manpage = '\'\\" t\n.pc\n.TH PCBASIC 1\n' + convert_html(docroot)
     # replace two starting spaces (not sure where from)
     return re.sub('\t +', '\t', re.sub('\n +', '\n', manpage))
 
