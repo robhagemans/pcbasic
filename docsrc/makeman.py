8be79d232e29d315c8754e9d686b300a66aab7b8
Rob Hagemans <robhagemans@yahoo.co.uk>
2015-09-07 11:48:47 +0200

    compile usage through python modules only

--- docsrc/makeman.py
+++ docsrc/makeman.py
@@ -8,16 +8,17 @@ import subprocess
 import time
 import os
 import gzip
+import textwrap
 
 
 def html_to_man(html):
     def massage(text):
         return re.sub(' +', ' ', text.encode('utf-8').replace('\\', '\\\\').replace('-', '\\-').replace('|', '\\||').replace('.', '\\|.').replace('\n', ' '))
 
-    def convert_html(e, indent='', inside=False):
+    def convert_html(e, indent=0, inside=False):
         inner = massage(e.text) if e.text else ''
         tail = massage(e.tail) if e.tail else ''
-        children = ''.join(convert_html(child, '  '+indent, inside or e.tag.upper()=='DD') for child in e.iterchildren(tag=etree.Element))
+        children = ''.join(convert_html(child, 1+indent, inside or e.tag.upper()=='DD') for child in e.iterchildren(tag=etree.Element))
         if e.tag.upper() == 'H1':
             return '\n.SH NAME\n' + inner.lower().replace('\-', '') + ' \- '
         elif e.tag.upper() == 'H2':
@@ -48,6 +49,41 @@ def html_to_man(html):
     return re.sub('\t +', '\t', re.sub('\n +', '\n', manpage))
 
 
+
+def html_to_text(html):
+    def massage(text):
+        return re.sub(' +', ' ', text.replace('\n', ' '))
+
+    def convert_html(e, indent=0, inside=False):
+        inner = massage(e.text) if e.text else ''
+        tail = massage(e.tail) if e.tail else ''
+        children = ''.join(convert_html(child, 1+indent, inside or e.tag.upper()=='DD') for child in e.iterchildren(tag=etree.Element))
+        if e.tag.upper() == 'H1':
+            return ''
+        elif e.tag.upper() == 'H2':
+            text = ('\n'+'\t'*indent).join(textwrap.wrap((inner.upper() + children.upper()).strip(), replace_whitespace=False))
+            return '\n' + '\t'*indent + text + '\n' + '\t'*indent + tail
+        elif e.tag.upper() in ('P', 'DT'):
+            text = ('\n' + '\t'*indent).join(textwrap.wrap((inner + children).strip(), replace_whitespace=False))
+            return '\n' + '\t'*indent  + text + '\n' + '\t'*indent + tail
+        elif e.tag.upper() == 'DD':
+            text = ('\n' + '\t'*indent + '\t').join(textwrap.wrap((inner + children).strip(), replace_whitespace=False))
+            return '\t'*indent + text + '\n' + '\t'*indent + tail
+        elif e.tag.upper() == 'CODE' and e.get("class") == "block":
+            text = ('\n'+'\t'*indent).join(textwrap.wrap((inner + children).strip(), replace_whitespace=False))
+            return '\n' + '\t'*indent + text + '\n'+ '\t'*indent + tail
+        #elif e.tag.upper() == 'DL':
+        #    text = ('\n' + '\t'*indent).join(textwrap.wrap((inner + children).strip(), replace_whitespace=False))
+        #    return '\n' + '\t'*indent + text + '\n' + tail
+        else:
+            return (inner + children + tail)
+
+    parser = etree.HTMLParser(encoding='utf-8')
+    doc = etree.parse(StringIO(html), parser)
+    docroot = doc.getroot()
+    return convert_html(docroot, indent=-3)
+
+
 title_html = '<h1>pcbasic</h1><p>%s</p>\n' % open('tagline.txt', mode='r').read()
 desc_html = '<h2>Description</h2><p>%s</p>\n' % open('description.txt', mode='r').read()
 options_html = open('options.html', mode='r').read()
@@ -60,9 +96,9 @@ with gzip.open('../doc/pcbasic.1.gz', 'wb') as manfile:
     manfile.write(html_to_man(man_html))
 
 # output usage
-with open('usage.man', 'w') as manfile:
-    manfile.write(html_to_man(usage_html))
+with codecs.open('usage.txt', 'w', 'utf-8') as textfile:
+    textfile.write(html_to_text(usage_html))
 
 # constructing the pipes through popen seems to cut short the file some way or another
-subprocess.call('cat usage.man | groff -t -e -mandoc -Tascii  | col -bx | tail -n +5 | head -n -5 > ../pcbasic/data/usage.txt', shell=True)
-os.remove('usage.man')
+#subprocess.call('cat usage.man | groff -t -e -mandoc -Tascii  | col -bx | tail -n +5 | head -n -5 > ../pcbasic/data/usage.txt', shell=True)
+#os.remove('usage.man')
