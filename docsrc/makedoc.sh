a295e5975cb07b664e2f4dcfacf928a7cfb5c7ec
Rob Hagemans <robhagemans@yahoo.co.uk>
2015-09-08 21:42:03 +0100

    docs: add HRs to improve readability on unstyled browsers

--- docsrc/makedoc.sh
+++ docsrc/makedoc.sh
@@ -20,8 +20,8 @@ cp LICENSE.md ../doc
 ./mdtohtml.py LICENSE.md doclicense.html
 
 (echo "<article>"; ./mdtohtml.py ../README.md /dev/stdout; echo "</article>") | sed -e "s/h3/h1/g" -e "s/h4/h2/g" -e "s_PC-BASIC</h1>_Overview</h1>_"> quickstart.html
-(echo -e "<footer>\n<h1 id=\"licence\">Licences</h1>"; cat pcbasiclicense.html doclicense.html; echo "</footer>") > licences.html
-(echo -e "<article>"; cat settings.html options.html examples.html; echo "</article>") > settings-options.html
+(echo -e "<footer>\n<h1 id=\"licence\">Licences</h1>"; cat pcbasiclicense.html; echo -e "<hr />\n"; cat doclicense.html; echo -e "\n</footer>") > licences.html
+(echo -e "<article>"; cat settings.html; echo -e "<hr />\n"; cat options.html examples.html; echo "</article>") > settings-options.html
 (cat quickstart.html documentation.html settings-options.html reference.html techref.html acknowledgements.html licences.html footer.html) > predoc.html
 ./maketoc.py predoc.html > toc.html
 echo -e "<header>\n<h1>PC-BASIC $(cat ../pcbasic/data/version.txt) documentation</h1>\n<small>Documentation compiled on $(date --utc).</small>\n</header>" > subheader.html
