1404f8fe106409b464fbf1300857ff68aba6d2e0
Rob Hagemans <robhagemans@yahoo.co.uk>
2015-09-09 22:52:46 +0100

    replace makedoc shell script with makedoc.py

---
+++ docsrc/makedoc.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python2
+import sys
+import os
+import shutil
+from cStringIO import StringIO
+from datetime import datetime
+import codecs
+
+from lxml import etree
+import markdown
+
+def mdtohtml(md_file, outf):
+    with codecs.open(md_file, 'r', 'utf-8') as inf:
+        md = inf.read()
+        outf.write(markdown.markdown(md, extensions=['markdown.extensions.tables', 'markdown.extensions.toc'], output_format='html5', lazy_ol=False).encode('utf-8'))
+
+def maketoc(html_doc, toc):
+    parser = etree.HTMLParser()
+    doc = etree.parse(html_doc, parser)
+    last = -1
+    toc.write('<nav class="toc">\n')
+    toc.write('    <h2 id="toc">Table of Contents</h2>\n')
+    for node in doc.xpath('//h2|//h3|//h4'):
+        level = int(node.tag[1])
+        node_id = node.get('id')
+        if last == -1:
+            last += level
+            first = last
+        node.tag = 'a'
+        node.attrib.clear()
+        if node_id:
+            node.set('href', '#' + node_id)
+        if level-last < 0:
+            toc.write('</li>\n')
+            for i in range((last-level), 0, -1):
+                toc.write('    '*((level+i-1)*2+1) + '</ul>\n')
+                toc.write('    '*(level+i-1)*2 + '</li>\n')
+        elif level-last > 0:
+            toc.write('\n')
+            for i in range(level-last, 0, -1):
+                toc.write('    '*((level-i)*2+1) + '<ul>\n')
+        else:
+            toc.write('</li>\n')
+        toc.write('    '*(level*2) + '<li>' + etree.tostring(node).strip())
+        last = level
+    toc.write('</li>\n')
+    while level > first:
+        toc.write('    '*(level*2-1) + '</ul>\n')
+        level -= 1
+    toc.write('</nav>\n')
+
+
+def makedoc(header=None, output=None):
+    header = header or 'header.html'
+    output = output or '../doc/PC-BASIC_documentation.html'
+    try:
+        os.mkdir('../doc')
+    except OSError:
+        # already there, ignore
+        pass
+    shutil.copy('doc.css', '../doc/')
+    shutil.copy('LICENSE.md', '../doc/')
+    basic_license_stream = StringIO()
+    doc_license_stream = StringIO()
+    readme_stream = StringIO()
+    mdtohtml('../LICENSE.md', basic_license_stream)
+    mdtohtml('LICENSE.md', doc_license_stream)
+    mdtohtml('../README.md', readme_stream)
+    quickstart_html = ('<article>\n' + readme_stream.getvalue() + '</article>\n').replace('h3', 'h2').replace('h4', 'h3').replace('PC-BASIC</h2>', 'Overview</h2>')
+    licenses_html = '<footer>\n<h2 id="licence">Licences</h2>\n' + basic_license_stream.getvalue() + '<hr />\n' + doc_license_stream.getvalue() + '\n</footer>\n'
+    settings_html = '<article>\n' + open('settings.html', 'r').read() + '<hr />\n' + open('options.html', 'r').read() + open('examples.html', 'r').read() + '</article>\n'
+    predoc = StringIO()
+    predoc.write(quickstart_html)
+    predoc.write(open('documentation.html', 'r').read())
+    predoc.write(settings_html)
+    predoc.write(open('reference.html', 'r').read())
+    predoc.write(open('techref.html', 'r').read())
+    predoc.write(open('acknowledgements.html', 'r').read())
+    predoc.write(licenses_html)
+    predoc.write(open('footer.html', 'r').read())
+    predoc.seek(0)
+    toc = StringIO()
+    maketoc(predoc, toc)
+    now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    version = open('../pcbasic/data/version.txt', 'r').read().strip()
+    subheader_html = '<header>\n<h1>PC-BASIC {0} documentation</h1>\n<small>Documentation compiled on {1}.</small>\n</header>\n'.format(version, now)
+    with open(output, 'w') as outf:
+        outf.write(open(header, 'r').read())
+        outf.write(subheader_html)
+        outf.write(toc.getvalue())
+        outf.write(predoc.getvalue())
+
+if __name__ == '__main__':
+    header, output = None, None
+    try:
+        header = sys.argv[1]
+        output = sys.argv[2]
+    except IndexError:
+        pass
+    makedoc(header, output)
