99de0a81481e81b6b2b1896230a793115d5b4772
Rob Hagemans <robhagemans@yahoo.co.uk>
2015-09-09 22:58:20 +0100

    prepare.py: replace subprocess.call hack with sys.path.insert hack

--- prepare.py
+++ prepare.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python2
-import subprocess
+import sys
 import os
 cwd = os.getcwd()
 os.chdir('docsrc')
-print os.getcwd()
-subprocess.call(['./makeusage.py'])
-subprocess.call(['./makeman.py'])
-subprocess.call(['./makedoc.py'])
+sys.path.insert(0, os.getcwd())
+from makeusage import makeusage
+from makeman import makeman
+from makedoc import makedoc
+makeusage()
+makeman()
+makedoc()
 os.chdir(cwd)
