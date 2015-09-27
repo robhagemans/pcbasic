2a0e8ac1d2243d9ef74454840242b71373be76ff
Rob Hagemans <robhagemans@yahoo.co.uk>
2015-09-27 21:58:48 +0100

    re-implement non-numpy graphics operations

--- pcbasic/backend.py
+++ pcbasic/backend.py
@@ -17,7 +17,10 @@ from copy import copy
 import time
 import Queue
 
-import numpy
+try:
+    import numpy
+except ImportError:
+    numpy = None
 
 import plat
 import config
@@ -902,28 +905,20 @@ class PixelBuffer(object):
             srcrow = self.pages[src].row[x]
             dstrow.buf[:] = srcrow.buf[:]
 
-#if numpy:
 class PixelPage(object):
     """ Buffer for a screen page. """
 
     def __init__(self, bwidth, bheight, pagenum, bitsperpixel):
         """ Initialise the screen buffer to given dimensions. """
-        self.buffer = numpy.zeros((bheight, bwidth), dtype=numpy.int8)
+        if numpy:
+            self.buffer = numpy.zeros((bheight, bwidth), dtype=numpy.int8)
+        else:
+            self.buffer = [[0]*bwidth for _ in range(bheight)]
         self.width = bwidth
         self.height = bheight
         self.pagenum = pagenum
         self.bitsperpixel = bitsperpixel
         self.init_operations()
-
-    def init_operations(self):
-        """ Initialise operations closures. """
-        self.operations = {
-            tk.PSET: lambda x, y: x.__setitem__(slice(len(x)), y),
-            tk.PRESET: lambda x, y: x.__setitem__(slice(len(x)), y.__xor__((1<<self.bitsperpixel) - 1)),
-            tk.AND: lambda x, y: x.__iand__(y),
-            tk.OR: lambda x, y: x.__ior__(y),
-            tk.XOR: lambda x, y: x.__ixor__(y),
-        }
 
     def __getstate__(self):
         """ Pickle the page. """
@@ -940,96 +935,183 @@ class PixelPage(object):
     def put_pixel(self, x, y, attr):
         """ Put a pixel in the buffer. """
         try:
-            self.buffer[y, x] = attr
+            self.buffer[y][x] = attr
         except IndexError:
             pass
 
     def get_pixel(self, x, y):
         """ Get attribute of a pixel in the buffer. """
         try:
-            return self.buffer[y, x]
+            return self.buffer[y][x]
         except IndexError:
             return 0
 
     def fill_interval(self, x0, x1, y, attr):
         """ Write a list of attributes to a scanline interval. """
         try:
-            self.buffer[y, x0:x1+1].fill(attr)
+            self.buffer[y][x0:x1+1] = [attr]*(x1-x0+1)
         except IndexError:
             pass
 
-    def put_interval(self, x, y, colours, mask=0xff):
-        """ Write a list of attributes to a scanline interval. """
-        colours = numpy.array(colours).astype(int)
-        inv_mask = 0xff ^ mask
-        colours &= mask
-        try:
-            self.buffer[y, x:x+len(colours)] &= inv_mask
-            self.buffer[y, x:x+len(colours)] |= colours
-            return self.buffer[y, x:x+len(colours)]
-        except IndexError:
-            return numpy.zeros(len(colours), dtype=numpy.int8)
+    if numpy:
+        def init_operations(self):
+            """ Initialise operations closures. """
+            self.operations = {
+                tk.PSET: lambda x, y: x.__setitem__(slice(len(x)), y),
+                tk.PRESET: lambda x, y: x.__setitem__(slice(len(x)), y.__xor__((1<<self.bitsperpixel) - 1)),
+                tk.AND: lambda x, y: x.__iand__(y),
+                tk.OR: lambda x, y: x.__ior__(y),
+                tk.XOR: lambda x, y: x.__ixor__(y),
+            }
 
-    def get_interval(self, x, y, length):
-        """ Return *view of* attributes of a scanline interval. """
-        try:
-            return self.buffer[y, x:x+length]
-        except IndexError:
-            return numpy.zeros(length, dtype=numpy.int8)
+        def put_interval(self, x, y, colours, mask=0xff):
+            """ Write a list of attributes to a scanline interval. """
+            colours = numpy.array(colours).astype(int)
+            inv_mask = 0xff ^ mask
+            colours &= mask
+            try:
+                self.buffer[y, x:x+len(colours)] &= inv_mask
+                self.buffer[y, x:x+len(colours)] |= colours
+                return self.buffer[y, x:x+len(colours)]
+            except IndexError:
+                return numpy.zeros(len(colours), dtype=numpy.int8)
 
-    def fill_rect(self, x0, y0, x1, y1, attr):
-        """ Apply numpy array [y][x] of attribytes to an area. """
-        if (x1 < x0) or (y1 < y0):
-            return
-        try:
-            self.buffer[y0:y1+1, x0:x1+1].fill(attr)
-        except IndexError:
-            pass
+        def get_interval(self, x, y, length):
+            """ Return *view of* attributes of a scanline interval. """
+            try:
+                return self.buffer[y, x:x+length]
+            except IndexError:
+                return numpy.zeros(length, dtype=numpy.int8)
 
-    def put_rect(self, x0, y0, x1, y1, array, operation_token):
-        """ Apply numpy array [y][x] of attribytes to an area. """
-        if (x1 < x0) or (y1 < y0):
-            return
-        try:
-            self.operations[operation_token](self.buffer[y0:y1+1, x0:x1+1], numpy.asarray(array))
-            return self.buffer[y0:y1+1, x0:x1+1]
-        except IndexError:
-            return numpy.zeros((y1-y0+1, x1-x0+1), dtype=numpy.int8)
+        def fill_rect(self, x0, y0, x1, y1, attr):
+            """ Apply solid attribute to an area. """
+            if (x1 < x0) or (y1 < y0):
+                return
+            try:
+                self.buffer[y0:y1+1, x0:x1+1].fill(attr)
+            except IndexError:
+                pass
 
-    def get_rect(self, x0, y0, x1, y1):
-        """ Get *copy of* numpy array [y][x] of target area. """
-        try:
-            # our only user in module graphics needs a copy, so copy.
-            return numpy.array(self.buffer[y0:y1+1, x0:x1+1])
-        except IndexError:
-            return numpy.zeros((y1-y0+1, x1-x0+1), dtype=numpy.int8)
+        def put_rect(self, x0, y0, x1, y1, array, operation_token):
+            """ Apply numpy array [y][x] of attributes to an area. """
+            if (x1 < x0) or (y1 < y0):
+                return
+            try:
+                self.operations[operation_token](self.buffer[y0:y1+1, x0:x1+1], numpy.asarray(array))
+                return self.buffer[y0:y1+1, x0:x1+1]
+            except IndexError:
+                return numpy.zeros((y1-y0+1, x1-x0+1), dtype=numpy.int8)
 
-    def move_rect(self, sx0, sy0, sx1, sy1, tx0, ty0):
-        """ Move pixels from an area to another, replacing with attribute 0. """
-        w, h = sx1-sx0+1, sy1-sy0+1
-        area = numpy.array(self.buffer[sy0:sy1+1, sx0:sx1+1])
-        self.buffer[sy0:sy1+1, sx0:sx1+1] = numpy.zeros((h, w), dtype=numpy.int8)
-        self.buffer[ty0:ty0+h, tx0:tx0+w] = area
+        def get_rect(self, x0, y0, x1, y1):
+            """ Get *copy of* numpy array [y][x] of target area. """
+            try:
+                # our only user in module graphics needs a copy, so copy.
+                return numpy.array(self.buffer[y0:y1+1, x0:x1+1])
+            except IndexError:
+                return numpy.zeros((y1-y0+1, x1-x0+1), dtype=numpy.int8)
 
-    def get_until(self, x0, x1, y, c):
-        """ Get the attribute values of a scanline interval [x0, x1-1]. """
-        if x0 == x1:
-            return []
-        toright = x1 > x0
-        if not toright:
-            x0, x1 = x1+1, x0+1
-        try:
-            arr = self.buffer[y, x0:x1]
-        except IndexError:
-            return []
-        found = numpy.where(arr == c)
-        if len(found[0]) > 0:
-            if toright:
-                arr = arr[:found[0][0]]
-            else:
-                arr = arr[found[0][-1]+1:]
-        return list(arr.flatten())
+        def move_rect(self, sx0, sy0, sx1, sy1, tx0, ty0):
+            """ Move pixels from an area to another, replacing with attribute 0. """
+            w, h = sx1-sx0+1, sy1-sy0+1
+            area = numpy.array(self.buffer[sy0:sy1+1, sx0:sx1+1])
+            self.buffer[sy0:sy1+1, sx0:sx1+1] = numpy.zeros((h, w), dtype=numpy.int8)
+            self.buffer[ty0:ty0+h, tx0:tx0+w] = area
 
+        def get_until(self, x0, x1, y, c):
+            """ Get the attribute values of a scanline interval [x0, x1-1]. """
+            if x0 == x1:
+                return []
+            toright = x1 > x0
+            if not toright:
+                x0, x1 = x1+1, x0+1
+            try:
+                arr = self.buffer[y, x0:x1]
+            except IndexError:
+                return []
+            found = numpy.where(arr == c)
+            if len(found[0]) > 0:
+                if toright:
+                    arr = arr[:found[0][0]]
+                else:
+                    arr = arr[found[0][-1]+1:]
+            return list(arr.flatten())
+
+    else:
+        def init_operations(self):
+            """ Initialise operations closures. """
+            self.operations = {
+                tk.PSET: lambda x, y: y,
+                tk.PRESET: lambda x, y: y ^ ((1<<self.bitsperpixel)-1),
+                tk.AND: lambda x, y: x & y,
+                tk.OR: lambda x, y: x | y,
+                tk.XOR: lambda x, y: x ^ y,
+            }
+
+        def put_interval(self, x, y, colours, mask=0xff):
+            """ Write a list of attributes to a scanline interval. """
+            if mask != 0xff:
+                inv_mask = 0xff ^ mask
+                self.buffer[y][x:x+len(colours)] = [(c & mask) |
+                                                (self.buffer[y][x+i] & inv_mask)
+                                                for i,c in enumerate(colours)]
+            return self.buffer[y][x:x+len(colours)]
+
+        def get_interval(self, x, y, length):
+            """ Return *view of* attributes of a scanline interval. """
+            try:
+                return self.buffer[y][x:x+length]
+            except IndexError:
+                return [0] * length
+
+        def fill_rect(self, x0, y0, x1, y1, attr):
+            """ Apply solid attribute to an area. """
+            if (x1 < x0) or (y1 < y0):
+                return
+            try:
+                for y in range(y0, y1+1):
+                    self.buffer[y][x0:x1+1] = [attr] * (x1-x0+1)
+            except IndexError:
+                pass
+
+        def put_rect(self, x0, y0, x1, y1, array, operation_token):
+            """ Apply 2d list [y][x] of attributes to an area. """
+            if (x1 < x0) or (y1 < y0):
+                return
+            try:
+                for y in range(y0, y1+1):
+                    self.buffer[y][x0:x1+1] = [
+                        [self.operations[operation_token](a, b)
+                        for a, b in zip(self.buffer[y][x0:x1+1], array)]]
+                return [self.buffer[y][x0:x1+1] for y in range(y0, y1+1)]
+            except IndexError:
+                return [[0]*(x1-x0+1) for _ in range(y1-y0+1)]
+
+        def get_rect(self, x0, y0, x1, y1):
+            """ Get *copy of* 2d list [y][x] of target area. """
+            try:
+                return [self.buffer[y][x0:x1+1] for y in range(y0, y1+1)]
+            except IndexError:
+                return [[0]*(x1-x0+1) for _ in range(y1-y0+1)]
+
+        def move_rect(self, sx0, sy0, sx1, sy1, tx0, ty0):
+            """ Move pixels from an area to another, replacing with attribute 0. """
+            for y in range(0, sy1-sy0+1):
+                row = self.buffer[sy0+y][sx0:sx1+1]
+                self.buffer[sy0+y][sx0:sx1+1] = [0] * (sx1-sx0+1)
+                self.buffer[ty0+y][tx0:tx0+(sx1-sx0+1)] = row
+
+        def get_until(self, x0, x1, y, c):
+            """ Get the attribute values of a scanline interval [x0, x1-1]. """
+            if x0 == x1:
+                return []
+            toright = x1 > x0
+            if not toright:
+                x0, x1 = x1+1, x0+1
+            try:
+                index = self.buffer[y][x0:x1].index(c)
+            except ValueError:
+                index = x1-x0
+            return self.buffer[y][x0:x0+index]
 
 ###############################################################################
 # screen operations
@@ -1748,16 +1830,24 @@ class Screen(object):
                 video_queue.put(Event(VIDEO_BUILD_GLYPHS, {c: mask}))
         return mask
 
-    #if numpy:
-    def glyph_to_rect(self, row, col, mask, fore, back):
-        """ Return a sprite for a given character """
-        # set background
-        glyph = numpy.full(mask.shape, back)
-        # stamp foreground mask
-        glyph[mask] = fore
-        x0, y0 = (col-1) * self.mode.font_width, (row-1) * self.mode.font_height
-        x1, y1 = x0 + mask.shape[1] - 1, y0 + mask.shape[0] - 1
-        return x0, y0, x1, y1, glyph
+    if numpy:
+        def glyph_to_rect(self, row, col, mask, fore, back):
+            """ Return a sprite for a given character """
+            # set background
+            glyph = numpy.full(mask.shape, back)
+            # stamp foreground mask
+            glyph[mask] = fore
+            x0, y0 = (col-1) * self.mode.font_width, (row-1) * self.mode.font_height
+            x1, y1 = x0 + mask.shape[1] - 1, y0 + mask.shape[0] - 1
+            return x0, y0, x1, y1, glyph
+    else:
+        def glyph_to_rect(self, row, col, mask, fore, back):
+            """ Return a sprite for a given character """
+            glyph = [[(fore if bit else back) for bit in row] for row in mask]
+            x0, y0 = (col-1) * self.mode.font_width, (row-1) * self.mode.font_height
+            x1, y1 = x0 + len(mask[0]) - 1, y0 + len(mask) - 1
+            return x0, y0, x1, y1, glyph
+
 
     #MOVE to modes classes in modes.py
     def split_attr(self, attr):
