e3c9636d6e6a875c54a0f967b2f7cb20454cead1
Rob Hagemans <robhagemans@yahoo.co.uk>
2015-11-30 20:45:06 +0000

    remove discontinued pgs4a android packaging directory

--- packaging/android/prepare-build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-rsync -rvp pc-basic build/ --delete --exclude='patches/' --exclude='test/' --exclude='data/*.SAV' --exclude='*.pyc' --exclude='*.pyo' --exclude='*~' --exclude='.git*' --delete-excluded
-cp build/pc-basic/pcbasic build/pc-basic/main.py
