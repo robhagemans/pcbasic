e3e5522b0d5e2c8e597e6fc7705dbb2bb07e3dab
Rob Hagemans <robhagemans@yahoo.co.uk>
2016-01-16 19:57:02 +0000

    belated happy new year 2016

--- pcbasic/console.py
+++ pcbasic/console.py
@@ -2,7 +2,7 @@
 PC-BASIC - console.py
 Console front-end
 
-(c) 2013, 2014, 2015 Rob Hagemans
+(c) 2013, 2014, 2015, 2016 Rob Hagemans
 This file is released under the GNU GPL version 3.
 """
 
--- pcbasic/draw_and_play.py
+++ pcbasic/draw_and_play.py
@@ -2,7 +2,7 @@
 PC-BASIC - draw_and_play.py
 DRAW and PLAY macro language stream utilities
 
-(c) 2013, 2014, 2015 Rob Hagemans
+(c) 2013, 2014, 2015, 2016 Rob Hagemans
 This file is released under the GNU GPL version 3.
 """
 
