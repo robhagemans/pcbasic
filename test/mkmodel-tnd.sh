9a9cd23d09f39597bf4e0a4b2a5b5878cc89f191
Rob Hagemans <robhagemans@yahoo.co.uk>
2016-03-13 13:31:32 +0000

    add test model build scripts for double-precision and tandy

---
+++ test/mkmodel-tnd.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+name="$1"
+cd "$name"
+mkdir model
+cp -R * model/
+cd model/
+# remove model/model created by cp -R
+rm -r model/
+# remove the output directory which was copied along if it existed
+rm -r output/
+GWBASIC=$(cat ../../gwbasicdir)
+echo "gwbasic dir: $GWBASIC"
+dosbox -c "MOUNT C ." -c "MOUNT E $GWBASIC" -c "C:" -c "E:\GWBASIC\GW-TANDY TEST.BAS" -c "EXIT"
+cd ../..
