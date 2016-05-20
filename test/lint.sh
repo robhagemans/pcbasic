518073a1acd149371aa763b4f51b779afe424c6a
Rob Hagemans <robhagemans@yahoo.co.uk>
2016-05-20 08:28:07 +0100

    make pylint script output cleaner

--- test/lint.sh
+++ test/lint.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-for file in ../pcbasic/*.py ../pcbasic/*/*.py; do pylint -E --ignored-modules=pygame,numpy,pygame.mixer --enable=cyclic-import,relative-import --disable=too-many-function-args $file; done
+exclude=../pcbasic/ansipipe/example.py
+for file in ../pcbasic/*.py ../pcbasic/*/*.py
+do
+  if [[ "$file" != "$exclude" ]]
+  then
+    echo "$file"
+    pylint -E --ignored-modules=pygame,numpy,pygame.mixer --enable=cyclic-import,relative-import --disable=too-many-function-args "$file"
+  fi
+done
