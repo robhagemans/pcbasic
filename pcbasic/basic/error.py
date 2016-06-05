7d7c80fd214b8ed399b6d884362d21575944a262
Rob Hagemans <robhagemans@yahoo.co.uk>
2016-06-05 22:18:04 +0100

    reorganise error module

--- pcbasic/basic/error.py
+++ pcbasic/basic/error.py
@@ -70,77 +70,23 @@ DEADLOCK = 77
 STX = SYNTAX_ERROR
 IFC = ILLEGAL_FUNCTION_CALL
 
-default_msg = 'Unprintable error'
-errors = {
-    1: 'NEXT without FOR',
-    2: 'Syntax error',
-    3: 'RETURN without GOSUB',
-    4: 'Out of DATA',
-    5: 'Illegal function call',
-    6: 'Overflow',
-    7: 'Out of memory',
-    8: 'Undefined line number',
-    9: 'Subscript out of range',
-    10: 'Duplicate Definition',
-    11: 'Division by zero',
-    12: 'Illegal direct',
-    13: 'Type mismatch',
-    14: 'Out of string space',
-    15: 'String too long',
-    16: 'String formula too complex',
-    17: "Can't continue",
-    18: 'Undefined user function',
-    19: 'No RESUME',
-    20: 'RESUME without error',
-    # 21
-    22: 'Missing operand',
-    23: 'Line buffer overflow',
-    24: 'Device Timeout',
-    25: 'Device Fault',
-    26: 'FOR without NEXT',
-    27: 'Out of paper',
-    # 28
-    29: 'WHILE without WEND',
-    30: 'WEND without WHILE',
-    # 31--49
-    50: 'FIELD overflow',
-    51: 'Internal error',
-    52: 'Bad file number',
-    53: 'File not found',
-    54: 'Bad file mode',
-    55: 'File already open',
-    # 56
-    57: 'Device I/O error',
-    58: 'File already exists',
-    # 59--60
-    61: 'Disk full',
-    62: 'Input past end',
-    63: 'Bad record number',
-    64: 'Bad file name',
-    # 65
-    66: 'Direct statement in file',
-    67: 'Too many files',
-    68: 'Device Unavailable',
-    69: 'Communication buffer overflow',
-    70: 'Permission Denied',
-    71: 'Disk not Ready',
-    72: 'Disk media error',
-    73: 'Advanced Feature',
-    74: 'Rename across disks',
-    75: 'Path/File access error',
-    76: 'Path not found',
-    77: 'Deadlock',
-}
-
 
 class Error(Exception):
     """Base type for exceptions."""
 
+    def __str__(self):
+        """String representation of exception."""
+        return self.message
+
+
 class Exit(Error):
     """Exit emulator."""
+    message = 'Exit'
+
 
 class Reset(Exit):
     """Reset emulator."""
+    message = 'Reset'
 
 
 class Break(Error):
@@ -156,16 +102,74 @@ class Break(Error):
 class RunError(Error):
     """Runtime error."""
 
+    default_message = 'Unprintable error'
+    messages = {
+        1: 'NEXT without FOR',
+        2: 'Syntax error',
+        3: 'RETURN without GOSUB',
+        4: 'Out of DATA',
+        5: 'Illegal function call',
+        6: 'Overflow',
+        7: 'Out of memory',
+        8: 'Undefined line number',
+        9: 'Subscript out of range',
+        10: 'Duplicate Definition',
+        11: 'Division by zero',
+        12: 'Illegal direct',
+        13: 'Type mismatch',
+        14: 'Out of string space',
+        15: 'String too long',
+        16: 'String formula too complex',
+        17: "Can't continue",
+        18: 'Undefined user function',
+        19: 'No RESUME',
+        20: 'RESUME without error',
+        # 21
+        22: 'Missing operand',
+        23: 'Line buffer overflow',
+        24: 'Device Timeout',
+        25: 'Device Fault',
+        26: 'FOR without NEXT',
+        27: 'Out of paper',
+        # 28
+        29: 'WHILE without WEND',
+        30: 'WEND without WHILE',
+        # 31--49
+        50: 'FIELD overflow',
+        51: 'Internal error',
+        52: 'Bad file number',
+        53: 'File not found',
+        54: 'Bad file mode',
+        55: 'File already open',
+        # 56
+        57: 'Device I/O error',
+        58: 'File already exists',
+        # 59--60
+        61: 'Disk full',
+        62: 'Input past end',
+        63: 'Bad record number',
+        64: 'Bad file name',
+        # 65
+        66: 'Direct statement in file',
+        67: 'Too many files',
+        68: 'Device Unavailable',
+        69: 'Communication buffer overflow',
+        70: 'Permission Denied',
+        71: 'Disk not Ready',
+        72: 'Disk media error',
+        73: 'Advanced Feature',
+        74: 'Rename across disks',
+        75: 'Path/File access error',
+        76: 'Path not found',
+        77: 'Deadlock',
+    }
+
     def __init__(self, value, pos=None):
         """Initialise error."""
         Error.__init__(self)
         self.err = value
         self.pos = pos
-
-    @property
-    def message(self):
-        """Get error message."""
         try:
-            return errors[self.err]
+            self.message = self.messages[self.err]
         except KeyError:
-            return default_msg
+            self.message = self.default_message
