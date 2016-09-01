f12c3d21ad1ea0c9a0f3886e5477f02532988524
Rob Hagemans <robhagemans@yahoo.co.uk>
2016-09-01 21:54:19 +0100

    Arrays: split dict of tuples into three dicts, mark as private

--- pcbasic/basic/arrays.py
+++ pcbasic/basic/arrays.py
@@ -17,34 +17,38 @@ class Arrays(object):
 
     def __init__(self, memory, values):
         """Initialise arrays."""
-        self.memory = memory
-        self.values = values
+        self._memory = memory
+        self._values = values
         self.clear()
         # OPTION BASE is unset
-        self.base_index = None
+        self._base = None
 
     def __contains__(self, varname):
         """Check if a scalar has been defined."""
-        return varname in self.arrays
+        return varname in self._dims
 
     def __iter__(self):
         """Return an iterable over all scalar names."""
-        return self.arrays.iterkeys()
+        return self._dims.iterkeys()
 
     def __str__(self):
         """Debugging representation of variable dictionary."""
-        return '\n'.join('%s%s: %s' % (n, v[0], str(v[1]).encode('hex')) for n, v in self.arrays.iteritems())
+        return '\n'.join('%s%s: %s' % (n, v, str(self._buffers[n]).encode('hex')) for n, v in self._dims.iteritems())
 
     def clear(self):
         """Clear arrays."""
-        self.arrays = {}
-        self.array_memory = {}
+        self._dims = {}
+        self._buffers = {}
+        self._versions = {}
+        self._array_memory = {}
         self.current = 0
 
     def erase(self, name):
         """Remove an array from memory."""
         try:
-            del self.arrays[name]
+            del self._dims[name]
+            del self._buffers[name]
+            del self._versions[name]
         except KeyError:
             # illegal fn call
             raise error.RunError(error.IFC)
@@ -54,9 +58,9 @@ class Arrays(object):
         bigindex = 0
         area = 1
         for i in range(len(index)):
-            # dimensions is the *maximum index number*, regardless of self.base_index
-            bigindex += area*(index[i]-self.base_index)
-            area *= (dimensions[i]+1-self.base_index)
+            # dimensions is the *maximum index number*, regardless of self._base
+            bigindex += area * (index[i] - self._base)
+            area *= dimensions[i] + 1 - self._base
         return bigindex
 
     def array_len(self, dimensions):
@@ -66,37 +70,37 @@ class Arrays(object):
     def array_size_bytes(self, name):
         """Return the byte size of an array, if it exists. Return 0 otherwise."""
         try:
-            dimensions, _, _ = self.arrays[name]
+            dimensions = self._dims[name]
         except KeyError:
             return 0
         return self.array_len(dimensions) * values.size_bytes(name)
 
     def view_full_buffer(self, name):
         """Return a memoryview to a full array."""
-        return memoryview(self.arrays[name][1])
+        return memoryview(self._buffers[name])
 
     def dimensions(self, name):
         """Return the dimensions of an array."""
-        return self.arrays[name][0]
+        return self._dims[name]
 
     def version(self, name):
         """Return the update version of an array (for cached graphics)."""
-        return self.arrays[name][2]
+        return self._versions[name]
 
     def inc_version(self, name):
         """Increase the update version of an array (for cached graphics)."""
-        self.arrays[name][2] += 1
+        self._versions[name] += 1
 
     def dim(self, name, dimensions):
         """Allocate array space for an array of given dimensioned size. Raise errors if duplicate name or illegal index value."""
-        if self.base_index is None:
-            self.base_index = 0
-        if name in self.arrays:
+        if self._base is None:
+            self._base = 0
+        if name in self._dims:
             raise error.RunError(error.DUPLICATE_DEFINITION)
         for d in dimensions:
             if d < 0:
                 raise error.RunError(error.IFC)
-            elif d < self.base_index:
+            elif d < self._base:
                 raise error.RunError(error.SUBSCRIPT_OUT_OF_RANGE)
         size = self.array_len(dimensions)
         # update memory model
@@ -105,44 +109,46 @@ class Arrays(object):
         record_len = 1 + max(3, len(name)) + 3 + 2*len(dimensions)
         array_ptr = name_ptr + record_len
         array_bytes = size * values.size_bytes(name)
-        self.memory.check_free(record_len + array_bytes, error.OUT_OF_MEMORY)
+        self._memory.check_free(record_len + array_bytes, error.OUT_OF_MEMORY)
         self.current += record_len + array_bytes
-        self.array_memory[name] = (name_ptr, array_ptr)
-        self.arrays[name] = [dimensions, bytearray(array_bytes), 0]
+        self._array_memory[name] = (name_ptr, array_ptr)
+        self._buffers[name] = bytearray(array_bytes)
+        self._dims[name] = dimensions
+        self._versions[name] = 0
 
     def check_dim(self, name, index):
         """Check if an array has been allocated. If not, auto-allocate if indices are <= 10; raise error otherwise."""
         try:
-            dimensions, lst, _ = self.arrays[name]
+            dimensions = self._dims[name]
         except KeyError:
             # auto-dimension - 0..10 or 1..10
             # this even fixes the dimensions if the index turns out to be out of range
             dimensions = [10] * len(index)
             self.dim(name, dimensions)
-            dimensions, lst, _ = self.arrays[name]
+        lst = self._buffers[name]
         if len(index) != len(dimensions):
             raise error.RunError(error.SUBSCRIPT_OUT_OF_RANGE)
         for i, d in zip(index, dimensions):
             if i < 0:
                 raise error.RunError(error.IFC)
-            elif i < self.base_index or i > d:
-                # dimensions is the *maximum index number*, regardless of self.base_index
+            elif i < self._base or i > d:
+                # dimensions is the *maximum index number*, regardless of self._base
                 raise error.RunError(error.SUBSCRIPT_OUT_OF_RANGE)
         return dimensions, lst
 
     def clear_base(self):
         """Unset the array base."""
-        self.base_index = None
+        self._base = None
 
     def base(self, base):
         """Set the array base to 0 or 1 (OPTION BASE). Raise error if already set."""
         if base not in (1, 0):
             # syntax error
             raise error.RunError(error.STX)
-        if self.base_index is not None and base != self.base_index:
+        if self._base is not None and base != self._base:
             # duplicate definition
             raise error.RunError(error.DUPLICATE_DEFINITION)
-        self.base_index = base
+        self._base = base
 
     def view_buffer(self, name, index):
         """Return a memoryview to an array element."""
@@ -154,22 +160,22 @@ class Arrays(object):
     def get(self, name, index):
         """Retrieve a copy of the value of an array element."""
         # from_bytes makes the copy
-        return self.values.from_bytes(self.view_buffer(name, index))
+        return self._values.from_bytes(self.view_buffer(name, index))
 
     def set(self, name, index, value):
         """Assign a value to an array element."""
         # copy value into array
-        self.view_buffer(name, index)[:] = self.values.to_bytes(self.values.to_type(name[-1], value))
+        self.view_buffer(name, index)[:] = self._values.to_bytes(self._values.to_type(name[-1], value))
         # increment array version
-        self.arrays[name][2] += 1
+        self._versions[name] += 1
 
     def varptr(self, name, indices):
         """Retrieve the address of an array."""
         try:
-            dimensions, _, _ = self.arrays[name]
-            _, array_ptr = self.array_memory[name]
+            dimensions = self._dims[name]
+            _, array_ptr = self._array_memory[name]
             # arrays are kept at the end of the var list
-            return self.memory.var_current() + array_ptr + values.size_bytes(name) * self.index(indices, dimensions)
+            return self._memory.var_current() + array_ptr + values.size_bytes(name) * self.index(indices, dimensions)
         except KeyError:
             return -1
 
@@ -177,35 +183,35 @@ class Arrays(object):
         """Get a value for an array given its pointer address."""
         found_addr = -1
         found_name = None
-        for name, data in self.array_memory.iteritems():
-            addr = self.memory.var_current() + data[1]
+        for name, data in self._array_memory.iteritems():
+            addr = self._memory.var_current() + data[1]
             if addr > found_addr and addr <= address:
                 found_addr = addr
                 found_name = name
         if not found_name:
             return None
-        _, lst, _ = self.arrays[name]
+        lst = self._buffers[name]
         offset = address - found_addr
-        return self.values.from_bytes(lst[offset : offset+values.size_bytes(name)])
+        return self._values.from_bytes(lst[offset : offset+values.size_bytes(name)])
 
     def get_memory(self, address):
         """Retrieve data from data memory: array space """
         name_addr = -1
         arr_addr = -1
         the_arr = None
-        for name in self.array_memory:
-            name_try, arr_try = self.array_memory[name]
+        for name in self._array_memory:
+            name_try, arr_try = self._array_memory[name]
             if name_try <= address and name_try > name_addr:
                 name_addr, arr_addr = name_try, arr_try
                 the_arr = name
         if the_arr is None:
             return -1
-        var_current = self.memory.var_current()
+        var_current = self._memory.var_current()
         if address >= var_current + arr_addr:
             offset = address - arr_addr - var_current
             if offset >= self.array_size_bytes(the_arr):
                 return -1
-            _, byte_array, _ = self.arrays[the_arr]
+            byte_array = self._buffers[the_arr]
             return byte_array[offset]
         else:
             offset = address - name_addr - var_current
@@ -213,18 +219,18 @@ class Arrays(object):
                 return get_name_in_memory(the_arr, offset)
             else:
                 offset -= max(3, len(the_arr))+1
-                dimensions, _, _ = self.arrays[the_arr]
+                dimensions = self._dims[the_arr]
                 data_rep = struct.pack('<HB', self.array_size_bytes(the_arr) + 1 + 2*len(dimensions), len(dimensions))
                 for d in dimensions:
-                    data_rep += struct.pack('<H', d + 1 - self.base_index)
+                    data_rep += struct.pack('<H', d + 1 - self._base)
                 return data_rep[offset]
 
     def get_strings(self):
         """Return a list of views of string array elements."""
-        return [memoryview(record[1])[i:i+3]
-                    for name, record in self.arrays.iteritems()
+        return [memoryview(buf)[i:i+3]
+                    for name, buf in self._buffers.iteritems()
                         if name[-1] == '$'
-                            for i in range(0, len(record[1]), 3)]
+                            for i in range(0, len(buf), 3)]
 
 
     ###########################################################################
@@ -240,15 +246,15 @@ class Arrays(object):
             return
         if isinstance(python_list[0], list):
             for i, v in enumerate(python_list):
-                self._from_list(v, name, index+[i+(self.base_index or 0)])
+                self._from_list(v, name, index+[i+(self._base or 0)])
         else:
             for i, v in enumerate(python_list):
-                self.set(name, index+[i+(self.base_index or 0)], self.values.from_value(v, name[-1]))
+                self.set(name, index+[i+(self._base or 0)], self._values.from_value(v, name[-1]))
 
     def to_list(self, name):
         """Convert BASIC array to Python list."""
-        if name in self.arrays:
-            indices, _, _ = self.arrays[name]
+        if name in self._dims:
+            indices = self._dims[name]
             return self._to_list(name, [], indices)
         else:
             return []
@@ -258,6 +264,6 @@ class Arrays(object):
         if not remaining_dimensions:
             return []
         elif len(remaining_dimensions) == 1:
-            return [self.values.to_value(self.get(name, index+[i+(self.base_index or 0)])) for i in xrange(remaining_dimensions[0])]
+            return [self._values.to_value(self.get(name, index+[i+(self._base or 0)])) for i in xrange(remaining_dimensions[0])]
         else:
-            return [self._to_list(name, index+[i+(self.base_index or 0)], remaining_dimensions[1:]) for i in xrange(remaining_dimensions[0])]
+            return [self._to_list(name, index+[i+(self._base or 0)], remaining_dimensions[1:]) for i in xrange(remaining_dimensions[0])]
