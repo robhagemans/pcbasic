fe4346ed3867f0196762a697a96da0cc4ab43546
Rob Hagemans <robhagemans@yahoo.co.uk>
2016-09-23 08:24:33 -0400

    move function callbacks out of argument syntax tables

--- pcbasic/basic/expressions.py
+++ pcbasic/basic/expressions.py
@@ -59,95 +59,95 @@ class ExpressionParser(object):
         """Initialise functions."""
         self._with_presign = {
             tk.USR: {
-                None: (1, session.machine.usr_, values.SNG),
-                tk.C_0: (1, session.machine.usr_, values.SNG),
-                tk.C_1: (1, session.machine.usr_, values.SNG),
-                tk.C_2: (1, session.machine.usr_, values.SNG),
-                tk.C_3: (1, session.machine.usr_, values.SNG),
-                tk.C_4: (1, session.machine.usr_, values.SNG),
-                tk.C_5: (1, session.machine.usr_, values.SNG),
-                tk.C_6: (1, session.machine.usr_, values.SNG),
-                tk.C_7: (1, session.machine.usr_, values.SNG),
-                tk.C_8: (1, session.machine.usr_, values.SNG),
-                tk.C_9: (1, session.machine.usr_, values.SNG),
+                None: (1, values.SNG),
+                tk.C_0: (1, values.SNG),
+                tk.C_1: (1, values.SNG),
+                tk.C_2: (1, values.SNG),
+                tk.C_3: (1, values.SNG),
+                tk.C_4: (1, values.SNG),
+                tk.C_5: (1, values.SNG),
+                tk.C_6: (1, values.SNG),
+                tk.C_7: (1, values.SNG),
+                tk.C_8: (1, values.SNG),
+                tk.C_9: (1, values.SNG),
             },
             tk.IOCTL: {
-                '$': (None, self.value_ioctl, None),
+                '$': (None, self.value_ioctl),
             },
             tk.ENVIRON: {
-                '$': (1, dos.environ_, values.STR),
+                '$': (1, values.STR),
             },
             tk.INPUT: {
-                '$': (None, self.value_input, None),
+                '$': (None, self.value_input),
             },
             tk.ERDEV: {
-                '$': (1, session.devices.erdev_str_, values.STR),
-                None: (1, session.devices.erdev_, values.INT),
+                '$': (1, values.STR),
+                None: (1, values.INT),
             },
             tk.VARPTR: {
-                '$': (None, self.value_varptr_str, None),
-                None: (None, self.value_varptr, None),
+                '$': (None, self.value_varptr_str),
+                None: (None, self.value_varptr),
             },
         }
         self._bare = {
-            tk.SCREEN: (3, session.screen.screen_fn_, None, (values.cint_, values.cint_, values.cint_), True),
-            tk.FN: (None, self.value_fn, None),
-            tk.ERL: (0, session.parser.erl_, values.SNG),
-            tk.ERR: (0, session.parser.err_, values.INT),
-            tk.STRING: (None, self.value_string, None),
-            tk.INSTR: (None, self.value_instr, None),
-            tk.CSRLIN: (0, session.screen.csrlin_, values.INT),
-            tk.POINT: (2, session.screen.point_, None, (values.cint_, values.cint_), True),
-            tk.INKEY: (0, session.keyboard.get_char, values.STR),
-            tk.CVI: (1, values.cvi_, None),
-            tk.CVS: (1, values.cvs_, None),
-            tk.CVD: (1, values.cvd_, None),
-            tk.MKI: (1, values.mki_, None),
-            tk.MKS: (1, values.mks_, None),
-            tk.MKD: (1, values.mkd_, None),
-            tk.EXTERR: (1, session.devices.exterr_, values.INT),
-            tk.DATE: (0, session.clock.date_fn_, values.STR),
-            tk.TIME: (0, session.clock.time_fn_, values.STR),
-            tk.PLAY: (1, session.sound.play_fn_, values.INT),
-            tk.TIMER: (0, session.clock.timer_, values.SNG),
-            tk.PMAP: (2, session.screen.pmap_, None, (values.cint_, values.cint_), False),
-            tk.LEFT: (2, values.left_, None, (values.pass_string, values.cint_), False),
-            tk.RIGHT: (2, values.right_, None, (values.pass_string, values.cint_), False),
-            tk.MID: (3, values.mid_, None, (values.pass_string, values.cint_, values.cint_), True),
-            tk.SGN: (1, values.sgn_, None),
-            tk.INT: (1, values.int_, None),
-            tk.ABS: (1, values.abs_, None),
-            tk.SQR: (1, values.sqr_, None),
-            tk.RND: (None, self.value_rnd, None),
-            tk.SIN: (1, values.sin_, None),
-            tk.LOG: (1, values.log_, None),
-            tk.EXP: (1, values.exp_, None),
-            tk.COS: (1, values.cos_, None),
-            tk.TAN: (1, values.tan_, None),
-            tk.ATN: (1, values.atn_, None),
-            tk.FRE: (1, session.memory.fre_, values.SNG),
-            tk.INP: (1, session.machine.inp_, values.INT),
-            tk.POS: (1, session.screen.pos_, values.INT),
-            tk.LEN: (1, values.len_, None),
-            tk.STR: (1, values.str_, None),
-            tk.VAL: (1, values.val_, None),
-            tk.ASC: (1, values.asc_, None),
-            tk.CHR: (1, values.chr_, None),
-            tk.PEEK: (1, session.all_memory.peek_, values.INT),
-            tk.SPACE: (1, values.space_, None),
-            tk.OCT: (1, values.oct_, None),
-            tk.HEX: (1, values.hex_, None),
-            tk.LPOS: (1, session.files.lpos_, values.INT),
-            tk.CINT: (1, values.cint_, None),
-            tk.CSNG: (1, values.csng_, None),
-            tk.CDBL: (1, values.cdbl_, None),
-            tk.FIX: (1, values.fix_, None),
-            tk.PEN: (1, session.events.pen.pen_, values.INT),
-            tk.STICK: (1, session.stick.stick_, values.INT),
-            tk.STRIG: (1, session.stick.strig_, values.INT),
-            tk.EOF: (1, session.files.eof_, values.INT),
-            tk.LOC: (1, session.files.loc_, values.SNG),
-            tk.LOF: (1, session.files.lof_, values.SNG),
+            tk.SCREEN: (3, None, (values.cint_, values.cint_, values.cint_), True),
+            tk.FN: (None, self.value_fn),
+            tk.ERL: (0, values.SNG),
+            tk.ERR: (0, values.INT),
+            tk.STRING: (None, self.value_string),
+            tk.INSTR: (None, self.value_instr),
+            tk.CSRLIN: (0, values.INT),
+            tk.POINT: (2, None, (values.cint_, values.cint_), True),
+            tk.INKEY: (0, values.STR),
+            tk.CVI: (1, None),
+            tk.CVS: (1, None),
+            tk.CVD: (1, None),
+            tk.MKI: (1, None),
+            tk.MKS: (1, None),
+            tk.MKD: (1, None),
+            tk.EXTERR: (1, values.INT),
+            tk.DATE: (0, values.STR),
+            tk.TIME: (0, values.STR),
+            tk.PLAY: (1, values.INT),
+            tk.TIMER: (0, values.SNG),
+            tk.PMAP: (2, None, (values.cint_, values.cint_), False),
+            tk.LEFT: (2, None, (values.pass_string, values.cint_), False),
+            tk.RIGHT: (2, None, (values.pass_string, values.cint_), False),
+            tk.MID: (3, None, (values.pass_string, values.cint_, values.cint_), True),
+            tk.SGN: (1, None),
+            tk.INT: (1, None),
+            tk.ABS: (1, None),
+            tk.SQR: (1, None),
+            tk.RND: (None, self.value_rnd),
+            tk.SIN: (1, None),
+            tk.LOG: (1, None),
+            tk.EXP: (1, None),
+            tk.COS: (1, None),
+            tk.TAN: (1, None),
+            tk.ATN: (1, None),
+            tk.FRE: (1, values.SNG),
+            tk.INP: (1, values.INT),
+            tk.POS: (1, values.INT),
+            tk.LEN: (1, None),
+            tk.STR: (1, None),
+            tk.VAL: (1, None),
+            tk.ASC: (1, None),
+            tk.CHR: (1, None),
+            tk.PEEK: (1, values.INT),
+            tk.SPACE: (1, None),
+            tk.OCT: (1, None),
+            tk.HEX: (1, None),
+            tk.LPOS: (1, values.INT),
+            tk.CINT: (1, None),
+            tk.CSNG: (1, None),
+            tk.CDBL: (1, None),
+            tk.FIX: (1, None),
+            tk.PEN: (1, values.INT),
+            tk.STICK: (1, values.INT),
+            tk.STRIG: (1, values.INT),
+            tk.EOF: (1, values.INT),
+            tk.LOC: (1, values.SNG),
+            tk.LOF: (1, values.SNG),
         }
         self._functions = set(self._with_presign.keys() + self._bare.keys())
         self._callbacks = {
@@ -388,11 +388,14 @@ class ExpressionParser(object):
         else:
             fndict = self._with_presign[token]
             presign = ins.skip_blank_read_if(fndict)
+            if presign:
+                token += presign
             try:
                 fn_record = fndict[presign]
             except KeyError:
                 raise error.RunError(error.STX)
-        narity, fn, to_type = fn_record[:3]
+        narity, to_type = fn_record[:2]
+        fn = self._callbacks[token]
         if narity == 0:
             return self._values.from_value(fn(), to_type)
         elif narity == 1:
@@ -404,11 +407,12 @@ class ExpressionParser(object):
             else:
                 return fn(val)
         elif narity > 1:
-            conv, optional = fn_record[3:]
+            conv, optional = fn_record[2:]
             # these functions generate type mismatch and overflow errors *before* parsing the closing parenthesis
             # while unary functions generate it *afterwards*. this is to match GW-BASIC
             return fn(*self._parse_argument_list(ins, conv, optional))
         else:
+            fn = fn_record[1]
             # special cases
             return fn(ins)
 
