5b54a769f92c126a7d2ec70c00bb5fa4a404563d
Rob Hagemans <robhagemans@yahoo.co.uk>
2017-12-29 08:45:53 +0000

    update version string in Mac OSX installer CFBundleShortVersionString (fix issue #17)

--- packaging/osx/build.sh
+++ packaging/osx/build.sh
@@ -4,7 +4,7 @@ NAME="pcbasic-$VERSION-osx"
 pyinstaller installer.spec
 # replace executable started by Finder with workaround script to set cwd
 cp launcher.sh dist/PC-BASIC.app/Contents/MacOS/
-cp Info.plist dist/PC-BASIC.app/Contents/
+sed -e "s/0.0.0/$VERSION/g" Info.plist > dist/PC-BASIC.app/Contents/Info.plist
 # build compressed DMG image
 hdiutil create -srcfolder dist/PC-BASIC.app -volname "PC-BASIC release $VERSION" $NAME.uncompressed.dmg
 hdiutil convert $NAME.uncompressed.dmg -format UDZO -imagekey zlib-level=9 -o $NAME.dmg
