3d1eeaf994b653a45bf069f1eaf4c53870064dd6
Rob Hagemans <robhagemans@yahoo.co.uk>
2018-01-02 14:05:28 +0000

    make pcbasic.bat script work also if launcher.exe not present

--- pcbasic.bat
+++ pcbasic.bat
@@ -1,1 +1,7 @@
-launcher.exe python -m pcbasic %*
+@echo off
+if exist launcher.exe (
+    launcher.exe python -m pcbasic %*
+) else (
+    echo WARNING: ANSIpipe launcher not found, command-line output may be garbled
+    python -m pcbasic %*
+)
