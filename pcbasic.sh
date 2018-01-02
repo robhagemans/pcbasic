70f4c996a828441b36c152e4cbc5cb49b4cf6926
Rob Hagemans <robhagemans@yahoo.co.uk>
2018-01-02 19:46:35 +0000

    ensure quoted parameters are passed correctly by pcbasic.sh

--- pcbasic.sh
+++ pcbasic.sh
@@ -3,4 +3,4 @@
 # (c) 2013, 2014, 2015, 2016, 2017 Rob Hagemans
 # This file is released under the GNU GPL version 3 or later.
 cd "$(dirname -- "$0")"
-/usr/bin/env python2 -m pcbasic $@
+/usr/bin/env python2 -m pcbasic "$@"
