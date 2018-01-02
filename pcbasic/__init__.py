76bf08284859cecb46fdbb99700c17d325ba9623
Rob Hagemans <robhagemans@yahoo.co.uk>
2018-01-02 13:40:06 +0000

    fix intra-package imports after os.chdir() (re issue #54)

--- pcbasic/__init__.py
+++ pcbasic/__init__.py
@@ -5,5 +5,11 @@ PC-BASIC - GW-BASIC/BASICA/Cartridge BASIC compatible interpreter
 This file is released under the GNU GPL version 3 or later.
 """
 
+# __path__ hack to ensure os.chdir does not break intra-package imports
+# which they do because the package __path__ is given relative to cwd
+# at least if run with python -m package
+import os
+__path__ = [os.path.abspath(e) for e in __path__]
+
 from .version import __version__, __copyright__
 from .main import main, run
