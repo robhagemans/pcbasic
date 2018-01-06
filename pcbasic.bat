c3b558f8a6e244dd83aff186740f5fc07ac2b9b4
Rob Hagemans <robhagemans@yahoo.co.uk>
2018-01-06 09:21:21 +0000

    make runner scripts find module without changing working directory (fix issue #55)

--- pcbasic.bat
+++ pcbasic.bat
@@ -1,4 +1,13 @@
 @echo off
+
+rem PC-BASIC - GW-BASIC/BASICA/Cartridge BASIC compatible interpreter
+rem (c) 2013--2018 Rob Hagemans
+rem This file is released under the GNU GPL version 3 or later.
+
+setlocal
+set PYTHONPATH=%PYTHONPATH%;%~dp0
+echo %PYTHONPATH%
+
 if exist launcher.exe (
     launcher.exe python -m pcbasic %*
 ) else (
--- pcbasic.sh
+++ pcbasic.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
+
 # PC-BASIC - GW-BASIC/BASICA/Cartridge BASIC compatible interpreter
-# (c) 2013, 2014, 2015, 2016, 2017 Rob Hagemans
+# (c) 2013--2018 Rob Hagemans
 # This file is released under the GNU GPL version 3 or later.
-cd "$(dirname -- "$0")"
+
+SCRIPTDIR="$(dirname -- "$0")"
+export PYTHONPATH="$PYTHONPATH":"$SCRIPTDIR"
+
 /usr/bin/env python2 -m pcbasic "$@"
