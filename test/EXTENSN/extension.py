117497ba5b178a831e358ad329fd3d08f66293f9
Rob Hagemans <robhagemans@yahoo.co.uk>
2018-01-10 19:32:47 +0000

    add test EXTENSN for extension function _CALLP

---
+++ test/EXTENSN/extension.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+print "importing extension"
+
+import os
+
+def output(*args):
+    with open('python-output.txt', 'w') as g:
+        g.write(repr(list(args)))
+
+def add(x, y):
+    return x + y
