8e1d87d0be9be300096e84c48872c5ad8f6645a2
Rob Hagemans <robhagemans@yahoo.co.uk>
2018-01-11 19:22:17 +0000

    update extension test EXTENSN

--- test/EXTENSN/extension.py
+++ test/EXTENSN/extension.py
@@ -5,7 +5,7 @@ print "importing extension"
 import os
 
 def output(*args):
-    with open('python-output.txt', 'w') as g:
+    with open('python-output.txt', 'a') as g:
         g.write(repr(list(args)))
 
 def add(x, y):
