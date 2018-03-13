737bb3fcfccffa29a33a470e5b9e9fe50f45374e
Rob Hagemans <robhagemans@yahoo.co.uk>
2018-03-13 14:58:28 +0000

    merge printer module into parports

--- pcbasic/basic/devices/parports.py
+++ pcbasic/basic/devices/parports.py
@@ -8,6 +8,7 @@ This file is released under the GNU GPL version 3 or later.
 
 import logging
 import sys
+import os
 import io
 
 try:
@@ -15,9 +16,13 @@ try:
 except Exception:
     parallel = None
 
+from ...compat import line_print
 from ..base import error
 from . import devicebase
-from . import printer
+
+
+# flush triggers
+TRIGGERS = {'page': b'\f', 'line': b'\n', 'close': None, '': None}
 
 
 ###############################################################################
@@ -53,7 +58,10 @@ class LPTDevice(devicebase.Device):
         elif addr == 'PRINTER' or (val and not addr):
             # 'PRINTER' is default
             # name:parameters (LINE, PAGE, ...)
-            self.stream = printer.get_printer_stream(val, codepage, temp_dir)
+            options = val.split(b':')
+            printer_name = options[0]
+            flush_trigger = (options[1:] or [''])[0]
+            self.stream = PrinterStream(printer_name, flush_trigger, codepage, temp_dir)
         elif val:
             logging.warning('Could not attach %s to LPT device', arg)
         # column counter is the same across all LPT files
@@ -71,6 +79,9 @@ class LPTDevice(devicebase.Device):
         """Device is available."""
         return self.stream is not None
 
+
+###############################################################################
+# file on LPT device
 
 class LPTFile(devicebase.TextFileBase):
     """LPTn: device - line printer or parallel port."""
@@ -148,13 +159,69 @@ class LPTFile(devicebase.TextFileBase):
         self.do_print()
 
 
+##############################################################################
+# printers
+
+class PrinterStream(io.BytesIO):
+    """LPT output to printer."""
+
+    def __init__(self, printer_name, flush_trigger, codepage, temp_dir):
+        """Initialise the printer stream."""
+        self.printer_name = printer_name
+        self.codepage = codepage
+        # flush_trigger can be a char or a code word
+        self._flush_trigger = TRIGGERS.get(flush_trigger.lower(), flush_trigger)
+        io.BytesIO.__init__(self)
+        # temp file in temp dir
+        self._printfile = os.path.join(temp_dir, u'pcbasic_print.txt')
+
+    def close(self):
+        """Close the printer stream."""
+        self.flush()
+
+    def write(self, s):
+        """Write to printer stream."""
+        for c in s:
+            if c == b'\b':
+                # backspace: drop a non-newline character from the buffer
+                self.seek(-1, 1)
+                if self.read(1) not in (b'\r', b'\n', b'\f'):
+                    self.seek(-1, 1)
+                    self.truncate()
+            io.BytesIO.write(self, c)
+            if c == self._flush_trigger:
+                self.flush()
+
+    def flush(self):
+        """Flush the printer buffer to a printer."""
+        printbuf = self.getvalue()
+        if not printbuf:
+            return
+        self.seek(0)
+        self.truncate()
+        # any naked lead bytes in DBCS will remain just that - avoid in-line flushes.
+        utf8buf = self.codepage.str_to_unicode(
+                    printbuf, preserve_control=True).encode('utf-8', 'replace')
+        line_print(utf8buf, self.printer_name, self._printfile)
+
+    def set_control(self, select=False, init=False, lf=False, strobe=False):
+        """Set the values of the control pins."""
+
+    def get_status(self):
+        """Get the values of the status pins."""
+        return False, False, False, False, False
+
+
+##############################################################################
+# physical parallel ports
+
 class ParallelStream(object):
-    """Wrapper for Parallel object to implement stream-like API."""
+    """LPT output to parallel port."""
 
     def __init__(self, port):
         """Initialise the ParallelStream."""
         if not parallel:
-            raise IOError('PySerial Parallel module not found. Parallel port communication not available.')
+            raise IOError('`parallel` module not found. Parallel port communication not available.')
         try:
             self._parallel = parallel.Parallel(port)
         except TypeError:
@@ -202,8 +269,11 @@ class ParallelStream(object):
         pass
 
 
+##############################################################################
+# standard output
+
 class StdIOParallelStream(object):
-    """Wrapper object to route port to stdio."""
+    """LPT output to standard output."""
 
     def __init__(self, crlf=False):
         """Initialise the stream."""
