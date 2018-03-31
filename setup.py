32b2fffe9449ae1192ee6139012ad4f038c2ff89
Rob Hagemans <robhagemans@yahoo.co.uk>
2018-03-31 22:17:33 +0100

    setup cx_Freeze to produce a Windows MSI installer

--- setup.py
+++ setup.py
@@ -13,9 +13,20 @@ from codecs import open
 from setuptools.command import sdist, build_py
 
 import distutils.cmd
-from setuptools import setup, find_packages, Extension
-#from cx_Freeze import setup, Executable
+from setuptools import find_packages, Extension
 
+
+# operating in cx_Freeze mode
+CX_FREEZE = set(sys.argv) & set(('bdist_msi', 'bdist_dmg', 'build_exe'))
+
+if CX_FREEZE:
+    from cx_Freeze import setup, Executable
+else:
+    from setuptools import setup
+
+
+# file location
+HERE = os.path.abspath(os.path.dirname(__file__))
 
 # this is the base MANIFEST.in
 # but I need it to change for different platforms/commands
@@ -26,8 +37,6 @@ include doc/*
 include pcbasic/data/USAGE.txt
 include pcbasic/data/*/*
 """
-#
-HERE = os.path.abspath(os.path.dirname(__file__))
 
 
 ###############################################################################
@@ -137,21 +146,18 @@ else:
     ext_modules = []
 
 
-setup(
-
-    # metadata
-
-    name='pcbasic',
-    version=VERSION,
-    description=DESCRIPTION,
-    long_description=LONG_DESCRIPTION,
-    url=URL,
-    author=AUTHOR,
-    author_email=EMAIL,
-    license=LICENCE,
+SETUP_OPTIONS = {
+    'name': 'pcbasic',
+    'version': VERSION,
+    'description': DESCRIPTION,
+    'long_description': LONG_DESCRIPTION,
+    'url': URL,
+    'author': AUTHOR,
+    'author_email': EMAIL,
+    'license': LICENCE,
 
     # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
-    classifiers=[
+    'classifiers': [
         'Development Status :: 4 - Beta',
         'Intended Audience :: End Users/Desktop',
         'Intended Audience :: Developers',
@@ -160,63 +166,146 @@ setup(
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Programming Language :: Python :: 2.7',
     ],
-
-    keywords='emulator interpreter basic retro legacy gwbasic basica pcjr tandy',
+    'keywords': 'emulator interpreter basic retro legacy gwbasic basica pcjr tandy',
 
     # contents
-
-    packages=find_packages(exclude=['doc', 'test', 'docsrc', 'packaging']),
-
+    'packages': find_packages(exclude=['doc', 'test', 'docsrc', 'packaging']),
     # rule of thumb for sdist: package_data specifies what gets *installed*,
     # but manifest specifies what gets *included* in the archive in the first place
-    package_data={
+    'package_data': {
         'pcbasic': [
                 '*.txt', '*.md', 'pcbasic/*.txt', 'pcbasic/data/codepages/*',
                 'pcbasic/data/fonts/*', 'pcbasic/data/programs/*',
                 'pcbasic/lib/*',
             ],
     },
-
-    ext_modules=ext_modules,
-
-    include_package_data=True,
+    'ext_modules': ext_modules,
+    'include_package_data': True,
 
     # requirements
-
     # need a Python-2 that's 2.7.12 or better
-    python_requires='~=2.7.12',
-
-    install_requires=['PySDL2', 'numpy', 'pyserial'] + platform_specific_requirements,
-
+    'python_requires': '~=2.7.12',
+    'install_requires': ['PySDL2', 'numpy', 'pyserial'] + platform_specific_requirements,
     # use e.g. pip install -e .[dev,full]
-    extras_require={
+    'extras_require': {
         'dev': ['lxml', 'markdown', 'pylint', 'coverage'],
         'full': ['pygame', 'pyaudio'],
     },
 
     # launchers
-
-    entry_points={
+    'entry_points': {
         'console_scripts': console_scripts,
         'gui_scripts': gui_scripts,
     },
 
     # setup commands
-
-    cmdclass={
+    'cmdclass': {
         'build_docs': BuildDocCommand,
         'sdist': SDistCommand,
         'sdist_dev': SDistDevCommand,
         'build_py': BuildPyCommand,
     },
+}
+
+###############################################################################
+# freezing options
+
+if CX_FREEZE:
+    shortversion = '.'.join(VERSION.encode('ascii').split('.')[:2])
+    numversion = '.'.join(v for v in VERSION.encode('ascii').split('.') if v.isdigit())
+
+    # install dir for cx_Freeze bdist_msi
+    if 'bdist_msi' in sys.argv:
+        sys.argv += [
+            '--initial-target-dir',
+            'c:\\Program Files\\%s %s' % (NAME.encode('ascii'), shortversion)
+        ]
+
+    # these must be bytes for cx_Freeze bdist_msi
+    SETUP_OPTIONS['name'] = NAME.encode('ascii')
+    SETUP_OPTIONS['author'] = AUTHOR.encode('ascii')
+    SETUP_OPTIONS['version'] = numversion
+
+
+    directory_table = [
+        (
+            'StartMenuFolder',
+            'TARGETDIR',
+            '.',
+        ),
+        (
+            'MyProgramMenu',
+            'StartMenuFolder',
+            'PCBASI~1|PC-BASIC 2.0',
+        ),
+    ]
+    # https://stackoverflow.com/questions/15734703/use-cx-freeze-to-create-an-msi-that-adds-a-shortcut-to-the-desktop#15736406
+    shortcut_table = [
+        (
+            'ProgramShortcut',        # Shortcut
+            'MyProgramMenu',          # Directory_
+            'PC-BASIC %s' % VERSION,  # Name
+            'TARGETDIR',              # Component_
+            '[TARGETDIR]pcbasicw.exe',# Target
+            None,                     # Arguments
+            None,                     # Description
+            None,                     # Hotkey
+            None,                     # Icon
+            None,                     # IconIndex
+            None,                     # ShowCmd
+            # PersonalFolder is My Documents, use as Start In folder
+            'PersonalFolder'          # WkDir
+        ),
+        (
+            'DocShortcut',            # Shortcut
+            'MyProgramMenu',          # Directory_
+            'Documentation',          # Name
+            'TARGETDIR',              # Component_
+            '[TARGETDIR]PC-BASIC_documentation.html',# Target
+            None,                     # Arguments
+            None,                     # Description
+            None,                     # Hotkey
+            None,                     # Icon
+            None,                     # IconIndex
+            None,                     # ShowCmd
+            'TARGETDIR'               # WkDir
+      ),
+    ]
+    msi_data = {
+        'Directory': directory_table,
+        'Shortcut': shortcut_table,
+        # use Shortcut to produce custom or multiple shortcuts
+    }
 
     # cx_Freeze options
-    #
-    # options={'build_exe': {
-    #             'packages': ['numpy'],
-    #             'excludes': ['tkinter', 'tcltk', 'nose', 'PIL', 'PyQt4', 'scipy', 'pygame'],
-    #             #'optimize': 2,
-    #             },
-    #         },
-    # executables = [Executable('run.py', base=None)],
-)
+    SETUP_OPTIONS['options'] = {
+        'build_exe': {
+            'packages': ['numpy', 'pkg_resources._vendor', 'sdl2'],
+            'excludes': [
+                'Tkinter', '_tkinter', 'PIL', 'PyQt4', 'scipy', 'pygame',
+                'pywin', 'win32com', 'test',
+            ],
+            'include_files': ['doc/PC-BASIC_documentation.html'],
+            #'optimize': 2,
+        },
+        'bdist_msi': {
+            'add_to_path': True,
+            'data': msi_data,
+        },
+    }
+
+    SETUP_OPTIONS['executables'] = [
+        Executable(
+            'run.py', base='Console', targetName='pcbasic.exe', icon='packaging/icon/pcbasic.ico',
+            copyright=COPYRIGHT),
+        Executable(
+            'run.py', base='Win32GUI', targetName='pcbasicw.exe', icon='packaging/icon/pcbasic.ico',
+            #shortcutName='PC-BASIC %s' % VERSION, shortcutDir='MyProgramMenu',
+            copyright=COPYRIGHT),
+    ]
+
+
+###############################################################################
+# run the setup
+
+setup(**SETUP_OPTIONS)
