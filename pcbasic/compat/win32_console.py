7ed564babe27af3f7c07927f2f16a3f36a57a4a6
Rob Hagemans <robhagemans@yahoo.co.uk>
2018-09-11 21:57:09 +0100

    emit ansi codes from ConsoleInput object

--- pcbasic/compat/win32_console.py
+++ pcbasic/compat/win32_console.py
@@ -44,6 +44,32 @@ class INPUT_RECORD(Structure):
 KEY_EVENT = 1
 VK_MENU = 0x12
 
+KEY_CODE_TO_ANSI = {
+    0x21: u'\x1b[5~', # VK_PRIOR, page up
+    0x22: u'\x1b[6~', # VK_NEXT, page down
+    0x23: u'\x1bOF', # VK_END
+    0x24: u'\x1bOH', # VK_HOME
+    0x25: u'\x1b[D', # VK_LEFT
+    0x26: u'\x1b[A', # VK_UP
+    0x27: u'\x1b[C', # VK_RIGHT
+    0x28: u'\x1b[B', # VK_DOWN
+    0x2d: u'\x1b[2~', # VK_INSERT
+    0x2e: u'\x1b[3~', # VK_DELETE
+    0x70: u'\x1bOP', # VK_F1
+    0x71: u'\x1bOQ', # VK_F2
+    0x72: u'\x1bOR', # VK_F3
+    0x73: u'\x1bOS', # VK_F4
+    0x74: u'\x1b[15~', # VK_F5
+    0x75: u'\x1b[17~', # VK_F6
+    0x76: u'\x1b[18~', # VK_F7
+    0x77: u'\x1b[19~', # VK_F8
+    0x78: u'\x1b[20~', # VK_F9
+    0x79: u'\x1b[21~', # VK_F10
+    0x7a: u'\x1b[23~', # VK_F11
+    0x7b: u'\x1b[24~', # VK_F12
+}
+
+
 _ReadConsoleInputW = windll.kernel32.ReadConsoleInputW
 _ReadConsoleInputW.argtypes = (
     wintypes.HANDLE, POINTER(INPUT_RECORD), wintypes.DWORD, POINTER(wintypes.DWORD)
@@ -113,12 +139,10 @@ class ConsoleInput(_StreamWrapper):
                 for event in input_buffer:
                     if event.EventType != KEY_EVENT:
                         continue
-                    # key-up event for unicode Alt+HEX input
-                    if event.KeyEvent.bKeyDown or event.KeyEvent.wVirtualKeyCode == VK_MENU:
-                        char = event.KeyEvent.UnicodeChar
-                        bchar = char.encode(self.encoding)
-                        self._bytes_buffer += bchar
-                        if self.echo and char and char != u'\0':
+                    char = self._translate_event(event)
+                    if char:
+                        self._bytes_buffer += char.encode(self.encoding)
+                        if self.echo:
                             _WriteConsoleW(
                                 self._echo_handle, u'\n' if char == u'\r' else char, len(char),
                                 byref(wintypes.DWORD()), byref(wintypes.DWORD())
@@ -127,6 +151,25 @@ class ConsoleInput(_StreamWrapper):
                             # ctrl-z is end of input on windows console
                             return
             time.sleep(0.01)
+
+    def _translate_event(self, event):
+        char = event.KeyEvent.UnicodeChar
+        if char == u'\0':
+            # windows uses null-terminated strings so \0 means no output
+            char = u''
+        if not event.KeyEvent.bKeyDown:
+            # key-up event for unicode Alt+HEX input
+            if event.KeyEvent.wVirtualKeyCode == VK_MENU:
+                return char
+            # ignore other key-up events
+            return u''
+        elif event.KeyEvent.dwControlKeyState & 0xf:
+            # ctrl or alt are down; don't parse arrow keys etc.
+            # but if any unicode is produced, send it on
+            return char
+        else:
+            key_code = event.KeyEvent.wVirtualKeyCode
+            return KEY_CODE_TO_ANSI.get(key_code, char)
 
 
 # Python2-compatible standard bytes streams
