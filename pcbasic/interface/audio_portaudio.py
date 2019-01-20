adb48d5fdc3b058973a1b7f85ea8f637894729f6
Rob Hagemans <robhagemans@yahoo.co.uk>
2019-01-20 10:59:10 +0000

    clean up coding style in audio_portaudio

--- pcbasic/interface/audio_portaudio.py
+++ pcbasic/interface/audio_portaudio.py
@@ -24,9 +24,9 @@ from . import synthesiser
 
 # approximate generator chunk length
 # one wavelength at 37 Hz is 1192 samples at 44100 Hz
-CHUNK_LENGTH = 1192 * 4
+_CHUNK_LENGTH = 1192 * 4
 # buffer size in sample frames
-BUFSIZE = 1024
+_BUFSIZE = 1024
 
 
 @audio_plugins.register('portaudio')
@@ -38,23 +38,23 @@ class AudioPortAudio(AudioPlugin):
         if not pyaudio:
             raise InitFailed('Module `pyaudio` not found')
         # synthesisers
-        self.signal_sources = synthesiser.get_signal_sources()
+        self._signal_sources = synthesiser.get_signal_sources()
         # sound generators for each voice
-        self.generators = [deque(), deque(), deque(), deque()]
+        self._generators = [deque() for _ in synthesiser.VOICES]
         # buffer of samples; drained by callback, replenished by _play_sound
-        self._samples = [bytearray() for _ in range(4)]
-        self._dev = None
+        self._samples = [bytearray() for _ in synthesiser.VOICES]
+        self._device = None
+        self._stream = None
+        self._min_samples_buffer = 2 * _BUFSIZE
         AudioPlugin.__init__(self, audio_queue)
 
     def __enter__(self):
         """Perform any necessary initialisations."""
         with muffle(sys.stderr):
-            self._dev = pyaudio.PyAudio()
-            sample_format = self._dev.get_format_from_width(1)
-            self._min_samples_buffer = 2 * BUFSIZE
-            self._stream = self._dev.open(
-                format=sample_format, channels=1, rate=synthesiser.SAMPLE_RATE, output=True,
-                frames_per_buffer=BUFSIZE, stream_callback=self._get_next_chunk
+            self._device = pyaudio.PyAudio()
+            self._stream = self._device.open(
+                format=pyaudio.paUInt8, channels=1, rate=synthesiser.SAMPLE_RATE, output=True,
+                frames_per_buffer=_BUFSIZE, stream_callback=self._get_next_chunk
             )
             self._stream.start_stream()
             AudioPlugin.__enter__(self)
@@ -63,33 +63,34 @@ class AudioPortAudio(AudioPlugin):
         """Close down PortAudio."""
         self._stream.stop_stream()
         self._stream.close()
-        self._dev.terminate()
+        self._device.terminate()
         return AudioPlugin.__exit__(self, type, value, traceback)
 
     def tone(self, voice, frequency, duration, loop, volume):
         """Enqueue a tone."""
-        self.generators[voice].append(synthesiser.SoundGenerator(
-                    self.signal_sources[voice], synthesiser.FEEDBACK_TONE,
-                    frequency, duration, loop, volume))
+        self._generators[voice].append(synthesiser.SoundGenerator(
+            self._signal_sources[voice], synthesiser.FEEDBACK_TONE,
+            frequency, duration, loop, volume
+        ))
 
     def noise(self, source, frequency, duration, loop, volume):
         """Enqueue a noise."""
         feedback = synthesiser.FEEDBACK_NOISE if source else synthesiser.FEEDBACK_PERIODIC
-        self.generators[3].append(synthesiser.SoundGenerator(
-                    self.signal_sources[3], feedback,
-                    frequency, duration, loop, volume))
+        self._generators[synthesissr.NOISE_VOICE].append(synthesiser.SoundGenerator(
+            self._signal_sources[synthesissr.NOISE_VOICE], feedback,
+            frequency, duration, loop, volume
+        ))
 
     def hush(self):
         """Stop sound."""
-        for voice in range(4):
-            self._next_tone[voice] = None
-            while self.generators[voice]:
-                self.generators[voice].popleft()
-        self._samples = [bytearray() for _ in range(4)]
+        self._next_tone = [None for _ in synthesiser.VOICES]
+        for gen in self._generators:
+            gen.clear()
+        self._samples = [bytearray() for _ in synthesiser.VOICES]
 
     def _work(self):
         """Replenish sample buffer."""
-        for voice in range(4):
+        for voice in synthesiser.VOICES:
             if len(self._samples[voice]) > self._min_samples_buffer:
                 # nothing to do
                 continue
@@ -98,12 +99,12 @@ class AudioPortAudio(AudioPlugin):
                     try:
                         # looping tone will be interrupted
                         # by any new tone appearing in the generator queue
-                        self._next_tone[voice] = self.generators[voice].popleft()
+                        self._next_tone[voice] = self._generators[voice].popleft()
                     except IndexError:
                         if self._next_tone[voice] is None:
                             current_chunk = None
                             break
-                current_chunk = self._next_tone[voice].build_chunk(CHUNK_LENGTH)
+                current_chunk = self._next_tone[voice].build_chunk(_CHUNK_LENGTH)
                 if current_chunk is not None:
                     break
                 self._next_tone[voice] = None
