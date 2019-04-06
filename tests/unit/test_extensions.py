4751eabda1b99bb5b8d0af8f670e9ee3fffab6a0
Rob Hagemans <robhagemans@yahoo.co.uk>
2019-04-06 08:33:04 +0100

    split extension tests from session tests

---
+++ tests/unit/test_extensions.py
@@ -0,0 +1,97 @@
+"""
+PC-BASIC tests.test_extension
+unit tests for extensions
+
+(c) 2019 Rob Hagemans
+This file is released under the GNU GPL version 3 or later.
+"""
+
+import os
+
+from pcbasic import Session
+from tests.unit.utils import TestCase, run_tests
+
+
+class ExtensionTest(TestCase):
+    """Unit tests for extensions."""
+
+    tag = u'extensions'
+
+    def test_extension(self):
+        """Test extension functions."""
+
+        class Extension(object):
+            @staticmethod
+            def add(x, y):
+                return '%s plus %s equals %s' % (repr(x), repr(y), repr(x+y))
+
+            @staticmethod
+            def one():
+                return 1
+
+        with Session(extension=Extension) as s:
+            s.execute('''
+                10 a=5
+                run
+                b$ = _add(a, 1)
+                c% = _one
+            ''')
+            assert s.get_variable("a!") == 5
+            assert s.get_variable("c%") == 1
+            assert s.get_variable("b$") == b'5.0 plus 1 equals 6.0'
+
+    def test_extension_statement(self):
+        """Test extension statements."""
+        outfile = self.output_path('python-output.txt')
+        try:
+            os.remove(outfile)
+        except EnvironmentError:
+            pass
+
+        class Extension(object):
+            @staticmethod
+            def output(*args):
+                with open(outfile, 'ab') as g:
+                    for arg in args:
+                        if isinstance(arg, bytes):
+                            g.write(arg)
+                        else:
+                            g.write(b'%d' % (arg,))
+                        g.write(b' ')
+
+        with Session(extension=Extension) as s:
+            s.execute(b'''
+                _OUTPUT "one", 2, 3!, 4#
+                _output "!\x9c$"
+            ''')
+        with open(outfile, 'rb') as f:
+            assert f.read() == b'one 2 3 4 !\x9c$ '
+
+    def test_extended_session(self):
+        """Test extensions accessing the session."""
+
+        class ExtendedSession(Session):
+            def __init__(self):
+                Session.__init__(self, extension=self)
+
+            def adda(self, x):
+                return x + self.get_variable("a!")
+
+        with ExtendedSession() as s:
+            s.execute('a=4')
+            s.execute('b=_adda(1)')
+            assert s.evaluate('b') == 5.
+
+    def test_extension_module(self):
+        """Test using a module as extension."""
+        import random
+        with Session(extension=random) as s:
+            s.execute('''
+                _seed(42)
+                b = _uniform(a, 25.6)
+            ''')
+            self.assertAlmostEqual(s.evaluate('b'), 16.3693256378, places=10)
+
+
+if __name__ == '__main__':
+    run_tests()
