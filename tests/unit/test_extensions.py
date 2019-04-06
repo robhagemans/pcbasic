a7c4193f3723f8c850aed38a95e2296494aafce7
Rob Hagemans <robhagemans@yahoo.co.uk>
2019-04-06 10:46:34 +0100

    add more extension tests

--- tests/unit/test_extensions.py
+++ tests/unit/test_extensions.py
@@ -43,10 +43,6 @@ class ExtensionTest(TestCase):
     def test_extension_statement(self):
         """Test extension statements."""
         outfile = self.output_path('python-output.txt')
-        try:
-            os.remove(outfile)
-        except EnvironmentError:
-            pass
 
         class Extension(object):
             @staticmethod
@@ -91,6 +87,75 @@ class ExtensionTest(TestCase):
                 b = _uniform(a, 25.6)
             ''')
             self.assertAlmostEqual(s.evaluate('b'), 16.3693256378, places=10)
+
+    def test_extension_module_string(self):
+        """Test using a module name as extension."""
+        with Session(extension='random') as s:
+            s.execute('''
+                _seed(42)
+                b = _uniform(a, 25.6)
+            ''')
+            self.assertAlmostEqual(s.evaluate('b'), 16.3693256378, places=10)
+
+    def test_extension_module_not_found(self):
+        """Test using a non-existant module name as extension."""
+        with Session(extension='no-sirree') as s:
+            s.execute('_test')
+        assert self.get_text_stripped(s)[0] == b'Internal error\xff'
+
+    def test_no_extension(self):
+        """Test attempting to access extensions that aren't there."""
+        with Session() as s:
+            s.execute(b'''
+                _NOPE "one", 2, 3!, 4#
+            ''')
+        assert self.get_text_stripped(s)[0] == b'Syntax error\xff'
+
+    def test_no_statement(self):
+        """Test attempting to access extensions that aren't there."""
+        empty_ext = object()
+        with Session(extension=empty_ext) as s:
+            s.execute(b'''
+                _NOPE "one", 2, 3!, 4#
+            ''')
+        assert self.get_text_stripped(s)[0] == b'Internal error\xff'
+
+    def test_extension_function(self):
+        """Test extension functions."""
+        class Extension(object):
+            @staticmethod
+            def boolfunc():
+                return True
+            @staticmethod
+            def unicodefunc():
+                return u'test'
+            @staticmethod
+            def bytesfunc():
+                return b'test'
+            @staticmethod
+            def intfunc():
+                return 1
+            @staticmethod
+            def floatfunc():
+                return 1
+
+        with Session(extension=Extension) as s:
+            assert s.evaluate('_BOOLFUNC') == -1
+            assert s.evaluate('_INTFUNC') == 1.0
+            assert s.evaluate('_FLOATFUNC') == 1.0
+            assert s.evaluate('_UNICODEFUNC') == b'test'
+            assert s.evaluate('_BYTESFUNC') == b'test'
+
+
+    def test_extension_function_none(self):
+        """Test extension functions with disallowed return type."""
+        class Extension(object):
+            @staticmethod
+            def nonefunc():
+                return None
+        with Session(extension=Extension) as s:
+            s.evaluate('_NONEFUNC')
+        assert self.get_text_stripped(s)[0] == b'Type mismatch\xff'
 
 
 if __name__ == '__main__':
