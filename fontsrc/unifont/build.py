6c682f84fc0ff29941bcfa24aae62ec6361ac3c5
Rob Hagemans <robhagemans@yahoo.co.uk>
2019-05-27 20:14:04 +0100

    fontsrc: update unifont and univga scripts to python3

--- fontsrc/unifont/build.py
+++ fontsrc/unifont/build.py
@@ -1,5 +1,7 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
+
 import tarfile
+from io import TextIOWrapper
 
 unifont = 'unifont-8.0.01.tar.gz'
 loc = 'unifont-8.0.01/font/plane00/'
@@ -13,8 +15,7 @@ with open(output, 'w') as f:
     with open('header.txt') as g:
         f.write(g.read())
     for name in hexfiles:
-        z = unizip.extractfile(name)
-        f.write(z.read())
-        z.close()
+        with TextIOWrapper(unizip.extractfile(name)) as z:
+            f.write(z.read())
 
 
--- fontsrc/univga/build.py
+++ fontsrc/univga/build.py
@@ -1,27 +1,24 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 
 import os
 import string
 import tarfile
+from io import TextIOWrapper
+
+import monobit
 
 bdf_name = 'uni_vga/u_vga16.bdf'
 unizip = 'uni-vga.tgz'
 
-infile = tarfile.open(unizip, 'r:gz').extractfile(bdf_name)
-
 header = 'header.txt'
 hex_name = 'univga_16.hex'
-with open(hex_name, 'w') as outfile:
-    with open(header, 'r') as h:
-        for line in h:
-            outfile.write(line)
 
-    for line in infile:
-        if not line:
-            continue
-        if line[:8] == 'ENCODING':
-            outfile.write('\n%04X:' % int(line[8:]))
-        if len(line) == 3 and line[0] in string.hexdigits and line[1] in string.hexdigits and line[2] == '\n':
-            outfile.write(line[:2])
-    outfile.write('\n')
-infile.close()
+with tarfile.open(unizip, 'r:gz').extractfile(bdf_name) as infile:
+    uni_vga = monobit.load(TextIOWrapper(infile), format='bdf')
+    with open(hex_name, 'w') as outfile:
+        with open(header, 'r') as h:
+            for line in h:
+                outfile.write(line)
+        outfile.write('\n')
+        uni_vga.save(outfile, format='hex')
+
