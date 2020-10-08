6ee5ea11dd5e2c1cdb6f17463fcbeafb7991de47
Rob Hagemans <robhagemans@yahoo.co.uk>
2020-10-08 07:26:43 +0100

    freedos font builder: merge additions, precompose glyphs

--- fontsrc/freedos/build.py
+++ fontsrc/freedos/build.py
@@ -8,6 +8,7 @@ import sys
 import zipfile
 import subprocess
 import logging
+from unicodedata import normalize, name
 from collections import defaultdict
 
 import monobit
@@ -20,6 +21,37 @@ CPI_DIR = 'BIN/'
 CPI_NAMES = ['ega.cpx'] + [f'ega{_i}.cpx' for _i in range(2, 19)]
 HEADER = 'header.txt'
 CHOICES = 'choices'
+COMBINING = 'combining.yaff'
+ADDITIONS = 'additions.yaff'
+
+
+def fullname(char):
+    return ','.join(f'U+{ord(_c):04X} {name(_c)}' for _c in char)
+
+
+def precompose(font):
+    composed_glyphs = {}
+    codepoints = [cp.unicode for cp, _ in font.iter_unicode()]
+    # run through all of plane 0
+    for cp in range(0x10000):
+        char = chr(cp)
+        if char not in codepoints:
+            # first see if an equivalent precomposed char is already there
+            equiv = normalize('NFC', char)
+            if equiv in codepoints:
+                logging.info(f'Assigning {fullname(char)} == {fullname(equiv)}.')
+                font = font.with_glyph(
+                    font.get_char(equiv), f'u+{cp:04x}'
+                )
+            else:
+                decomp = normalize('NFD', char)
+                if all(c in codepoints for c in decomp):
+                    logging.info(f'Composing {fullname(char)} as {fullname(decomp)}.')
+                    glyphs = (font.get_char(c) for c in decomp)
+                    composed = monobit.glyph.Glyph.superimpose(glyphs)
+                    font = font.with_glyph(composed, f'u+{cp:04x}')
+    return font
+
 
 def main():
 
@@ -82,11 +114,23 @@ def main():
     with open(HEADER, 'r') as header:
         comments = tuple(_line[2:].rstrip() for _line in header)
 
+
+    final_font = {}
+    for size in fonts.keys():
+        if size != 16:
+            final_font[size] = monobit.font.Font([], comments=comments)
+        else:
+            # merge combining diacritics
+            logging.info('Merging combining diacritics.')
+            final_font[size] = monobit.load(COMBINING)[0]
+
+            # merging additions
+            logging.info('Merging non-FreeDOS glyphs.')
+            final_font[size] = final_font[size].merged_with(monobit.load(ADDITIONS)[0])
+
     # merge preferred picks
     logging.info('Merging choices')
-    final_font = {}
     for size, fontdict in fonts.items():
-        final_font[size] = monobit.font.Font([], comments=comments)
         for (cpi_name_0, codepage_0), labels in choices.items():
             for (cpi_name_1, codepage_1), font in fontdict.items():
                 if (
@@ -95,11 +139,17 @@ def main():
                     ):
                     final_font[size] = final_font[size].merged_with(font.subset(labels))
 
+
     # merge other fonts
     logging.info('Merging remaining fonts')
     for size, fontdict in fonts.items():
         for font in fontdict.values():
             final_font[size] = final_font[size].merged_with(font)
+
+    # precompose glyphs
+    logging.info('Precompose glyphs.')
+    for size in final_font.keys():
+        final_font[size] = precompose(final_font[size])
 
     # exclude personal use area code points
     logging.info('Removing private use area')
@@ -112,7 +162,7 @@ def main():
     # output
     logging.info('Writing output')
     for size, font in final_font.items():
-        monobit.Typeface([font]).save(f'freedos_{size:02d}.hex', format='hext')
+        monobit.Typeface([font.drop_comments()]).save(f'freedos_{size:02d}.hex', format='hext')
 
 
 main()
