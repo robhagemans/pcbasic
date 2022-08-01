7519bf166a05ae921a60fee5842394e45533a8f0
Rob Hagemans <rob.hagemans@hotmail.com>
2022-08-01 09:07:54 +0100

    simplify data directories and ensure required package data is in packages - __init__.py is needed

--- pcbasic/data/__init__.py
+++ pcbasic/data/__init__.py
@@ -6,6 +6,6 @@ Fonts, codepages, bundled programs and application branding
 This file is released under the GNU GPL version 3 or later.
 """
 
-from .resources import PROGRAMS, read_program_file, get_data, ResourceFailed
-from .loadhex import FONTS, read_fonts
-from .loaducp import CODEPAGES, read_codepage
+from .programs import PROGRAMS, read_program_file
+from .fonts import FONTS, read_fonts
+from .codepages import CODEPAGES, read_codepage
