11d0deb0f58deef7794bbd3a029225e58a7f82b0
Rob Hagemans <rob.hagemans@hotmail.com>
2022-08-10 20:42:58 +0100

    move classes outside of function body in make.mac and make.windows for slightly better readability

--- make/mac.py
+++ make/mac.py
@@ -21,155 +21,155 @@ from .freeze import SETUP_OPTIONS, SHORT_VERSION, INCLUDE_FILES, EXCLUDE_FILES, 
 from .freeze import EXCLUDE_EXTERNAL_PACKAGES
 
 
+class BuildExeCommand(cx_Freeze.build_exe):
+    """Custom build_exe command."""
+
+    def run(self):
+        """Run build_exe command."""
+        # prepare resources
+        mkdir(RESOURCE_PATH)
+        build_icon()
+        make_docs()
+        # build the executable and library
+        cx_Freeze.build_exe.run(self)
+        build_dir = 'build/exe.{}/'.format(PLATFORM_TAG)
+        # build_exe just includes everything inside the directory
+        # so remove some stuff we don't need
+        for root, _, files in os.walk(build_dir + 'lib'):
+            exclude = set(root.split(os.sep)) & set(
+                ('Headers', 'test', 'tests', 'testing', 'examples')
+            )
+            for fname in files:
+                name = os.path.join(root, fname)
+                # remove tests and examples
+                # remove windows DLLs and PYDs
+                if (exclude or 'win32_' in name or name.endswith('.dll')):
+                    remove(name)
+
+class BdistMacCommand(cx_Freeze.bdist_mac):
+    """Custom bdist_mac command."""
+
+    def run(self):
+        """Run bdist_mac command."""
+        cx_Freeze.bdist_mac.run(self)
+        #'build/PC-BASIC-2.0.app/Contents/MacOS/'
+        build_dir = self.bundle_dir + '/Contents/MacOS/'
+        # big libs we don't need
+        remove(build_dir + 'libcrypto.1.1.dylib')
+        remove(build_dir + 'libssl.1.1.dylib')
+        # sdl2 stuff we don't need
+        prune(build_dir + 'lib/sdl2dll/dll/SDL2_mixer.framework')
+        prune(build_dir + 'lib/sdl2dll/dll/SDL2_image.framework')
+        prune(build_dir + 'lib/sdl2dll/dll/SDL2_ttf.framework')
+        # fix sdl symlinks, or codesign will bork
+        cwd = os.getcwd()
+        os.chdir(build_dir + 'lib/sdl2dll/dll/SDL2.framework/Versions/')
+        os.symlink('A', 'Current')
+        os.chdir('..')
+        os.symlink('Versions/Current/SDL2', 'SDL2')
+        os.symlink('Versions/Current/Resources', 'Resources')
+        #os.symlink('Versions/Current/Headers', 'Headers')
+        os.chdir(cwd)
+        os.chdir(build_dir + 'lib/sdl2dll/dll/SDL2_gfx.framework/Versions/')
+        os.symlink('A', 'Current')
+        os.chdir('..')
+        os.symlink('Versions/Current/SDL2_gfx', 'SDL2_gfx')
+        os.symlink('Versions/Current/Resources', 'Resources')
+        #os.symlink('Versions/Current/Headers', 'Headers')
+        os.chdir(cwd)
+        # codesign the app
+        print('>>> ad hoc code signing ', self.bundle_dir)
+        subprocess.run(['codesign', '--force', '--deep', '--sign', '-', self.bundle_dir])
+
+    def copy_file(self, src, dst):
+        # catch copy errors, these happen with relative references with funny bracketed names
+        # like libnpymath.a(npy_math.o)
+        try:
+            cx_Freeze.bdist_mac.copy_file(self, src, dst)
+        except Exception as err:
+            print('ERROR: %s' % (err,))
+            # create an empty file
+            open(dst, 'w').close()
+
+
+class BdistDmgCommand(cx_Freeze.bdist_dmg):
+    """Custom bdist_mac command."""
+
+    def run(self):
+        """Run bdist_dmg command."""
+        cx_Freeze.bdist_dmg.run(self)
+        # move the disk image to dist/
+        mkdir('dist/')
+        dmg_name = '{}-{}.dmg'.format(NAME, VERSION)
+        if os.path.exists('dist/' + os.path.basename(dmg_name)):
+            os.unlink('dist/' + os.path.basename(dmg_name))
+        os.rename(self.dmg_name, dmg_name)
+        shutil.move(dmg_name, 'dist/')
+        #make_clean()
+
+    def build_dmg(self):
+        # from cx_Freeze 6.11.1 source
+
+        # Remove DMG if it already exists
+        if os.path.exists(self.dmg_name):
+            os.unlink(self.dmg_name)
+
+        # Make dist folder
+        self.dist_dir = os.path.join(self.build_dir, "dist")
+        if os.path.exists(self.dist_dir):
+            shutil.rmtree(self.dist_dir)
+        self.mkpath(self.dist_dir)
+
+        # Copy App Bundle
+        dest_dir = os.path.join(
+            self.dist_dir, os.path.basename(self.bundle_dir)
+        )
+        #self.copy_tree(self.bundle_dir, dest_dir)
+        shutil.copytree(self.bundle_dir, dest_dir, symlinks=True)
+
+        # seems we have to sign *again*, for some reason
+        subprocess.run(['codesign', '--force', '--deep', '--sign', '-', dest_dir])
+
+        ### added
+        # include the docs at them top level in the dmg
+        shutil.copy('build/doc/PC-BASIC_documentation.html', self.dist_dir)
+        ###
+
+        createargs = [
+            "hdiutil",
+            "create",
+        ]
+        if self.silent:
+            createargs += ["-quiet"]
+        createargs += [
+            "-fs",
+            "HFSX",
+            "-format",
+            "UDZO",
+            self.dmg_name,
+            "-imagekey",
+            "zlib-level=9",
+            "-srcfolder",
+            self.dist_dir,
+            "-volname",
+            self.volume_label,
+        ]
+
+        if self.applications_shortcut:
+            apps_folder_link = os.path.join(self.dist_dir, "Applications")
+            os.symlink(
+                "/Applications", apps_folder_link, target_is_directory=True
+            )
+
+        # Create the dmg
+        if subprocess.call(createargs) != 0:
+            raise OSError("creation of the dmg failed")
+
+
 def package():
     """Build a Mac .DMG package."""
     setup_options = SETUP_OPTIONS
     make_ready()
-
-    class BuildExeCommand(cx_Freeze.build_exe):
-        """Custom build_exe command."""
-
-        def run(self):
-            """Run build_exe command."""
-            # prepare resources
-            mkdir(RESOURCE_PATH)
-            build_icon()
-            make_docs()
-            # build the executable and library
-            cx_Freeze.build_exe.run(self)
-            build_dir = 'build/exe.{}/'.format(PLATFORM_TAG)
-            # build_exe just includes everything inside the directory
-            # so remove some stuff we don't need
-            for root, _, files in os.walk(build_dir + 'lib'):
-                exclude = set(root.split(os.sep)) & set(
-                    ('Headers', 'test', 'tests', 'testing', 'examples')
-                )
-                for fname in files:
-                    name = os.path.join(root, fname)
-                    # remove tests and examples
-                    # remove windows DLLs and PYDs
-                    if (exclude or 'win32_' in name or name.endswith('.dll')):
-                        remove(name)
-
-    class BdistMacCommand(cx_Freeze.bdist_mac):
-        """Custom bdist_mac command."""
-
-        def run(self):
-            """Run bdist_mac command."""
-            cx_Freeze.bdist_mac.run(self)
-            #'build/PC-BASIC-2.0.app/Contents/MacOS/'
-            build_dir = self.bundle_dir + '/Contents/MacOS/'
-            # big libs we don't need
-            remove(build_dir + 'libcrypto.1.1.dylib')
-            remove(build_dir + 'libssl.1.1.dylib')
-            # sdl2 stuff we don't need
-            prune(build_dir + 'lib/sdl2dll/dll/SDL2_mixer.framework')
-            prune(build_dir + 'lib/sdl2dll/dll/SDL2_image.framework')
-            prune(build_dir + 'lib/sdl2dll/dll/SDL2_ttf.framework')
-            # fix sdl symlinks, or codesign will bork
-            cwd = os.getcwd()
-            os.chdir(build_dir + 'lib/sdl2dll/dll/SDL2.framework/Versions/')
-            os.symlink('A', 'Current')
-            os.chdir('..')
-            os.symlink('Versions/Current/SDL2', 'SDL2')
-            os.symlink('Versions/Current/Resources', 'Resources')
-            #os.symlink('Versions/Current/Headers', 'Headers')
-            os.chdir(cwd)
-            os.chdir(build_dir + 'lib/sdl2dll/dll/SDL2_gfx.framework/Versions/')
-            os.symlink('A', 'Current')
-            os.chdir('..')
-            os.symlink('Versions/Current/SDL2_gfx', 'SDL2_gfx')
-            os.symlink('Versions/Current/Resources', 'Resources')
-            #os.symlink('Versions/Current/Headers', 'Headers')
-            os.chdir(cwd)
-            # codesign the app
-            print('>>> ad hoc code signing ', self.bundle_dir)
-            subprocess.run(['codesign', '--force', '--deep', '--sign', '-', self.bundle_dir])
-
-        def copy_file(self, src, dst):
-            # catch copy errors, these happen with relative references with funny bracketed names
-            # like libnpymath.a(npy_math.o)
-            try:
-                cx_Freeze.bdist_mac.copy_file(self, src, dst)
-            except Exception as err:
-                print('ERROR: %s' % (err,))
-                # create an empty file
-                open(dst, 'w').close()
-
-
-    class BdistDmgCommand(cx_Freeze.bdist_dmg):
-        """Custom bdist_mac command."""
-
-        def run(self):
-            """Run bdist_dmg command."""
-            cx_Freeze.bdist_dmg.run(self)
-            # move the disk image to dist/
-            mkdir('dist/')
-            dmg_name = '{}-{}.dmg'.format(NAME, VERSION)
-            if os.path.exists('dist/' + os.path.basename(dmg_name)):
-                os.unlink('dist/' + os.path.basename(dmg_name))
-            os.rename(self.dmg_name, dmg_name)
-            shutil.move(dmg_name, 'dist/')
-            #make_clean()
-
-        def build_dmg(self):
-            # from cx_Freeze 6.11.1 source
-
-            # Remove DMG if it already exists
-            if os.path.exists(self.dmg_name):
-                os.unlink(self.dmg_name)
-
-            # Make dist folder
-            self.dist_dir = os.path.join(self.build_dir, "dist")
-            if os.path.exists(self.dist_dir):
-                shutil.rmtree(self.dist_dir)
-            self.mkpath(self.dist_dir)
-
-            # Copy App Bundle
-            dest_dir = os.path.join(
-                self.dist_dir, os.path.basename(self.bundle_dir)
-            )
-            #self.copy_tree(self.bundle_dir, dest_dir)
-            shutil.copytree(self.bundle_dir, dest_dir, symlinks=True)
-
-            # seems we have to sign *again*, for some reason
-            subprocess.run(['codesign', '--force', '--deep', '--sign', '-', dest_dir])
-
-            ### added
-            # include the docs at them top level in the dmg
-            shutil.copy('build/doc/PC-BASIC_documentation.html', self.dist_dir)
-            ###
-
-            createargs = [
-                "hdiutil",
-                "create",
-            ]
-            if self.silent:
-                createargs += ["-quiet"]
-            createargs += [
-                "-fs",
-                "HFSX",
-                "-format",
-                "UDZO",
-                self.dmg_name,
-                "-imagekey",
-                "zlib-level=9",
-                "-srcfolder",
-                self.dist_dir,
-                "-volname",
-                self.volume_label,
-            ]
-
-            if self.applications_shortcut:
-                apps_folder_link = os.path.join(self.dist_dir, "Applications")
-                os.symlink(
-                    "/Applications", apps_folder_link, target_is_directory=True
-                )
-
-            # Create the dmg
-            if subprocess.call(createargs) != 0:
-                raise OSError("creation of the dmg failed")
-
 
     setup_options['cmdclass'] = dict(
         build_exe=BuildExeCommand,
--- make/windows.py
+++ make/windows.py
@@ -23,175 +23,177 @@ UPGRADE_CODE = '{714d23a9-aa94-4b17-87a5-90e72d0c5b8f}'
 PRODUCT_CODE = msilib.gen_uuid()
 
 
+class BuildExeCommand(cx_Freeze.build_exe):
+    """Custom build_exe command."""
+
+    def run(self):
+        """Run build_exe command."""
+        mkdir(RESOURCE_PATH)
+        build_icon()
+        # only include 32-bit DLLs
+        cx_Freeze.build_exe.run(self)
+        build_dir = 'build/exe.{}/'.format(PLATFORM_TAG)
+        # build_exe just includes everything inside the directory
+        # so remove some stuff we don't need
+        for root, _, files in os.walk(build_dir + 'lib'):
+            testing = set(root.split(os.sep)) & set(('test', 'tests', 'testing', 'examples'))
+            for fname in files:
+                name = os.path.join(root, fname)
+                if (
+                        # remove superfluous copies of python dll in lib/
+                        # as there is a copy in the package root already
+                        fname.lower() == 'python37.dll'
+                        # remove tests and examples
+                        or testing
+                        # we're only producing packages for win32_x86
+                        or 'win32_x64' in name or name.endswith('.dylib')
+                    ):
+                    remove(name)
+        # chunky libs on python3.7 that I don't think we use
+        remove(build_dir + 'lib/libcrypto-1_1.dll')
+        remove(build_dir + 'lib/libssl-1_1.dll')
+        # unneeded sdl2 bits
+        remove(build_dir + 'lib/sdl2dll/dll/SDL2_image.dll')
+        remove(build_dir + 'lib/sdl2dll/dll/SDL2_mixer.dll')
+        remove(build_dir + 'lib/sdl2dll/dll/SDL2_ttf.dll')
+        remove(build_dir + 'lib/sdl2dll/dll/libwebp-7.dll')
+        remove(build_dir + 'lib/sdl2dll/dll/libtiff-5.dll')
+        remove(build_dir + 'lib/sdl2dll/dll/libopus-0.dll')
+        remove(build_dir + 'lib/sdl2dll/dll/libopusfile-0.dll')
+        remove(build_dir + 'lib/sdl2dll/dll/libogg-0.dll')
+        remove(build_dir + 'lib/sdl2dll/dll/libmodplug-1.dll')
+
+
+class BdistMsiCommand(cx_Freeze.bdist_msi):
+    """Custom bdist_msi command."""
+
+    def run(self):
+        """Run build_msi command."""
+        name = '{}-{}'.format(NAME, VERSION)
+        remove('dist/{}.msi'.format(name))
+        cx_Freeze.bdist_msi.run(self)
+        # close the database file so we can rename the file
+        del self.db
+        os.rename('dist/{}-win32.msi'.format(name), 'dist/{}.msi'.format(name))
+        #make_clean()
+
+    def add_config(self):
+        """Override cx_Freeze add_config."""
+        # mostly copy-paste from cxfreeze source, wich some changes
+        if self.directories:
+            msilib.add_data(self.db, "Directory", self.directories)
+        msilib.add_data(
+            self.db, 'CustomAction',
+            [("A_SET_TARGET_DIR", 256 + 51, "TARGETDIR", self.initial_target_dir)]
+        )
+        if self.add_to_path:
+            msilib.add_data(
+                self.db, 'Environment', [("E_PATH", "=-*Path", r"[~];[TARGETDIR]", "TARGETDIR")]
+            )
+        msilib.add_data(self.db, 'InstallUISequence', [
+            ("PrepareDlg", None, 140),
+            # this is new
+            # sould probably be conditional on "not already installed" or smth
+            ("WhichUsersDlg", "not Installed", 400),
+            ("A_SET_TARGET_DIR", 'TARGETDIR=""', 401),
+            ("SelectDirectoryDlg", "not Installed", 1230),
+            ("MaintenanceTypeDlg", "Installed and not Resume and not Preselected", 1250),
+            ("ProgressDlg", None, 1280),
+        ])
+        msilib.add_data(self.db, 'InstallExecuteSequence', [
+            ("A_SET_TARGET_DIR", 'TARGETDIR=""', 401),
+            ('WriteEnvironmentStrings', 'MSIINSTALLPERUSER=""', 5200),
+        ])
+        for index, executable in enumerate(self.distribution.executables):
+            if executable.shortcut_name is not None and executable.shortcut_dir is not None:
+                base_name = os.path.basename(executable.target_name)
+                msilib.add_data(
+                    self.db, "Shortcut", [(
+                        "S_APP_%s" % index, executable.shortcut_dir, executable.shortcut_name,
+                        "TARGETDIR", "[TARGETDIR]%s" % base_name,
+                        None, None, None, None, None, None, None
+                    )]
+                )
+        for table_name, data in self.data.items():
+            col = self._binary_columns.get(table_name)
+            if col is not None:
+                data = [
+                    (*row[:col], msilib.Binary(row[col].name), *row[col + 1 :])
+                    for row in data
+                ]
+            msilib.add_data(self.db, table_name, data)
+
+    def add_properties(self):
+        """Override cx_Freeze add_properties."""
+        # mostly copy-paste from cxfreeze
+        metadata = self.distribution.metadata
+        props = [
+            ('DistVersion', metadata.get_version()),
+            ('DefaultUIFont', 'DlgFont8'),
+            ('ErrorDialog', 'ErrorDlg'),
+            ('Progress1', 'Install'),
+            ('Progress2', 'installs'),
+            ('MaintenanceForm_Action', 'Repair'),
+            ('ALLUSERS', '2'),
+            ('MSIINSTALLPERUSER', '1'),
+        ]
+        email = metadata.author_email or metadata.maintainer_email
+        if email:
+            props.append(("ARPCONTACT", email))
+        if metadata.url:
+            props.append(("ARPURLINFOABOUT", metadata.url))
+        if self.upgrade_code is not None:
+            props.append(("UpgradeCode", self.upgrade_code))
+        msilib.add_data(self.db, 'Property', props)
+
+    def _add_whichusers_dialog(self):
+        """Per-user or per-machine install dialog."""
+        # based on dialog from cpython 2.7 source code
+        # https://svn.python.org/projects/python/trunk/Tools/msi/msi.py
+        whichusers = cx_Freeze.command.bdist_msi.PyDialog(
+            self.db, "WhichUsersDlg", self.x, self.y, self.width, self.height,
+            self.modal, self.title, "AdminInstall", "Next", "Cancel"
+        )
+        whichusers.title(
+            "Select for which users to install [ProductName]."
+        )
+        # A radio group with two options: allusers, justme
+        radio = whichusers.radiogroup(
+            "AdminInstall", 135, 60, 235, 80, 3, "WhichUsers", "", "Next"
+        )
+        radio.condition("Disable", "VersionNT=600") # Not available on Vista and Windows 2008
+        radio.add("ALL", 0, 5, 150, 20, "Install for all users")
+        radio.add("JUSTME", 0, 25, 235, 20, "Install just for me")
+
+        whichusers.backbutton("Back", None, active=0)
+
+        button = whichusers.nextbutton("Next >", "Cancel")
+        # SetProperty events
+        # https://docs.microsoft.com/en-us/windows/desktop/Msi/setproperty-controlevent
+        button.event("[MSIINSTALLPERUSER]", "{}", 'WhichUsers="ALL"', 1)
+        button.event("[MSIINSTALLPERUSER]", "1", 'WhichUsers="JUSTME"', 1)
+        # set the target dir to the default location for per-user/per-machine installs
+        button.event(
+            "[TARGETDIR]",
+            "[ProgramFilesFolder]\\%s %s" % (NAME, SHORT_VERSION),
+            'WhichUsers="JUSTME"', 1
+        )
+        button.event("EndDialog", "Return", 3)
+
+        button = whichusers.cancelbutton("Cancel", "AdminInstall")
+        button.event("SpawnDialog", "CancelDlg")
+
+    def add_ui(self):
+        self._add_whichusers_dialog()
+        cx_Freeze.bdist_msi.add_ui(self)
+
+
+
+
 def package():
     """Build a Windows .MSI package."""
     setup_options = SETUP_OPTIONS
     make_ready()
-
-    class BuildExeCommand(cx_Freeze.build_exe):
-        """Custom build_exe command."""
-
-        def run(self):
-            """Run build_exe command."""
-            mkdir(RESOURCE_PATH)
-            build_icon()
-            # only include 32-bit DLLs
-            cx_Freeze.build_exe.run(self)
-            build_dir = 'build/exe.{}/'.format(PLATFORM_TAG)
-            # build_exe just includes everything inside the directory
-            # so remove some stuff we don't need
-            for root, _, files in os.walk(build_dir + 'lib'):
-                testing = set(root.split(os.sep)) & set(('test', 'tests', 'testing', 'examples'))
-                for fname in files:
-                    name = os.path.join(root, fname)
-                    if (
-                            # remove superfluous copies of python dll in lib/
-                            # as there is a copy in the package root already
-                            fname.lower() == 'python37.dll'
-                            # remove tests and examples
-                            or testing
-                            # we're only producing packages for win32_x86
-                            or 'win32_x64' in name or name.endswith('.dylib')
-                        ):
-                        remove(name)
-            # chunky libs on python3.7 that I don't think we use
-            remove(build_dir + 'lib/libcrypto-1_1.dll')
-            remove(build_dir + 'lib/libssl-1_1.dll')
-            # unneeded sdl2 bits
-            remove(build_dir + 'lib/sdl2dll/dll/SDL2_image.dll')
-            remove(build_dir + 'lib/sdl2dll/dll/SDL2_mixer.dll')
-            remove(build_dir + 'lib/sdl2dll/dll/SDL2_ttf.dll')
-            remove(build_dir + 'lib/sdl2dll/dll/libwebp-7.dll')
-            remove(build_dir + 'lib/sdl2dll/dll/libtiff-5.dll')
-            remove(build_dir + 'lib/sdl2dll/dll/libopus-0.dll')
-            remove(build_dir + 'lib/sdl2dll/dll/libopusfile-0.dll')
-            remove(build_dir + 'lib/sdl2dll/dll/libogg-0.dll')
-            remove(build_dir + 'lib/sdl2dll/dll/libmodplug-1.dll')
-
-
-    class BdistMsiCommand(cx_Freeze.bdist_msi):
-        """Custom bdist_msi command."""
-
-        def run(self):
-            """Run build_msi command."""
-            name = '{}-{}'.format(NAME, VERSION)
-            remove('dist/{}.msi'.format(name))
-            cx_Freeze.bdist_msi.run(self)
-            # close the database file so we can rename the file
-            del self.db
-            os.rename('dist/{}-win32.msi'.format(name), 'dist/{}.msi'.format(name))
-            #make_clean()
-
-        def add_config(self):
-            """Override cx_Freeze add_config."""
-            # mostly copy-paste from cxfreeze source, wich some changes
-            if self.directories:
-                msilib.add_data(self.db, "Directory", self.directories)
-            msilib.add_data(
-                self.db, 'CustomAction',
-                [("A_SET_TARGET_DIR", 256 + 51, "TARGETDIR", self.initial_target_dir)]
-            )
-            if self.add_to_path:
-                msilib.add_data(
-                    self.db, 'Environment', [("E_PATH", "=-*Path", r"[~];[TARGETDIR]", "TARGETDIR")]
-                )
-            msilib.add_data(self.db, 'InstallUISequence', [
-                ("PrepareDlg", None, 140),
-                # this is new
-                # sould probably be conditional on "not already installed" or smth
-                ("WhichUsersDlg", "not Installed", 400),
-                ("A_SET_TARGET_DIR", 'TARGETDIR=""', 401),
-                ("SelectDirectoryDlg", "not Installed", 1230),
-                ("MaintenanceTypeDlg", "Installed and not Resume and not Preselected", 1250),
-                ("ProgressDlg", None, 1280),
-            ])
-            msilib.add_data(self.db, 'InstallExecuteSequence', [
-                ("A_SET_TARGET_DIR", 'TARGETDIR=""', 401),
-                ('WriteEnvironmentStrings', 'MSIINSTALLPERUSER=""', 5200),
-            ])
-            for index, executable in enumerate(self.distribution.executables):
-                if executable.shortcut_name is not None and executable.shortcut_dir is not None:
-                    base_name = os.path.basename(executable.target_name)
-                    msilib.add_data(
-                        self.db, "Shortcut", [(
-                            "S_APP_%s" % index, executable.shortcut_dir, executable.shortcut_name,
-                            "TARGETDIR", "[TARGETDIR]%s" % base_name,
-                            None, None, None, None, None, None, None
-                        )]
-                    )
-            for table_name, data in self.data.items():
-                col = self._binary_columns.get(table_name)
-                if col is not None:
-                    data = [
-                        (*row[:col], msilib.Binary(row[col].name), *row[col + 1 :])
-                        for row in data
-                    ]
-                msilib.add_data(self.db, table_name, data)
-
-        def add_properties(self):
-            """Override cx_Freeze add_properties."""
-            # mostly copy-paste from cxfreeze
-            metadata = self.distribution.metadata
-            props = [
-                ('DistVersion', metadata.get_version()),
-                ('DefaultUIFont', 'DlgFont8'),
-                ('ErrorDialog', 'ErrorDlg'),
-                ('Progress1', 'Install'),
-                ('Progress2', 'installs'),
-                ('MaintenanceForm_Action', 'Repair'),
-                ('ALLUSERS', '2'),
-                ('MSIINSTALLPERUSER', '1'),
-            ]
-            email = metadata.author_email or metadata.maintainer_email
-            if email:
-                props.append(("ARPCONTACT", email))
-            if metadata.url:
-                props.append(("ARPURLINFOABOUT", metadata.url))
-            if self.upgrade_code is not None:
-                props.append(("UpgradeCode", self.upgrade_code))
-            msilib.add_data(self.db, 'Property', props)
-
-        def _add_whichusers_dialog(self):
-            """Per-user or per-machine install dialog."""
-            # based on dialog from cpython 2.7 source code
-            # https://svn.python.org/projects/python/trunk/Tools/msi/msi.py
-            whichusers = cx_Freeze.command.bdist_msi.PyDialog(
-                self.db, "WhichUsersDlg", self.x, self.y, self.width, self.height,
-                self.modal, self.title, "AdminInstall", "Next", "Cancel"
-            )
-            whichusers.title(
-                "Select for which users to install [ProductName]."
-            )
-            # A radio group with two options: allusers, justme
-            radio = whichusers.radiogroup(
-                "AdminInstall", 135, 60, 235, 80, 3, "WhichUsers", "", "Next"
-            )
-            radio.condition("Disable", "VersionNT=600") # Not available on Vista and Windows 2008
-            radio.add("ALL", 0, 5, 150, 20, "Install for all users")
-            radio.add("JUSTME", 0, 25, 235, 20, "Install just for me")
-
-            whichusers.backbutton("Back", None, active=0)
-
-            button = whichusers.nextbutton("Next >", "Cancel")
-            # SetProperty events
-            # https://docs.microsoft.com/en-us/windows/desktop/Msi/setproperty-controlevent
-            button.event("[MSIINSTALLPERUSER]", "{}", 'WhichUsers="ALL"', 1)
-            button.event("[MSIINSTALLPERUSER]", "1", 'WhichUsers="JUSTME"', 1)
-            # set the target dir to the default location for per-user/per-machine installs
-            button.event(
-                "[TARGETDIR]",
-                "[ProgramFilesFolder]\\%s %s" % (NAME, SHORT_VERSION),
-                'WhichUsers="JUSTME"', 1
-            )
-            button.event("EndDialog", "Return", 3)
-
-            button = whichusers.cancelbutton("Cancel", "AdminInstall")
-            button.event("SpawnDialog", "CancelDlg")
-
-        def add_ui(self):
-            self._add_whichusers_dialog()
-            cx_Freeze.bdist_msi.add_ui(self)
-
 
     # remove the WriteEnvironmentStrings action from the default list
     # since cx_Freeze picks up the default list and puts it into the MSI
